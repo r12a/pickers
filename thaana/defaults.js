6a4b50dcec26aee44913765de521270dba04d4a9
r12a <ishida@w3.org>
2017-07-09 09:02:07 +0100

    thaana, new picker (v20)

---
+++ thaana/defaults.js
@@ -0,0 +1,13 @@
+var defaults = new Object;
+
+defaults.font = '"Noto Sans Thaana WF"'; // font name per usage in CSS
+defaults.size = "50";  // number of pixels (just the number)
+defaults.rows = "2"; // number representing number of times 100px for height of box
+defaults.lineHeight = "1.5";  // just the number
+defaults.view = "alphabet";  // alphabet, phonic, phonic2, phonelist
+defaults.language = 'hy'
+defaults.blocklocation= '/scripts/thaana/block'
+defaults.uifont ='"Noto Sans Thaana WF"'; // font name per usage in CSS
+defaults.uisize = "35";  // number of pixels (just the number)
+defaults.ccbase = "◌";  // default base for combining characters
+
