866b9cbf73b4c708d32b11c10ab37ba68e37201c
r12a <ishida@w3.org>
2017-07-09 15:25:29 +0100

    thaana, tweaks to ui

--- thaana/defaults.js
+++ thaana/defaults.js
@@ -1,7 +1,7 @@
 var defaults = new Object;
 
 defaults.font = '"Noto Sans Thaana WF"'; // font name per usage in CSS
-defaults.size = "50";  // number of pixels (just the number)
+defaults.size = "40";  // number of pixels (just the number)
 defaults.rows = "2"; // number representing number of times 100px for height of box
 defaults.lineHeight = "1.5";  // just the number
 defaults.view = "alphabet";  // alphabet, phonic, phonic2, phonelist
