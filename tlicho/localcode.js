bdfea25648d98d0513be7c90a97c3c94c7ea9ac7
r12a <ishida@w3.org>
2017-07-09 16:29:30 +0100

    Tłı̨chǫ, update to v20

--- tlicho/localcode.js
+++ tlicho/localcode.js
@@ -1,5 +1,3 @@
-var _showMesherTrans =  ''
-var _showISOTrans = ''
 
 
 function localInitialise () {
@@ -15,8 +13,17 @@ end: {}
 
 
 var keyboarddef = [
-"|1,|2,|3,|4,|5,|6,|7,|8,‹|9,›|0,«|»,‘|̀",
-"Q|q,W|w,E|e,R|r,T|t,Y|y,U|u,I|ı,O|o,P|p,“|̨,”|̨̀",
-"A|a,S|s,D|d,F|f,G|g,H|h,J|j,K|k,L|l,Ł|ł,|’",
-"Z|z,X|x,C|c,V|v,B|b,N|n,M|m,|;,.|:,Ɂ|ɂ"
+",1|,2|,3|,4|,5|,6|,7|,8|‹,9|›,0|«,»|‘,̀",
+"Q,q|W,w|E,e|R,r|T,t|Y,y|U,u|I,ı|O,o|P,p|“,̨|”,̨̀",
+"A,a|S,s|D,d|F,f|G,g|H,h|J,j|K,k|L,l|Ł,ł|,’",
+"Z,z|X,x|C,c|V,v|B,b|N,n|M,m|,;|.,:|Ɂ,ɂ"
 ]
+
+var keyboardguide = [
+"1,2,3,4,5,6,7,8,9,0,-,=",
+"q,w,e,r,t,y,u,i,o,p,[,],",
+"a,s,d,f,g,h,j,k,l,;,',\\",
+"z,x,c,v,b,n,m,,.,/,"
+]
+
+var keyboardRowOffset = ['5px','20px','30px','50px']
