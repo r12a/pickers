8cedbd34fd49d3b3942b542b26b3fe9c5def0e35
r12a <ishida@w3.org>
2017-07-09 15:18:28 +0100

    runic, update to v20

--- runic/defaults.js
+++ runic/defaults.js
@@ -1,7 +1,7 @@
 var defaults = new Object;
 
 defaults.font = '"Junicode WF"'; // font name per usage in CSS
-defaults.size = "30";  // number of pixels (just the number)
+defaults.size = "36";  // number of pixels (just the number)
 defaults.rows = "3"; // number representing number of times 100px for height of box
 defaults.lineHeight = "1.5";  // just the number
 defaults.view = "alphabet";  // alphabet, phonic, phonelist
