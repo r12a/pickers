630546178a65a4684fcdfd96bc47e7429c5740ea
r12a <ishida@w3.org>
2017-07-18 09:16:39 +0100

    add cherokee picker

---
+++ cherokee/defaults.js
@@ -0,0 +1,13 @@
+var defaults = new Object;
+
+defaults.font = 'Noto Sans Cherokee WF'; // font name per usage in CSS
+defaults.size = "45";  // number of pixels (just the number)
+defaults.rows = "3"; // number representing number of times 100px for height of box
+defaults.lineHeight = "1.5";  // just the number
+defaults.view = "alphabet";  // alphabet, phonic, phonic2, phonelist
+defaults.language = 'chr'
+defaults.blocklocation= '/scripts/cherokee/block'
+defaults.uifont = 'Noto Sans Cherokee WF'; // font name per usage in CSS
+defaults.uisize = "40";  // number of pixels (just the number)
+defaults.ccbase = "◌";  // default base for combining characters
+
