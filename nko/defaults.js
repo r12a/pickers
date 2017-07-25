69a8bef095307dcc2a8b4dec76b1c7ff0cc35a26
r12a <ishida@w3.org>
2017-07-25 17:21:44 +0100

    nko, improve ui styling

--- nko/defaults.js
+++ nko/defaults.js
@@ -2,12 +2,12 @@ var defaults = new Object;
 
 defaults.font = '"Noto Sans NKo WF"'; // font name per usage in CSS
 defaults.size = "35";  // number of pixels (just the number)
-defaults.rows = "1.5"; // number representing number of times 100px for height of box
+defaults.rows = "2.5"; // number representing number of times 100px for height of box
 defaults.lineHeight = "1.5";  // just the number
 defaults.view = "alphabet";  // alphabet, phonic, phonic2, phonelist
 defaults.language = 'hy'
 defaults.blocklocation= '/scripts/nko/block'
 defaults.uifont ='"Noto Sans NKo WF"'; // font name per usage in CSS
-defaults.uisize = "24";  // number of pixels (just the number)
-defaults.ccbase = "";  // default base for combining characters
+defaults.uisize = "36";  // number of pixels (just the number)
+defaults.ccbase = "\u00A0";  // default base for combining characters
 
