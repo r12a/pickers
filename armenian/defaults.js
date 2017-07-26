a1d2117d1c4197fbf40208998234e4653b5d980b
r12a <ishida@w3.org>
2017-07-26 21:18:51 +0100

    armenian, restyle space etc; add url generator; change example text; add del touches; centre selection area; add punctuation; move deprecated characters

--- armenian/defaults.js
+++ armenian/defaults.js
@@ -1,13 +1,13 @@
 var defaults = new Object;
 
-defaults.font = '"Noto Sans Armenian WF"'; // font name per usage in CSS
+defaults.font = '"Arian AMU WF"'; // font name per usage in CSS
 defaults.size = "35";  // number of pixels (just the number)
-defaults.rows = "1.5"; // number representing number of times 100px for height of box
+defaults.rows = "2"; // number representing number of times 100px for height of box
 defaults.lineHeight = "1.5";  // just the number
 defaults.view = "alphabet";  // alphabet, phonic, phonic2, phonelist
 defaults.language = 'hy'
 defaults.blocklocation= '/scripts/armenian/block'
-defaults.uifont ='"Noto Sans Armenian WF"'; // font name per usage in CSS
-defaults.uisize = "24";  // number of pixels (just the number)
+defaults.uifont ='"Arian AMU WF"'; // font name per usage in CSS
+defaults.uisize = "28";  // number of pixels (just the number)
 defaults.ccbase = "";  // default base for combining characters
 
