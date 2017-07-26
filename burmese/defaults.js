62c024467da25691e8b0dfce143da542c20317bd
r12a <ishida@w3.org>
2017-07-26 22:22:04 +0100

    burmese, restyle space etc; add url generator; change example text; add del touches; centre selection area

--- burmese/defaults.js
+++ burmese/defaults.js
@@ -2,7 +2,7 @@ var defaults = new Object;
 
 defaults.font = 'Myanmar3 WF' // font name per usage in CSS
 defaults.size = "32";  // number of pixels (just the number)
-defaults.rows = "1.5"; // number representing number of times 100px for height of box
+defaults.rows = "2"; // number representing number of times 100px for height of box
 defaults.view = "alphabet";  // alphabet, fontgrid
 defaults.language = 'my'
 defaults.blocklocation= '/scripts/myanmar/block'
