508a63196d942810a19f513472cd88b357cdd040
r12a <ishida@w3.org>
2017-07-30 14:35:25 +0100

    new syriac picker

---
+++ syriac/defaults.js
@@ -0,0 +1,14 @@
+var defaults = new Object;
+
+defaults.font = '"Noto Sans Syriac Estrangela"'; // font name per usage in CSS
+defaults.size = "50";  // number of pixels (just the number)
+defaults.rows = "2.5"; // number representing number of times 100px for height of box
+defaults.lineHeight = "1.2";  // just the number
+defaults.view = "alphabet";  // alphabet, phonic, phonelist
+defaults.language = 'ar'
+defaults.blocklocation= '/scripts/arabic/block'
+defaults.uifont = '"Noto Sans Syriac Estrangela"'; // font name per usage in CSS
+defaults.uisize = "30";  // number of pixels (just the number)
+defaults.ccbase = "\u00A0";  // default base for combining characters
+
+
