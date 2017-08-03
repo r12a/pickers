f4f49446396e860af98e0b7df498a47ba38a96d7
r12a <ishida@w3.org>
2017-08-03 22:45:53 +0100

    syriac, change sample; change placeholder; reduce default size

--- syriac/defaults.js
+++ syriac/defaults.js
@@ -1,7 +1,7 @@
 var defaults = new Object;
 
 defaults.font = '"Noto Sans Syriac Estrangela"'; // font name per usage in CSS
-defaults.size = "50";  // number of pixels (just the number)
+defaults.size = "40";  // number of pixels (just the number)
 defaults.rows = "2.5"; // number representing number of times 100px for height of box
 defaults.lineHeight = "1.2";  // just the number
 defaults.view = "alphabet";  // alphabet, phonic, phonelist
