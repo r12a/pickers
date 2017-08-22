2957f30c04c638701f9d3ee6d916dd08b52ce862
r12a <ishida@w3.org>
2017-08-22 11:14:47 +0100

    hebrew, add webfont, set default base to ''

--- hebrew/defaults.js
+++ hebrew/defaults.js
@@ -1,14 +1,14 @@
 var defaults = new Object
 var factoryDefaults = new Object
-	factoryDefaults.font = 'Times New Roman'; // text area font name (a single font, no quotes)
+	factoryDefaults.font = 'Noto Serif Hebrew WF'; // text area font name (a single font, no quotes)
 	factoryDefaults.size = "48";  // text area font size, number of pixels (just the number)
 	factoryDefaults.rows = "2.5"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "1.5"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'he' // language to use for examples
 	factoryDefaults.blocklocation= '/scripts/hebrew/block'  // blocklocation to use for examples
-	factoryDefaults.uifont = 'Times New Roman'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uifont = 'Noto Serif Hebrew WF'; // font name  for selection area (a single font, no quotes)
 	factoryDefaults.uisize = "36";  // selection area font size, number of pixels (just the number)
-	factoryDefaults.ccbase = "\u25CC";  // default base for combining characters
+	factoryDefaults.ccbase = "";  // default base for combining characters
 	factoryDefaults.direction = "rtl" // indicates whether this is a picker for a RTL script
 	factoryDefaults.uidir = "rtl" // indicates the base direction for the selection area
 	factoryDefaults.contrast = "low" // contrast for UI text colours
