1d9544a5851e3d9846ea09ec76f2f143ec58229c
r12a <ishida@w3.org>
2017-09-06 10:01:04 +0100

    mongoliangkbd

---
+++ mongoliangkbd/defaults.js
@@ -0,0 +1,38 @@
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'Graphetic prototype WF' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "50";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "2.5"; // number representing number of times 100px for height of text area
+	factoryDefaults.lineheight = "1.8"; // number representing line height of text area in pixels/100
+	factoryDefaults.language = 'mn' // language to use for examples
+	factoryDefaults.uifont = 'Graphetic prototype WF'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "40";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "\u00A0";  // default base for combining characters
+	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
+	factoryDefaults.contrast = "low" // contrast for UI text colours
+
+
+var thisPicker = 'mongoliangPicker'
+
+if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
+else  defaults = factoryDefaults
+ 
+ 
+var template = {}
+	template.title = 'Mongolian graphetic kbd character picker'
+	template.sample = 'ᠰᡠᡣᡣᡠ ᠪᡠᡣᡣᡡᢀ ᡥᡦ'
+	template.blocklocation= 'mongolian'  // blocklocation to use for examples
+	template.direction = "ltr" // indicates whether this is a picker for a RTL script
+	template.github = 'mongoliang'
+
+
+// ᮞᮊᮥᮙ᮪ᮔ ᮏᮜ᮪ᮙ ᮌᮥᮘᮢᮌ᮪ ᮊ ᮃᮜᮙ᮪ ᮓᮥᮑ ᮒᮨᮂᮞᮤᮖᮒ᮪ᮔ ᮙᮨᮛ᮪ᮓᮤᮊ ᮏᮩᮀ ᮘᮧᮌ ᮙᮛ᮪ᮒᮘᮒ᮪ ᮊᮒᮥᮒ᮪ ᮠᮊ᮪-ᮠᮊ᮪ ᮃᮔᮥ ᮞᮛᮥᮃ. ᮙᮛᮔᮨᮂᮔ ᮓᮤᮘᮨᮛᮨ ᮃᮊᮜ᮪ ᮏᮩᮀ ᮠᮒᮨ ᮔᮥᮛᮔᮤ, ᮎᮙ᮪ᮕᮥᮁ-ᮌᮅᮜ᮪ ᮏᮩᮀ ᮞᮞᮙᮔ ᮃᮚ ᮓᮤᮔ ᮞᮥᮙᮔᮨᮒ᮪ ᮓᮥᮓᮥᮜᮥᮛᮔ᮪. 
+
+
+var inputAids = [
+{"title":"Latin characters", "dataVar":"showLatinTrans", "dataLocn":"latintranscript", "dataShortTitle":"L", "type":"palette"}
+]
+
+var controls = [
+// {"title":"Sundanese<br/>to Latin", "alt":"Convert Sundanese text to a Latin transcription.", "code":"doTranscription('toLatin')"},
+]
---
+++ mongoliangkbd/localcode.js
@@ -0,0 +1,112 @@
+function localInitialise () {
+
+	}
+
+
+
+var _h = {
+'1B83': ['1B86', '1B89'],
+'1B86': ['1B83', '1B89'],
+'1B89': ['1B86', '1B83'],
+'1B84': ['1B94', '1BB2'],
+'1B87': ['1B90', '1B8C'],
+'1B8A': ['1B9E'],
+'1B8C': ['1B8D', '1B87'],
+'1B8D': ['1B8C'],
+'1B8E': ['1B96', '1BB8'],
+'1B8F': ['1B93'],
+'1B93': ['1B8F'],
+'1B94': ['1B8D'],
+'1B96': ['1B97', '1B95'],
+'1B97': ['1B96', '1B96', '1B95'],
+'1B95': ['1B96', '1B96', '1B97', '1BBB'],
+'1B91': ['1B98'],
+'1B98': ['1B91'],
+'1B9C': ['1BB7'],
+'1BB7': ['1B9C'],
+'1B90': ['1B87', '1B85'],
+'1B85': ['1BB3', '1BB6'],
+'1BB3': ['1B85', '1BB6'],
+'1B9E': ['1B9F', '1BB4'],
+'1B9D': ['1BB1'],
+'1BAF': ['1B9F', '1B9A'],
+'1B9F': ['1BAF', '1B9E'],
+'1BB8': ['1B8E'],
+'1BB9': ['1BB6', '1BB3', '1B85'],
+'1BB6': ['1BB9', '1BB3'],
+'1BB2': ['1B8D'],
+'1BB4': ['1B9E'],
+'1CC3': ['1BB0'],
+'1BB0': ['1CC3'],
+'1BBB': ['1B95'],
+
+end: {}
+}
+
+
+function showCodepoints () {
+	var output = document.getElementById('output')
+	showNameDetails(normalise(getHighlightedText(output)), defaults.language, defaults.blocklocation, 'c', document.getElementById('panel') )
+	document.querySelector('#panel #title').style.fontFamily = output.style.fontFamily
+	output.focus()
+	}
+
+
+// forms with various positions
+var isolinitmedifina = new Set(['\u1860', '\u1863'])
+
+var initmedifina = new Set(['\u1861', '\u1865',, '\u1868', '\u1869', '\u1889', '\u186A', '\u186B', '\u186D', '\u1841',
+						   '\u182A', '\u182B', '\u182E', '\u182F', '\u1830', '\u1831', '\u1834', '\u1837', '\u1839', '\u183A', '\u183C', '\u183D'])
+
+var isolonly = new Set(['\u1880'])
+
+var isolfina = new Set(['\u1866'])
+
+var initmedi = new Set(['\u1864'])
+
+var medifina = new Set(['\u1867', '\u186C', '\u1854'])
+
+var initonly = new Set(['\u183F', '\u1842'])
+
+
+function event_mouseoverChar ()  {
+	// overrides the function in shared20/code.js to add shaping forms for all syriac styles
+	
+	// add cursive forms to table
+	var ncr = String.fromCodePoint('0x'+this.id)
+	if (isolinitmedifina.has(ncr)) ncr = ncr+' '+ncr+'\u200D \u200D'+ncr+'\u200D \u200D'+ncr
+	else if (initmedifina.has(ncr)) ncr = ncr+'\u200D \u200D'+ncr+'\u200D \u200D'+ncr
+	else if (isolfina.has(ncr)) ncr = ncr+' • • \u200D'+ncr
+	else if (initmedi.has(ncr)) ncr = ncr+'\u200D \u200D'+ncr+'\u200D •'
+	else if (medifina.has(ncr)) ncr = '• \u200D'+ncr+'\u200D \u200D'+ncr
+	else if (initonly.has(ncr)) ncr = ncr+'\u200D • •'
+	var cursive = '<bdi>'+ncr+'</bdi>'
+	document.getElementById('cursive').innerHTML = cursive
+	
+	
+	// display character information
+	var span = document.createElement( 'span' );
+	span.setAttribute( 'id', 'charname' );
+	var charinfo = document.createTextNode( this.title );
+	span.appendChild(charinfo);
+	
+	var chardata = document.getElementById('chardata');	
+	chardata.replaceChild( span, chardata.firstChild );
+	
+	// highlight this character
+	this.style.backgroundColor = '#CF9'
+	this.style.backgroundColor = '#fc6'
+	this.style.backgroundColor = 'rgb(244,99,11)'
+	this.style.color = 'rgb(221,221,221)'
+	
+	// highlight similar characters
+	if (globals.showShapeHints && _h[this.id]) {
+		ptr = this.id
+		for (i=0;i<_h[ptr].length;i++) {
+			//document.getElementById(_h[ptr][i]).style.backgroundColor = '#E6FFCD'
+			document.getElementById(_h[ptr][i]).style.backgroundColor = '#FFE6B2'
+			//document.getElementById(_h[ptr][i]).style.backgroundColor = '#FFE680'
+			}
+		}
+	}
+
