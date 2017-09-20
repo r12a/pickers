c2b1b829a496cc9573642ed4ac7503d10c018a43
r12a <ishida@w3.org>
2017-09-20 11:14:09 +0100

    mongoliang, add ZWNJ after U-tailed

--- mongoliang/localcode.js
+++ mongoliang/localcode.js
@@ -6,39 +6,6 @@ function localInitialise () {
 
 var _h = {
 '1B83': ['1B86', '1B89'],
-'1B86': ['1B83', '1B89'],
-'1B89': ['1B86', '1B83'],
-'1B84': ['1B94', '1BB2'],
-'1B87': ['1B90', '1B8C'],
-'1B8A': ['1B9E'],
-'1B8C': ['1B8D', '1B87'],
-'1B8D': ['1B8C'],
-'1B8E': ['1B96', '1BB8'],
-'1B8F': ['1B93'],
-'1B93': ['1B8F'],
-'1B94': ['1B8D'],
-'1B96': ['1B97', '1B95'],
-'1B97': ['1B96', '1B96', '1B95'],
-'1B95': ['1B96', '1B96', '1B97', '1BBB'],
-'1B91': ['1B98'],
-'1B98': ['1B91'],
-'1B9C': ['1BB7'],
-'1BB7': ['1B9C'],
-'1B90': ['1B87', '1B85'],
-'1B85': ['1BB3', '1BB6'],
-'1BB3': ['1B85', '1BB6'],
-'1B9E': ['1B9F', '1BB4'],
-'1B9D': ['1BB1'],
-'1BAF': ['1B9F', '1B9A'],
-'1B9F': ['1BAF', '1B9E'],
-'1BB8': ['1B8E'],
-'1BB9': ['1BB6', '1BB3', '1B85'],
-'1BB6': ['1BB9', '1BB3'],
-'1BB2': ['1B8D'],
-'1BB4': ['1B9E'],
-'1CC3': ['1BB0'],
-'1BB0': ['1CC3'],
-'1BBB': ['1B95'],
 
 end: {}
 }
@@ -75,6 +42,7 @@ function event_mouseoverChar ()  {
 	
 	// add cursive forms to table
 	var ncr = this.id.replace(/200D/g,'')
+	var ncr = ncr.replace(/200C/g,'')
 	ncr = String.fromCodePoint('0x'+ncr)
 	//var ncr = String.fromCodePoint('0x'+this.id)
 	if (isolinitmedifina.has(ncr)) ncr = ncr+' '+ncr+'\u200D \u200D'+ncr+'\u200D \u200D'+ncr
@@ -131,3 +99,21 @@ function toggleShift (node, LC, UC) {
 		node.className = 'unshifted'
 		}
 	}
+
+
+
+var keyboarddef = [
+"᠑|᠒|᠓|᠔|᠕|᠖|᠗|᠘|᠙|᠐",
+"᠁,ᠴ|ᠸ,ᠣ|ᠧ,ᠡ|ᠿ,ᠷ|ᠲ|ᠶ|ᠦ|ᠢ|ᠥ|ᠫ",
+"ᡠ,ᢀ|ᠰ|ᠳ|ᠹ|ᠭ|ᠾ,ᠬ|ᠵ|ᠻ,ᠺ|ᡀ,ᠯ|《,》",
+"ᡁ,ᠽ|ᠱ|ᡂ,ᠼ|ᠤ|ᠪ|ᠩ,ᠨ|ᠮ|᠂|᠃"
+]
+
+var keyboardguide = [
+"1,2,3,4,5,6,7,8,9,0,-,=",
+"q,w,e,r,t,y,u,i,o,p,[,],",
+"a,s,d,f,g,h,j,k,l,;,',\\",
+"z,x,c,v,b,n,m,,.,/,"
+]
+
+var keyboardRowOffset = ['5px','20px','30px','50px']
