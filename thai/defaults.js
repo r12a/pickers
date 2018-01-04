470e61f789781c7a2eb9a7f4672b2d77375ece8b
r12a <ishida@w3.org>
2018-01-04 20:31:18 +0000

    thai, updated to latest v21 template

--- thai/defaults.js
+++ thai/defaults.js
@@ -1,15 +1,13 @@
-var defaults = new Object
-var factoryDefaults = new Object
-	factoryDefaults.font = 'Noto Sans Thai WF'; // text area font name (a single font, no quotes)
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'Noto Sans Thai WF' // text area font name (a single font, no quotes)
 	factoryDefaults.size = "40";  // text area font size, number of pixels (just the number)
 	factoryDefaults.rows = "2"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "1.8"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'th' // language to use for examples
-	factoryDefaults.blocklocation= '/scripts/thai/block'  // blocklocation to use for examples
 	factoryDefaults.uifont = 'Noto Sans Thai WF'; // font name  for selection area (a single font, no quotes)
 	factoryDefaults.uisize = "32";  // selection area font size, number of pixels (just the number)
 	factoryDefaults.ccbase = "\u00A0";  // default base for combining characters
-	factoryDefaults.direction = "ltr" // indicates whether this is a picker for a RTL script
 	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
 	factoryDefaults.contrast = "low" // contrast for UI text colours
 
@@ -18,3 +16,31 @@ var thisPicker = 'thaiPicker'
 
 if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
 else  defaults = factoryDefaults
+ 
+ 
+var template = {}
+	template.title = 'Thai character picker'
+	template.sample = "2. ทุกคนมีสิทธิที่จะออกจากประเทศใด ๆ ไป รวมทั้งประเทศของตนเองด้วย และที่จะกลับยังประเทศตน"
+	template.blocklocation= '/scripts/thai/block'  // blocklocation to use for examples
+	template.direction = "ltr" // indicates whether this is a picker for a RTL script
+	template.github = 'thai'
+	template.scriptcode = 'Thai'
+
+
+
+var inputAids = [
+{"title":"Hinting", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint"},
+{"title":"Shape lookup", "dataVar":"showShapeLookup", "dataLocn":"shapelist", "dataShortTitle":"S", "type":"shape"},
+{"title":"Latin characters", "dataVar":"showLatinTrans", "dataLocn":"latintranscript", "dataShortTitle":"L", "type":"palette"},
+{"title":"ISO transcription", "dataVar":"showISO2Trans", "dataLocn":"iso2transcript", "dataShortTitle":"I", "type":"palette"},
+{"title":"Becker transcription", "dataVar":"showBeckerTrans", "dataLocn":"beckertranscript", "dataShortTitle":"B", "type":"palette"}
+//{"title":"Keyboard", "dataVar":"showKeyboard", "dataLocn":"keyboard", "dataShortTitle":"K", "type":"keyboard"}
+]
+
+var controls = [
+{"title":"Thai<br>to ISO-1", "alt":"Convert Thai text to an ISO Latin transcription.", "code":"doTranscription('toISO1')"},
+{"title":"Thai<br>to ISO-2", "alt":"Convert Thai text to an ISO Latin transcription.", "code":"doTranscription('toISO2')"},
+{"title":"Thai<br>to IPA", "alt":"Convert Thai text to an approximate IPA transcription.", "code":"doTranscription('toIPA')"},
+{"title":"Remove<br>sp/hyph", "alt":"Remove spaces and hyphens.", "code":"add(condense(getHighlightedText(document.getElementById('output'))));document.getElementById('output').focus();"},
+]
+
