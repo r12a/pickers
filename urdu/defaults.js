0e8295248de856ba1699715687f0fdeca4cf85f6
r12a <ishida@w3.org>
2018-01-04 20:32:37 +0000

    urdu, updated to latest v21 template

--- urdu/defaults.js
+++ urdu/defaults.js
@@ -1,29 +1,13 @@
-var defaults = new Object;
-
-defaults.font = '"Noto Nastaliq Urdu WF"'; // font name per usage in CSS
-defaults.size = "30";  // number of pixels (just the number)
-defaults.rows = "3"; // number representing number of times 100px for height of box
-defaults.lineHeight = "3";  // just the number
-defaults.view = "alphabet";  // alphabet, phonic, phonelist
-defaults.language = 'ur'
-defaults.blocklocation= '/scripts/arabic/block'
-defaults.uifont = '"Noto Nastaliq Urdu WF"'; // font name per usage in CSS
-defaults.uisize = "24";  // number of pixels (just the number)
-defaults.ccbase = "◌";  // default base for combining characters
-
-
-var defaults = new Object
-var factoryDefaults = new Object
-	factoryDefaults.font = 'Noto Nastaliq Urdu WF'; // text area font name (a single font, no quotes)
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'Noto Nastaliq Urdu WF' // text area font name (a single font, no quotes)
 	factoryDefaults.size = "30";  // text area font size, number of pixels (just the number)
 	factoryDefaults.rows = "3"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "3"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'ur' // language to use for examples
-	factoryDefaults.blocklocation= '/scripts/arabic/block'  // blocklocation to use for examples
 	factoryDefaults.uifont = 'Noto Nastaliq Urdu WF'; // font name  for selection area (a single font, no quotes)
 	factoryDefaults.uisize = "24";  // selection area font size, number of pixels (just the number)
-	factoryDefaults.ccbase = "\u25CC";  // default base for combining characters
-	factoryDefaults.direction = "rtl" // indicates whether this is a picker for a RTL script
+	factoryDefaults.ccbase = "◌";  // default base for combining characters
 	factoryDefaults.uidir = "rtl" // indicates the base direction for the selection area
 	factoryDefaults.contrast = "low" // contrast for UI text colours
 
@@ -32,3 +16,27 @@ var thisPicker = 'urduPicker'
 
 if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
 else  defaults = factoryDefaults
+ 
+ 
+var template = {}
+	template.title = 'Urdu character picker'
+	template.sample = "(۲) ہر شخص کو اس بات کا حق ہے کہ وہ ملک سے چلا جائے چاہے یہ ملک اس کا اپنا ہو۔ اور اسی طرح اسے ملک میں واپس آ جانے کا بھی حق ہے۔"
+    template.blocklocation= '/scripts/arabic/block'  // blocklocation to use for examples
+	template.direction = "rtl" // indicates whether this is a picker for a RTL script
+	template.github = 'urdu'
+	template.scriptcode = 'Arab'
+
+
+
+var inputAids = [
+//{"title":"Show cursive forms", "dataVar":"showCursive", "dataLocn":"cursive", "dataShortTitle":"S", "type":"hint"},
+{"title":"Hinting", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint"},
+{"title":"Shape lookup", "dataVar":"showShapeLookup", "dataLocn":"shapelist", "dataShortTitle":"S", "type":"shape"},
+{"title":"Latin characters", "dataVar":"showLatinTrans", "dataLocn":"latintranscript", "dataShortTitle":"L", "type":"palette"},
+{"title":"Latin to Arabic", "dataVar":"showLOCTrans", "dataLocn":"loctranscript", "dataShortTitle":"L", "type":"palette"},
+{"title":"IPA to Arabic", "dataVar":"showIPATrans", "dataLocn":"ipatranscript", "dataShortTitle":"I", "type":"palette"},
+]
+
+var controls = [
+]
+
