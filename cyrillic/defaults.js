168b50b7f0f1a53557295d2c43b36fc18a347c46
r12a <ishida@w3.org>
2018-01-31 20:28:33 +0000

    Cyrillic picker !

---
+++ cyrillic/defaults.js
@@ -0,0 +1,35 @@
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'Times New Roman' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "40";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "2"; // number representing number of times 100px for height of text area
+	factoryDefaults.lineheight = "1.5"; // number representing line height of text area in pixels/100
+	factoryDefaults.language = 'en' // language to use for examples
+	factoryDefaults.uifont = 'Times New Roman'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "32";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "\u25CC";  // default base for combining characters
+	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
+	factoryDefaults.contrast = "low" // contrast for UI text colours
+
+
+var thisPicker = 'cyrillicPicker'
+
+if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
+else  defaults = factoryDefaults
+ 
+ 
+var template = {}
+	template.title = 'Cyrillic character picker'
+	template.sample = "Дарбанзаалак ауаҩы аҧсҭазаареи, ахақәиҭреи, хаҭала аӡә диламкьысуа аҟазаареи рзин имоуп."
+	template.blocklocation= '/scripts/cyrillic/block'  // blocklocation to use for examples
+	template.direction = "ltr" // indicates whether this is a picker for a RTL script
+	template.github = 'cyrillic'
+	template.scriptcode = 'Cyrl'
+
+
+
+var inputAids = [
+]
+
+var controls = [
+]
---
+++ cyrillic/localcode.js
@@ -0,0 +1,13 @@
+function localInitialise () {
+	}
+	
+
+var _h = {
+end: {}
+}
+
+
+
+
+
+
