7ecf79f820188561af7f12e20f27a17e2af84ec7
r12a <ishida@w3.org>
2018-01-31 19:48:09 +0000

    russian, fixed case conversion feature so that it updates character names

--- russian/localcode.js
+++ russian/localcode.js
@@ -35,4 +35,32 @@ function toggleShift (node, LC, UC) {
             }
 		node.className = 'unshifted'
 		}
+	// reinitialise ids to codepoint values of character sequence (with no leading zeros)
+	node = document.querySelectorAll( '.c' ); 
+	for (var n = 0; n < node.length; n++ ) { 
+		content = node[n].textContent
+		id=''
+		for (i=0;i<content.length;i++) {
+			id += convertChar2CP(content[i])
+			}
+		node[n].id = id
+        node[n].dataset.c = 'c'+id
+        dec = parseInt(id, 16)
+        while (id.length<4) id = '0'+id
+        node[n].title = 'U+'+id+': '+charData[content]
+		}
 	}
+
+
+function toTitleCase () {
+    // convert text area so each word begins with capital letter
+    var str = document.getElementById('output').value
+    var words = str.split(' ')
+    var newwords = []
+    for (let i=0;i<words.length;i++) {
+        console.log(words[i][0])
+        newwords.push(words[i][0].toUpperCase()+words[i].substr(1).toLowerCase())
+        console.log(newwords[i])
+        }
+    document.getElementById('output').value = newwords.join(' ')
+    }
