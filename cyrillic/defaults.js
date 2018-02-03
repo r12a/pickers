09831cee86c19be00ac850f09da87980c10f1766
r12a <ishida@w3.org>
2018-02-03 16:54:04 +0000

    cyrillic, default font times new roman -> doulos sil

--- cyrillic/defaults.js
+++ cyrillic/defaults.js
@@ -1,11 +1,11 @@
 var defaults = {}
 var factoryDefaults = {}
-	factoryDefaults.font = 'Times New Roman' // text area font name (a single font, no quotes)
+	factoryDefaults.font = 'Doulos SIL WF' // text area font name (a single font, no quotes)
 	factoryDefaults.size = "40";  // text area font size, number of pixels (just the number)
 	factoryDefaults.rows = "2"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "1.5"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'en' // language to use for examples
-	factoryDefaults.uifont = 'Times New Roman'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uifont = 'Doulos SIL WF'; // font name  for selection area (a single font, no quotes)
 	factoryDefaults.uisize = "32";  // selection area font size, number of pixels (just the number)
 	factoryDefaults.ccbase = "\u25CC";  // default base for combining characters
 	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
