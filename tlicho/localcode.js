12b0d4d8f0b70d828d352d84907f99495128ccc6
r12a <ishida@w3.org>
2018-02-21 08:44:21 +0000

    tlicho: upgrade to v22

--- tlicho/localcode.js
+++ tlicho/localcode.js
@@ -13,6 +13,22 @@ end: {}
 
 
 var keyboarddef = [
+"§|1 1|2 2|3 3|4 4|5 5|6 6|7 7|8 8|9 9 ‹|0 0 ›|- « »|= ‘ \u0300",
+"q q Q|w w W|e e E|r r R|t t T|y y Y|u u U|i ı I|o o O|p p P|[ “ \u0328|] ” \u0328\u0300",
+"a a A|s s S|d d D|f f F|g g G|h h H|j j J|k k K|l l L|; Ł ł|' ’|\\",
+"`|z z Z|x x X|c c C|v v V|b b B|n n N|m m M|, ;|. . :|/ Ɂ ɂ"
+]
+
+
+/*  FOR KEYBOARDS WITH DIFFERENT CHARS ON SHIFT LEVEL
+var keyboardUC = [
+"§|1|2|3|4|5|6|7|8|9|0|-|=",
+"q|w|e|r|t උ|y|u|i|o|p|[|]",
+"a ඒ ඌ ඖ|s|d ො ඎ ෲ|f|g|h|j|k|l|;|'|\\",
+"`|z|x|c|v|b|n|m|,|.|/"
+]
+
+var keyboarddef = [
 ",1|,2|,3|,4|,5|,6|,7|,8|‹,9|›,0|«,»|‘,̀",
 "Q,q|W,w|E,e|R,r|T,t|Y,y|U,u|I,ı|O,o|P,p|“,̨|”,̨̀",
 "A,a|S,s|D,d|F,f|G,g|H,h|J,j|K,k|L,l|Ł,ł|,’",
@@ -27,3 +43,4 @@ var keyboardguide = [
 ]
 
 var keyboardRowOffset = ['5px','20px','30px','50px']
+*/
