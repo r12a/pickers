9aad83f5db2f712798b750001c3ed8c56db5ec6b
r12a <ishida@w3.org>
2018-02-25 09:29:41 +0000

    old norse: upgrade to v22

--- oldnorse/localcode.js
+++ oldnorse/localcode.js
@@ -16,20 +16,26 @@ end: {}
 
 
 var keyboarddef = [
-"1|2|3|4|5|6|7|8|‹,9|›,0|«,»",
-"œ,q|w|é,e|ʀ,r|þ,t|ý,y|ú,u|í,i|ø,o|ǫ,ó|p",
-"á,a|s|ð,d|f|g|h|j|k|l|’|æ",
-"z|x|c|v|b|n|m|;|.,:|\u0361"
+"§|1 1|2 2|3 3|4 4|5 5|6 6|7 7|8 8|9 9 ‹|0 0 ›|- «|= »",
+"q q œ|w w|e e é|r r ʀ|t t þ|y y ý|u u ú|i i í|o o ǫ ó|p p p|[ ø|]",
+"a a á|s s|d d ð|f f|g g|h h|j j|k k|l l|; ’|' æ|\\",
+"z z|x x|c c|v v|b b|n n|m m|, , ;|. . :|/ \u0361"
 ]
 
-var keyboardguide = [
-"1,2,3,4,5,6,7,8,9,0,-,=",
-"q,w,e,r,t,y,u,i,o,,p,[,],",
-"a,s,d,f,g,h,j,k,l,;,',\\",
-"z,x,c,v,b,n,m,,.,/,"
+var keyboardUC = [
+"§|1 1|2 2|3 3|4 4|5 5|6 6|7 7|8 8|9 9 ‹|0 0 ›|- «|= »",
+"Q Q Œ|W W|E E É|R R Ʀ|T T Þ|Y Y Ý|U U Ú|I I Í|O O Ǫ Ó|P P P|[ Ø|]",
+"A A Á|S S|D D Ð|F F|G G|H H|J J|K K|L L|; ’|' Æ|\\",
+"Z Z|X X|C C|V V|B B|N N|M M|, , ;|. . :|/ \U0361"
 ]
 
-var keyboardRowOffset = ['5px','20px','30px','50px']
+
+function event_toggleKbdShift () {
+    if (this.classList.contains('shiftKeyOn')) {
+        makeKeyboard(keyboarddef, keyboardLCGuide)
+        }
+    else makeKeyboard(keyboardUC, keyboardUCGuide)
+    }
 
 
 
