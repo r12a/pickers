63dcbc26ed9fca0917362bf56ee4dc7b7f11544e
r12a <ishida@w3.org>
2018-02-26 07:49:55 +0000

    arabicblock: upgrade to v22

--- arabicblock/defaults.js
+++ arabicblock/defaults.js
@@ -1,12 +1,12 @@
 var defaults = {}
 var factoryDefaults = {}
-	factoryDefaults.font = 'Noto Naskh Arabic WF' // text area font name (a single font, no quotes)
+	factoryDefaults.font = 'Scheherazade WF' // text area font name (a single font, no quotes)
 	factoryDefaults.size = "50";  // text area font size, number of pixels (just the number)
 	factoryDefaults.rows = "2"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "1.8"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'ar' // language to use for examples
 	factoryDefaults.uifont = 'Noto Naskh Arabic WF'; // font name  for selection area (a single font, no quotes)
-	factoryDefaults.uisize = "40";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.uisize = "32";  // selection area font size, number of pixels (just the number)
 	factoryDefaults.ccbase = "\u25CC";  // default base for combining characters
 	factoryDefaults.uidir = "rtl" // indicates the base direction for the selection area
 	factoryDefaults.contrast = "low" // contrast for UI text colours
