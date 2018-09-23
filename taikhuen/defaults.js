38a9209abbab88322794d601665f61758f47d2a7
r12a <ishida@w3.org>
2018-09-23 08:56:25 +0100

    tai khün, +hinting

--- taikhuen/defaults.js
+++ taikhuen/defaults.js
@@ -29,7 +29,7 @@ var template = {}
 
 
 var inputAids = [
-//{"title":"Hinting", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint"},
+{"title":"Hinting", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint"},
 {"title":"Shape lookup", "dataVar":"showShapeLookup", "dataLocn":"shapelist", "dataShortTitle":"S", "type":"shape"},
 {"title":"Latin characters", "dataVar":"showLatinTrans", "dataLocn":"latintranscript", "dataShortTitle":"L", "type":"palette"},
 {"title":"Reverse transliteration", "dataVar":"showTranslit", "dataLocn":"transliteration", "dataShortTitle":"R", "type":"palette"},
