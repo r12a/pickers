a28ef4df0d289fcc52b1e9faa2a9cd386c01a748
r12a <ishida@w3.org>
2018-09-23 03:29:55 +0100

    northern thai, +hints, +shape lookup, +subjoined keys

--- northernthai/defaults.js
+++ northernthai/defaults.js
@@ -6,8 +6,8 @@ var factoryDefaults = {}
 	factoryDefaults.lineheight = "1.8"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'nod' // language to use for examples
 	factoryDefaults.uifont = 'Lanna Alif WF'; // font name  for selection area (a single font, no quotes)
-	factoryDefaults.uisize = "26";  // selection area font size, number of pixels (just the number)
-	factoryDefaults.ccbase = "\u25CC";  // default base for combining characters
+	factoryDefaults.uisize = "24";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "\u0020";  // default base for combining characters
 	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
 	factoryDefaults.contrast = "low" // contrast for UI text colours
 
@@ -29,14 +29,15 @@ var template = {}
 
 
 var inputAids = [
-//{"title":"Hinting", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint"},
-//{"title":"Shape lookup", "dataVar":"showShapeLookup", "dataLocn":"shapelist", "dataShortTitle":"S", "type":"shape"},
+{"title":"Hinting", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint"},
+{"title":"Shape lookup", "dataVar":"showShapeLookup", "dataLocn":"shapelist", "dataShortTitle":"S", "type":"shape"},
 {"title":"Latin characters", "dataVar":"showLatinTrans", "dataLocn":"latintranscript", "dataShortTitle":"L", "type":"palette"},
 {"title":"Reverse transliteration", "dataVar":"showTranslit", "dataLocn":"transliteration", "dataShortTitle":"R", "type":"palette"},
 {"title":"Keyboard", "dataVar":"showKeyboard", "dataLocn":"keyboard", "dataShortTitle":"K", "type":"keyboard"}
 ]
 
 var controls = [
-{"title":"Trans-<br/>literate", "alt":"Convert Thai text to a Latin transliteration.", "code":"doTranscription('transliterate')"},
+{"title":"Trans-<br/>literate", "alt":"Convert Northern Thai text to a Latin transliteration.", "code":"doTranscription('transliterate')"},
+//{"title":"Trans-<br/>scribe", "alt":"Convert Northern Thai text to an approximate Latin transcription.", "code":"doTranscription('transcription')", "warning":"Still in development. Mostly useful for complex vowels."},
 ]
 
--- northernthai/localcode.js
+++ northernthai/localcode.js
@@ -1,4 +1,5 @@
 globals.showTranslit =  ''
+globals.showShapeHints =  ''
 
 function localInitialise () {
 
@@ -7,57 +8,48 @@ function localInitialise () {
 
 
 var _h = {
-'E01': ['E20', 'E24', 'E20'],
-'E02': ['E0A', 'E0B', 'E03'],
-'E04': ['E14', 'E28', 'E15', 'E05'],
-'E06': ['E11', 'E21'],
-'E08': ['E27', 'E25'],
-'E0A': ['E02', 'E0B', 'E03'],
-'E0C': ['E13', 'E0D'],
-'E15': ['E05', 'E04', 'E14'],
-'E0F': ['E0E'],
-'E10': ['E23', 'E18'],
-'E11': ['E06'],
-'E16': ['E24', 'E20', 'E01', 'E26'],
-'E17': ['E11'],
-'E18': ['E23', 'E10'],
-'E1B': ['E1A', 'E29'],
-'E1C': ['E1E', 'E1D', 'E1F', 'E2C'],
-'E1E': ['E1C', 'E1D', 'E1F', 'E2C'],
-'E20': ['E16', 'E24', 'E01', 'E26'],
-'E1A': ['E1B', 'E29'],
-'E0E': ['E0F', 'E24', 'E26'],
-'E14': ['E04', 'E28', 'E15'],
-'E0B': ['E0A', 'E02', 'E03'],
-'E28': ['E04', 'E14', 'E15'],
-'E29': ['E1A'],
-'E2A': ['E25', 'E09'],
-'E1F': ['E1D', 'E1C', 'E1E', 'E2C'],
-'E1D': ['E1F', 'E1C', 'E1E', 'E2C'],
-'E2B': ['E17', 'E19'],
-'E2E': ['E2D'],
-'E25': ['E2A', 'E2FE25E2F'],
-'E2C': ['E1D', 'E1F', 'E1C', 'E1E'],
-'E23': ['E18', 'E10'],
-'E0D': ['E13', 'E0C'],
-'E27': ['E08', 'E07'],
-'E2D': ['E2E'],
-'E19': ['E21', 'E2B'],
-'E13': ['E0C', 'E0D'],
-'E21': ['E19', 'E17', 'E2B'],
-'E07': ['E27'],
-'E24': ['E26', 'E20', 'E16', 'E01'],
-'E2F': ['E5A'],
-'E5A': ['E2F'],
-'E32': ['E45', 'E33'],
-'E45': ['E32', 'E33'],
-'E33': ['E32', 'E45', 'E4D'],
-'E4D': ['E33'],
-'E2FE25E2F': ['E2A', 'E25'],
-'E05': ['E15'],
-'E03': ['E02', 'E0B', 'E0A'],
-'E26': ['E24', 'E20', 'E01', 'E16'],
-
+'1A601A38': ['1A601A37'],
+'1A601A37': ['1A601A38'],
+'1A601A36': ['1A601A3E'],
+'1A601A3E': ['1A601A36'],
+'1A601A48': ['1A601A47', '1A57'],
+'1A601A47': ['1A57', '1A601A48'],
+'1A601A4B': ['1A6C'],
+'1A6C': ['1A601A4B'],
+'1A45': ['1A80', '1A90'],
+'1A80': ['1A45', '1A90'],
+'1A90': ['1A45', '1A90'],
+'1A601A40': ['1A601A3F','1A6D'],
+'1A601A3F': ['1A601A40','1A6D'],
+'1A6D': ['1A601A40','1A601A3F'],
+'1A601A43': ['1A53','1A57'],
+'1A53': ['1A601A43','1A57'],
+'1A57': ['1A53','1A601A43','1A601A47', '1A601A48'],
+'1A63': ['1A81', '1A41'],
+'1A81': ['1A63', '1A41'],
+'1A41': ['1A81', '1A63'],
+'1A43': ['1A99','1A49'],
+'1A99': ['1A43'],
+'1A24': ['1A3C'],
+'1A3C': ['1A24'],
+'1A49': ['1A43'],
+'1A51': ['1A89'],
+'1A89': ['1A51'],
+'1A4D': ['1A5A'],
+'1A5A': ['1A4D'],
+'1A54': ['1A48', '1A63'],
+'1A5B': ['1A4C', '1A2E'],
+'1A2E': ['1A4A', '1A5B'],
+'1A4A': ['1A2E', '1A5B', '1A94'],
+'1A94': ['1A4A'],
+'1A56': ['1A601A43', '1A601A49'],
+'1A59': ['1A26'],
+'1A26': ['1A59'],
+'1A62': ['1A76'],
+'1A76': ['1A62'],
+'1A83': ['1A2F'],
+'1A85': ['1AA7'],
+'1AA7': ['1A85'],
 end: {}
 }
 
@@ -70,6 +62,20 @@ var keyboarddef = [
 ]
 
 
-
+function toggleSubjoined (node) {
+    // displays or hides the subjoined keys
+    var sjs = document.querySelectorAll('.sj')
+    console.log(sjs.length)
+    if (sjs[0].style.display !== 'inline-block') {
+        for (let i=0;i<sjs.length;i++) sjs[i].style.display = 'inline-block'
+        node.classList.toggle('subjoined')
+        document.getElementById('consonantGrid').style.width = '18em'
+        }
+    else {
+        for (let i=0;i<sjs.length;i++) sjs[i].style.display = 'none'
+        node.classList.toggle('subjoined')
+        document.getElementById('consonantGrid').style.width = '12em'
+        }
+    }
 
 
