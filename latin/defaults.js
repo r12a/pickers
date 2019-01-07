1252d84d68f984d01fef363a1bf67033cc2c9cea
r12a <ishida@w3.org>
2019-01-07 07:53:48 +0000

    latin, v23 + additional characters

--- latin/defaults.js
+++ latin/defaults.js
@@ -32,4 +32,6 @@ var inputAids = [
 ]
 
 var controls = [
+{"title":"Upper-<br>case", "alt":"Convert selection to uppercase.", "code":"add(getHighlightedText(document.getElementById('output')).toUpperCase());document.getElementById('output').focus();"},
+{"title":"Lower-<br>case", "alt":"Convert selection to lowercase.", "code":"add(getHighlightedText(document.getElementById('output')).toLowerCase());document.getElementById('output').focus();"},
 ]
--- latin/localcode.js
+++ latin/localcode.js
@@ -1,77 +1,49 @@
-var _showRadiceTrans =  ''
-var _showISOTrans = ''
-
 
 function localInitialise () {
 	}
 	
 
 var _h = {
-'995': ['9AC'],
-'996': ['9A5', '9AC'],
-'998': ['9AF', '9B7'],
-'999': ['9EC', '9AD'],
-'99A': ['9A2'],
-'99D': ['9AC'],
-'99E': ['98F', '990'],
-'99F': ['9A2', '9A29BC'],
-'9A1': ['9A19BC', '989', '98A', '9AD'],
-'9A2': ['9A29BC', '99F'],
-'9A4': ['985', '986', '9E9'],
-'9A5': ['996', '98B'],
-'9A8': ['9A3', '9B2'],
-'9AB': ['9AF', '998'],
-'9AC': ['9B0', '99D','9F1','9F0'],
-'9AD': ['9A1', '989', '9EC'],
-'9AE': ['9B8'],
-'9AF': ['9AF9BC', '998', '9B7'],
-'9B0': ['9AC', '99D'],
-'9B2': ['9A8'],
-'9B7': ['9AF', '998'],
-'9B8': ['9AE'],
-'9B9': ['987','9BD'],
-'9BD': ['9B9'],
-'9A19BC': ['9A1', '989', '98A'],
-'9A29BC': ['9A2', '99F'],
-'9AF9BC': ['9AF', '9B7', '998'],
-'985': ['986', '9A4'],
-'986': ['985', '9A4'],
-'987': ['9B9'],
-'989': ['98A', '9A1', '9AD'],
-'98A': ['989', '9A1', '9AD'],
-'98B': ['996','9E0'],
-'98F': ['990', '99E'],
-'990': ['98F', '99E'],
-'993': ['994'],
-'994': ['993'],
-'9C7': ['9CB', '9CC', '9C8'],
-'9C8': ['9C7'],
-'9CB': ['9CC', '9C7'],
-'9DC': ['9A19BC'],
-'9DD': ['9A29BC'],
-'9DF': ['9AF9BC'],
-'9F1': ['9F0', '9AC', '9B0'],
-'9F0': ['9F1', '9AC', '9B0'],
-'9F6': ['9F5', '9EF'],
-'9F5': ['9F6', '98C'],
-'9F7': ['964'],
-'964': ['9F7'],
-'9C0': ['9D7'],
-'9D7': ['9C0'],
-'9FA': ['981'],
-'981': ['9FA'],
-'9E0': ['98B', '9C3'],
-'9E1': ['9E2', '98C', '9F5'],
-'9E2': ['9E1'],
-'98C': ['9E1', '9F5'],
-'9F8': ['9A6'],
-'9A6': ['9F8'],
-
 end: {}
 }
 
 
+function toggleShift (node, LC, UC) {
+    var chars = document.querySelectorAll('.c')
+
+    if (node.className =='unshifted') {
+        for (let i=0;i<chars.length;i++) {
+            chars[i].textContent = chars[i].textContent.toLowerCase()
+            }
+		node.className = 'shifted'
+		} 
+	else {
+        for (let i=0;i<chars.length;i++) {
+            chars[i].textContent = chars[i].textContent.toUpperCase()
+            }
+		node.className = 'unshifted'
+		}
+	// reinitialise ids to codepoint values of character sequence (with no leading zeros)
+	node = document.querySelectorAll( '.c' ); 
+	for (var n = 0; n < node.length; n++ ) { 
+		content = node[n].textContent
+		id=''
+		for (i=0;i<content.length;i++) {
+			id += convertChar2CP(content[i])
+			}
+		node[n].id = id
+        node[n].dataset.c = 'c'+id
+        dec = parseInt(id, 16)
+        while (id.length<4) id = '0'+id
+        node[n].title = 'U+'+id+': '+charData[content]
+		}
+	}
 
 
+function unsetCase () {
+    // set case button back to default
+    // used when selecting a new orange character
+    document.getElementById('shiftToggle').className = 'shifted'
+    }
 
 
