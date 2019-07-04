2b6a9d2cba02de544358a59a1a46775e52fef7f7
r12a <ishida@w3.org>
2019-07-04 09:59:42 +0100

    tool updates

--- _tools/hint-maker-code.js
+++ _tools/hint-maker-code.js
@@ -1,24 +1,31 @@
 function generateCode () {
 	var lines = document.getElementById('in').value.split("\n")
 	// '56F': ['587','576'],
-	out = ''
+	var out = ''
 	for (var l=0;l<lines.length;l++) {  // for each line
-		chars = lines[l].trim().split(' ')
-		if (lines[l] === '') continue
-		out += "'";  // print the trigger character
-		for (i=0;i<chars[0].length;i++) {
-			out += chars[0].codePointAt(i).toString(16).toUpperCase()
-			}
-		out += "': ["
-		for (c=1;c<chars.length;c++) { // for each remaining character on the line
-			out += "'"
-			for (j=0;j<chars[c].length;j++) { // print that character
-				out += chars[c].codePointAt(j).toString(16).toUpperCase()
+		line = lines[l].replace(/\s+/g,' ').trim()
+		if (line === '') continue
+		chars = line.split(' ')
+		
+		for (q=0;q<chars.length;q++){
+			firstItem = chars.shift()
+			chars.push(firstItem)
+			
+			out += "'";  // print the trigger character
+			for (let i=0;i<chars[0].length;i++) {
+				out += chars[0].codePointAt(i).toString(16).toUpperCase()
 				}
-			out += "'"
-			if (c<chars.length-1) { out += ", " }
+			out += "': ["
+			for (let c=1;c<chars.length;c++) { // for each remaining character on the line
+				out += "'"
+				for (j=0;j<chars[c].length;j++) { // print that character
+					out += chars[c].codePointAt(j).toString(16).toUpperCase()
+					}
+				out += "'"
+				if (c<chars.length-1) { out += ", " }
+				}
+			out += "],\n"
 			}
-		out += "],\n"
 		}
 	document.getElementById('out').value = out
 	}
