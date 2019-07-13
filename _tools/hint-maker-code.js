e85fdeb26463641bc738b702407fefe14ef17aec
r12a <ishida@w3.org>
2019-07-13 06:40:50 +0100

    _tools/hint-maker-code, Remove redundancy

--- _tools/hint-maker-code.js
+++ _tools/hint-maker-code.js
@@ -27,5 +27,42 @@ function generateCode () {
 			out += "],\n"
 			}
 		}
+	
+	// reduce redundancy
+	out = out.replace(/ /g,'')
+	out = out.replace(/'/g,'')
+	out = out.replace(/\[|\]/g,'')
+	var lines = out.split('\n')
+	out = ''
+	var triggerList = {}
+	for (let i=0;i<lines.length;i++) {
+		if (lines[i] == '') continue
+	console.log('linesi',lines[i])
+		var temp = lines[i].split(':')
+		var trigger = temp[0]
+		var targets = temp[1]
+		var targetArray = targets.split(',')
+		
+		if (triggerList[trigger]) {
+			triggerList[trigger] = triggerList[trigger].concat(targetArray)
+			// remove redundancy in the list of targets
+			const uniqueSet = new Set(triggerList[trigger])
+			triggerList[trigger] = [...uniqueSet]
+			}
+		else triggerList[trigger] = targetArray
+		}
+	
+	for (item in triggerList) {
+		out += '"'+item+'": ['
+		for (let j=0;j<triggerList[item].length-1;j++) {
+			out += '"'+triggerList[item][j]+'"'
+			if (j<triggerList[item].length-2) out += ', '
+			}
+		out += '],\n'
+		}
+	
+	out = out.replace(/"",/g,'')
+	
 	document.getElementById('out').value = out
+	document.getElementById('out').select()
 	}
