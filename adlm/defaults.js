4c764e72891078ba103e38ff72ee088735aa3ce9
r12a <ishida@w3.org>
2019-08-06 20:33:17 +0100

    + Adlam

---
+++ adlm/defaults.js
@@ -0,0 +1,56 @@
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'Noto Sans Adlam WF' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "36";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "2"; // number representing number of times 100px for height of text area
+	factoryDefaults.lineheight = "1.4"; // number representing line height of text area in pixels/100
+	factoryDefaults.language = 'fuf' // language to use for examples
+	factoryDefaults.uifont = 'Noto Sans Adlam WF'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "28";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "";  // default base for combining characters
+	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
+	factoryDefaults.contrast = "low" // contrast for UI text colours
+
+
+var thisPicker = 'adlamPicker'
+
+if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
+else  defaults = factoryDefaults
+ 
+ 
+var template = {}
+	template.title = 'Adlam character app'
+	template.sample = "𞤑𞤵𞥅𞤤𞤢𞤤 𞤺𞤢𞤣𞤢𞤲𞤢𞤤 𞤋𞤲𞥆𞤢𞤥𞤢 𞤢𞥄𞤣𞤫𞥅𞤶𞤭 𞤬𞤮𞤬 𞤨𞤮𞤼𞤭، 𞤲ˈ𞤣𞤭𞤥𞤯𞤭𞤣𞤭 𞤫 𞤶𞤭𞤦𞤭𞤲𞤢𞤲𞥆𞤣𞤫 𞤼𞤮 𞤦𞤢𞤲𞥆𞤺𞤫 𞤸𞤢𞤳𞥆𞤫𞥅𞤶𞤭. 𞤉𞤩𞤫 𞤲ˈ𞤺𞤮𞥅𞤣𞤭 𞤥𞤭𞥅𞤶𞤮 𞤫 𞤸𞤢𞤳𞥆𞤭𞤤𞤢𞤲𞤼𞤢𞥄𞤺𞤢𞤤 𞤫𞤼𞤫 𞤫𞤩𞤫 𞤨𞤮𞤼𞤭 𞤸𞤵𞥅𞤬𞤮 𞤲ˈ𞤣𞤭𞤪𞤣𞤫 𞤫 𞤲ˈ𞤣𞤫𞤪 𞤩 𞤭𞤴𞤲𞤺𞤵𞤴𞤵𞤥𞥆𞤢𞥄𞤺𞤵."
+	template.sampleSource = 'UDHR https://unicode.org/udhr/d/udhr_fuf_adlm.html'
+    template.blocklocation= '/scripts/adlam/block'  // blocklocation to use for examples
+	template.direction = "rtl" // indicates whether this is a picker for a RTL script
+	template.github = 'adlm'
+	template.scriptcode = 'Adlm'
+	template.cursive = true
+
+
+
+
+var controls = [
+{"title":"Trans-<br/>literate", "alt":"Convert ajami text to a Latin transliteration.", "code":"doTranscription('transliterate')"},
+//{"title":"Transl<br/>++", "alt":"Convert ajami text to a latin transliteration with vowels and other changes.", "code":"doTranscription('translitPlus')"},
+]
+
+
+
+var pulldown = [
+]
+
+
+
+
+var inputAids = [
+{"title":"Shape-based lookup", "dataVar":"showShapeLookup", "dataLocn":"shapelist", "dataShortTitle":"S", "type":"shape", "desc":"Click on a panel of shapes to find similar characters."},
+
+{"title":"Hint at similar shapes", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint", "desc":"Show similar shapes when you mouse over a character"},
+
+{"title":"IME-like assisted typing", "dataVar":"showTranslit", "dataLocn":"transcriptionPalette", "dataShortTitle":"I", "type":"palette", "initialCode":"window.latinOnly=false;makePalette(translitCharacterMap);makeKbdEventList(translitCharacterMap);", "desc":"Use ASCII characters to type Hausa from the keyboard via reverse transliteration."},
+
+{"title":"Latin character input", "dataVar":"showLatinTrans", "dataLocn":"transcriptionPalette", "dataShortTitle":"L", "type":"palette", "initialCode":"window.latinOnly=true;makePalette(justLatinMap);makeKbdEventList(justLatinMap);", "desc":"Show Latin characters used for IPA or other transcriptions and transliterations."},
+]
+
---
+++ adlm/localtranscription.js
@@ -0,0 +1,117 @@
+﻿function localtranscribe (direction, str) {
+	
+	if (direction == 'transliterate') { return transliterate(str) }
+	if (direction == 'translitPlus') { return translitPlus(str) }
+	if (direction == 'toISO') { return transcribetoISO(str) }
+	if (direction == 'toLOC') { return transcribetoLOC(str) }
+	}
+
+
+
+function transliterate (str) {
+
+str = ' '+str
+
+var strArray = [...str]
+var exclusions = new Set(['(',')','[',']','.',' '])
+
+for (i=0;i<strArray.length;i++) {
+	if (exclusions.has(strArray[i])) continue
+	if (! autoTranslitArray[strArray[i]]) continue
+	re = new RegExp(strArray[i],'g')
+	str = str.replace(re, autoTranslitArray[strArray[i]])
+	}
+
+
+return str.trim()
+}
+
+
+
+
+
+
+function translitPlus (str) {
+
+var consonants = new Set(['ٻ', 'ݑ', 'پ', 'ط', 'ڟ', 'ق', 'ࢼ', 'ب', 'ت', 'د', 'ث', 'ك', 'ک', 'غ', 'ع', 'ف', 'ࢻ', 'پ', 'س', 'ص', 'ز', 'ذ', 'ظ', 'ش', 'ج', 'ه', 'ح', 'م', 'ن', 'ࢽ', 'و', 'ل', 'ض', 'ر', 'ی', 'ۑ', 'ؿ', 'ݣ', 'ࣃ', 'ࣄ' ])
+//var indvowels = new Set(['ं', 'ः', 'इ', 'ई', 'ॶ', 'ॷ', 'उ', 'ऊ', 'ऎ', 'ए', 'ॳ', 'ॴ', 'ऒ', 'ओ', 'अ', 'आ', 'ॵ', 'ऐ', 'औ' ])
+//var vowels = new Set(['ि', 'ी', 'ॖ', 'ॗ', 'ु', 'ू', 'ॆ', 'े', 'ऺ', 'ऻ', 'ॊ', 'ो', 'ा', 'ॏ', 'ै', 'ौ', ])
+
+str = ' '+str.normalize('NFC')+'        '
+
+// deal with shadda
+strArray = [...str].reverse()
+var shaddaFound = false
+for (let i=0;i<strArray.length;i++) {
+	if (strArray[i] === '\u0651') {
+		shaddaFound = true
+		strArray[i] = ''
+		}
+	else {
+		if (shaddaFound && consonants.has(strArray[i])) {
+			strArray[i] += strArray[i]
+			shaddaFound = false
+			}
+		}
+	}
+str = [...strArray].reverse().join('')
+
+// stop viramas being treated as combining characters
+// (otherwise they're difficult to extract from cchar sequences later)
+//str = str.replace(/\u094D/g,'V')
+
+
+// add inherent vowels
+//var withInherents = ''
+//for (let i=0; i<str.length-1; i++) {
+//console.log(str[i])
+//    if (consonants.has(str[i]) && consonants.has(str[i+1]) && vowels.has(str[i+2]) && str[i+3] === ' ') { console.log('hey');withInherents += str[i] } // penultimate syllable
+//	else if (consonants.has(str[i]) && (consonants.has(str[i+1]) || indvowels.has(str[i+1])))  withInherents += str[i]+'ʘ'
+//    else if (consonants.has(str[i]) && str[i+1] === '\u093C' && ((consonants.has(str[i+2]) || indvowels.has(str[i+2])))) { // nukta
+//        withInherents += str[i]+str[i+1]+'ʘ'
+//        i++
+//        }
+//	else  withInherents += str[i]
+//	}
+//str=withInherents
+
+
+str = ' '+transliterate(str)+'  '
+
+
+
+// convert translit letters to ipa letters
+// convert these to uppercase first, then lowercase all later, so there's no interference
+// nuktas
+str = str.replace(/waɑ/g, "WĀ")
+
+str = str.replace(/uwɑ͓/g, "ō")
+str = str.replace(/ıi˓/g, "ī")
+str = str.replace(/ıˈe/g, "ē")
+str = str.replace(/ˈeı/g, "ē")
+str = str.replace(/ɑ͑a/g, "a")
+str = str.replace(/ɑ̜i/g, "i")
+str = str.replace(/aɑ/g, "ā")
+str = str.replace(/uw/g, "ū")
+str = str.replace(/iı/g, "ī")
+str = str.replace(/ˈe/g, "ē")
+
+
+// remove sukun
+str = str.replace(/͓/g, "")
+
+// replace 0 for inherent vowel
+//str = str.replace(/ʘ/g, "ə")
+
+
+
+// add markup for ambiguous cases
+str = str.replace(/\[/g,'<span class=alts><span class=altfirst>')
+str = str.replace(/\|/g,'</span><span class=alt>')
+str = str.replace(/\{/g,'</span><span class=altlast>')
+str = str.replace(/\]/g,'</span></span>')
+
+return str.trim().toLowerCase()
+}
+
+
