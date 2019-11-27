8bdbcc095b8fa22bcc37bc5488057103bd1358bc
r12a <ishida@w3.org>
2019-11-27 10:21:04 +0000

    + shared25  Common code for new v25 apps

---
+++ shared25/setup.js
@@ -0,0 +1,81 @@
+
+function compareLength (a, b) {
+  var nameA = a.length; 
+  var nameB = b.length; 
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // names must be equal
+  return 0;
+}
+
+debug=true
+
+var sortIndex = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z','{','}','§','0','1','2','3','4','5','6','7','8','9','#','[',',','.',';',':','?','_',"'",'*','!','&','|',   '§', '±', '@', '$', '%', '^', '(', ')', '+', '-', '=', ']', '{', '}', '\'', '\\', '<', '>', ',', '/']
+
+
+var translitCharacterMap = ''
+
+
+function makeTranslitCharacterMap () {
+	// creates a string to be used for reverse translation and assigns it to translitCharacterMap
+	// that is then used elsewhere to set up the reverse translation panel
+	// takes its information from the spreadsheet
+    var charArray = {}
+    
+    // suck out the relevant data into the chars array
+    for (var line in spreadsheetRows) {
+		if (spreadsheetRows[line][cols.key] === '') continue
+        var primary = false
+		var keypress = spreadsheetRows[line][cols.key]
+		
+		// check for a marker that this pair should come first
+		if (keypress.includes('¶')) {
+            primary = true
+            keypress = keypress.replace('¶', '')
+            }
+
+		// create entry for the item
+        if (charArray[keypress]) {
+            if (primary) charArray[keypress] = charArray[keypress].replace(keypress, keypress+' '+spreadsheetRows[line][cols.transLoc]+' '+line)
+            else charArray[keypress] += ' '+spreadsheetRows[line][cols.transLoc]+' '+line
+            }
+        else {
+            charArray[keypress] = keypress+' '+spreadsheetRows[line][cols.transLoc]+' '+line
+            }
+		
+		}
+	//console.log(charArray)
+    
+	//for (item in charArray) translitCharacterMap += item+('\n')
+	for (let i=0;i<window.sortIndex.length;i++) {
+		if (charArray[window.sortIndex[i]]) translitCharacterMap += charArray[window.sortIndex[i]]+'\n'
+		}
+
+ 	console.log(translitCharacterMap)
+	}
+
+
+
+var autoTranslitArray = {}
+
+function makeAutoTranslitArray () {
+	// creates an array of correlated items that will be used by the transliteration control
+	// takes the data from the spreadsheet
+	
+    
+    // suck out the relevant data into the chars array
+    for (var line in spreadsheetRows) {
+		if (spreadsheetRows[line][cols.transLoc] === '') continue		
+			
+        // create entry for the item
+        if (autoTranslitArray[line]) console.log('Unexpected duplicate in makeTransliterationArray: ',line)
+        else autoTranslitArray[line] = spreadsheetRows[line][cols.transLoc]
+		
+		}
+	}
+
