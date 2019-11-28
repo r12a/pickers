37080f3f0c4eaff4cfcffc4042fffe497f6d4225
r12a <ishida@w3.org>
2019-11-28 04:40:55 +0000

    latn-ha, Change default font to Noto Sans

--- latn-ha/defaults.js
+++ latn-ha/defaults.js
@@ -1,12 +1,12 @@
 var defaults = {}
 var factoryDefaults = {}
-	factoryDefaults.font = 'Gentium Plus' // text area font name (a single font, no quotes)
-	factoryDefaults.size = "36";  // text area font size, number of pixels (just the number)
+	factoryDefaults.font = 'Noto Sans Light WF' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "28";  // text area font size, number of pixels (just the number)
 	factoryDefaults.rows = "2"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "1.5"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'ha' // language to use for examples
-	factoryDefaults.uifont = 'Gentium Plus'; // font name  for selection area (a single font, no quotes)
-	factoryDefaults.uisize = "28";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.uifont = 'Noto Sans Light WF'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "24";  // selection area font size, number of pixels (just the number)
 	factoryDefaults.ccbase = "-";  // default base for combining characters
 	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
 	factoryDefaults.contrast = "low" // contrast for UI text colours
