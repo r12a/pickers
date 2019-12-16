d5f7ba4ef8c0d30fdd73b00783c0f6f35a14209b
r12a <ishida@w3.org>
2019-12-16 13:00:19 +0000

    template update

--- _template_ltr/defaults.js
+++ _template_ltr/defaults.js
@@ -39,6 +39,7 @@ var controls = [
 
 
 var pulldown = [
+{"title":"Reverse<br/>transliterate", "alt":"Convert a Latin transliteration to Javanese text.", "code":"doTranscription('revTransliterate')"},
 ]
 
 
