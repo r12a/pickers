4cf75eaeedfe95961ee9bfc4d49e22e5fae4a2c5
r12a <ishida@w3.org>
2020-01-12 09:42:50 +0000

    + geok-ka

---
+++ geok-ka/defaults.js
@@ -0,0 +1,61 @@
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'BGP 2017 DejaVu Serif WF' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "30";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "2"; // number representing number of times 100px for height of text area
+	factoryDefaults.lineheight = "1.5"; // number representing line height of text area in pixels/100
+	factoryDefaults.language = 'ka' // language to use for examples
+	factoryDefaults.uifont = 'BGP 2017 DejaVu Sans WF'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "28";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "";  // default base for combining characters
+	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
+	factoryDefaults.contrast = "low" // contrast for UI text colours
+
+
+var thisPicker = 'khutsuri'
+
+if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
+else  defaults = factoryDefaults
+ 
+ 
+var template = {}
+	template.title = 'Khutsuri character app'
+	template.sample = "მუხლი 1. ყველა ადამიანი იბადება თავისუფალი და თანასწორი თავისი ღირსებითა და უფლებებით. მათ მინიჭებული აქვთ გონება და სინდისი და ერთმანეთის მიმართ უნდა იქცეოდნენ ძმობის სულისკვეთებით."
+	// source: https://unicode.org/udhr/d/udhr_kat.html
+	template.blocklocation= '/scripts/georgian/block'  // blocklocation to use for examples
+	template.direction = "ltr" // indicates whether this is a picker for a RTL script
+	template.github = 'georgian'
+	template.scriptcode = 'Geok'
+
+
+
+var controls = [
+{"title":"Trans-<br/>literate", "alt":"Convert Georgian Khutsuri text to a Latin transliteration.", "code":"doTranscription('transliterate')"},
+]
+
+
+
+var pulldown = [
+{"title":"Reverse<br/>transliterate", "alt":"Convert a Latin transliteration to Khutsuri text.", "code":"doTranscription('revTransliterate')"},
+
+{"title":"Georgian to<br/>National Tr", "alt":"Convert Khutsuri text to a national Georgian transcription.", "code":"doTranscription('toNational')"},
+]
+
+
+
+var inputAids = [
+//{"title":"Shape-based lookup", "dataVar":"showShapeLookup", "dataLocn":"shapelist", "dataShortTitle":"S", "type":"shape", "desc":"Click on a panel of shapes to find similar characters."},
+
+{"title":"Hint at similar shapes", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint", "desc":"Show similar shapes as you mouse over a character."},
+
+{"title":"Type assist", "dataVar":"typeAssist", "dataLocn":"transcriptionPalette", "dataShortTitle":"T", "type":"palette", "initialCode":"makePalette(''); window.latinOnly=false;makeKbdEventList(translitCharacterMap);", "desc":"Use ASCII characters to type Georgian from the keyboard."},
+
+{"title":"Latin type-assist", "dataVar":"showLatinTrans", "dataLocn":"transcriptionPalette", "dataShortTitle":"L", "type":"palette", "initialCode":"window.latinOnly=true;makePalette(justLatinMap);makeKbdEventList(justLatinMap);", "desc":"Show characters needed for IPA or other transcriptions and transliterations."},
+
+{"title":"Georgian Natl transliteration", "dataVar":"showNationalTrans", "dataLocn":"transcriptionPalette", "dataShortTitle":"G", "type":"palette", "initialCode":"window.latinOnly=false;makePalette(nationalCharacterMap);makeKbdEventList(nationalCharacterMap);", "desc":"Create Georgian text from characters in the Georgian National transcription."},
+
+{"title":"Reverse transliteration", "dataVar":"showTranslit", "dataLocn":"transcriptionPalette", "dataShortTitle":"R", "type":"palette", "initialCode":"window.latinOnly=false;makePalette(translitCharacterMap);makeKbdEventList(translitCharacterMap);", "desc":"Use ASCII characters to type Georgian from the keyboard via reverse transliteration."},
+
+{"title":"Keyboard", "dataVar":"showKeyboard", "dataLocn":"keyboard", "dataShortTitle":"K", "type":"keyboard", "desc":"Select characters from a keyboard layout."}
+]
+
---
+++ geok-ka/localcode.js
@@ -0,0 +1,194 @@
+globals.showTranslit =  ''
+globals.typeAssist = ' ✓'
+globals.showKeysTranslitToggle = true
+
+
+function localInitialise () {
+	}
+
+
+
+
+
+
+globals.showNationalTrans = ''
+
+var nationalCharacterMap = `
+a a ⴀ a ა
+A a Ⴀ a Ა
+b b ⴁ b ბ
+B b Ⴁ b Ბ
+c ch ⴙ ch' ⴝ ch ჩ ch' ჭ
+C ch Ⴙ ch' Ⴝ ch Ჩ ch' Ჭ
+d d ⴃ dz ⴛ d დ dz ძ
+D d Ⴃ dz Ⴛ d Დ dz Ძ
+e e ⴄ e ე
+E e Ე
+g g ⴂ gh ⴖ g გ gh ღ
+G g Ⴂ gh Ⴖ g Გ gh Ღ
+h h ⴠ h ჰ
+H h Ⴠ h Ჰ
+i i ⴈ i ი
+I i Ⴈ i Ი
+j j ⴟ j ჯ
+J j Ⴟ j Ჯ
+k k' ⴉ k ⴕ kh ⴞ k' კ k ქ kh ხ
+K k' Ⴉ k Ⴕ kh Ⴞ k' Კ k Ქ kh Ხ
+l l ⴊ l ლ
+L l Ⴊ l Ლ
+m m ⴋ m მ
+M m Ⴋ m Მ
+n n ⴌ n ნ
+N n Ⴌ n Ნ
+o o ⴍ p ⴔ o ო
+O o Ⴍ o Ო
+p p' ⴎ p' პ p ფ
+P p' Ⴎ p Ⴔ p' Პ p Ფ
+q q' ⴗ q' ყ
+Q q' Ⴗ q' Ყ
+r r ⴐ r რ
+R e Ⴄ r Ⴐ r Რ
+s s ⴑ sh ⴘ s ს sh შ
+S s Ⴑ sh Ⴘ s Ს sh Შ
+t t ⴇ t' ⴒ ts ⴚ ts' ⴜ t თ t' ტ ts ც ts' წ
+T t Ⴇ t' Ⴒ ts Ⴚ ts' Ⴜ t Თ t' Ტ ts Ც ts' Წ
+u u ⴓ u უ
+U u Ⴓ u Უ
+v v ⴅ v ვ
+V v Ⴅ v Ვ
+z z ⴆ zh ⴏ z ზ zh ჟ
+Z z Ⴆ zh Ⴏ z Ზ zh Ჟ
+`
+
+
+
+var _h = {
+"2D0A": ["2D07", "2D1C", "2D05", "2D10", "2D13", "2D0D", "2D20"],
+"2D07": ["2D1C", "2D05", "2D10", "2D13", "2D0D", "2D20", "2D0A"],
+"2D1C": ["2D05", "2D10", "2D13", "2D0D", "2D20", "2D0A", "2D07"],
+"2D05": ["2D10", "2D13", "2D0D", "2D20", "2D0A", "2D07", "2D1C"],
+"2D10": ["2D13", "2D0D", "2D20", "2D0A", "2D07", "2D1C", "2D05"],
+"2D13": ["2D0D", "2D20", "2D0A", "2D07", "2D1C", "2D05", "2D10"],
+"2D0D": ["2D20", "2D0A", "2D07", "2D1C", "2D05", "2D10", "2D13"],
+"2D20": ["2D0A", "2D07", "2D1C", "2D05", "2D10", "2D13", "2D0D",  "2D04", "2D1A", "2D19"],
+"2D0E": ["2D11", "2D18", "2D01"],
+"2D11": ["2D18", "2D01", "2D0E"],
+"2D18": ["2D01", "2D0E", "2D11"],
+"2D01": ["2D0E", "2D11", "2D18"],
+"2D19": ["2D16", "2D20", "2D04", "2D1A"],
+"2D16": ["2D20", "2D04", "2D1A", "2D19"],
+"2D04": ["2D1A", "2D19", "2D16", "2D20"],
+"2D1A": ["2D19", "2D16", "2D20", "2D04"],
+"2D0B": ["2D1B"],
+"2D1B": ["2D0B"],
+"10BC": ["10B2"],
+"10B2": ["10BC"],
+"10B7": ["10AF", "10A1"],
+"10AF": ["10A1", "10B7"],
+"10A1": ["10B7", "10AF"],
+"10A6": ["10B1"],
+"10B1": ["10A6"],
+"10A5": ["10A8", "10A4", "10A2"],
+"10A8": ["10A4", "10A2", "10A5"],
+"10A4": ["10A2", "10A5", "10A8"],
+"10A2": ["10A5", "10A8", "10A4"],
+"10B3": ["10AD", "10A3"],
+"10AD": ["10A3", "10B3"],
+"10A3": ["10B3", "10AD"],
+
+"c2D0A": ["c2D07", "c2D1C", "c2D05", "c2D10", "c2D13", "c2D0D", "c2D20"],
+"c2D07": ["c2D1C", "c2D05", "c2D10", "c2D13", "c2D0D", "c2D20", "c2D0A"],
+"c2D1C": ["c2D05", "c2D10", "c2D13", "c2D0D", "c2D20", "c2D0A", "c2D07"],
+"c2D05": ["c2D10", "c2D13", "c2D0D", "c2D20", "c2D0A", "c2D07", "c2D1C"],
+"c2D10": ["c2D13", "c2D0D", "c2D20", "c2D0A", "c2D07", "c2D1C", "c2D05"],
+"c2D13": ["c2D0D", "c2D20", "c2D0A", "c2D07", "c2D1C", "c2D05", "c2D10"],
+"c2D0D": ["c2D20", "c2D0A", "c2D07", "c2D1C", "c2D05", "c2D10", "c2D13"],
+"c2D20": ["c2D0A", "c2D07", "c2D1C", "c2D05", "c2D10", "c2D13", "c2D0D",  "c2D04", "c2D1A", "c2D19"],
+"c2D0E": ["c2D11", "c2D18", "c2D01"],
+"c2D11": ["c2D18", "c2D01", "c2D0E"],
+"c2D18": ["c2D01", "c2D0E", "c2D11"],
+"c2D01": ["c2D0E", "c2D11", "c2D18"],
+"c2D19": ["c2D16", "c2D20", "c2D04", "c2D1A"],
+"c2D16": ["c2D20", "c2D04", "c2D1A", "c2D19"],
+"c2D04": ["c2D1A", "c2D19", "c2D16", "c2D20"],
+"c2D1A": ["c2D19", "c2D16", "c2D20", "c2D04"],
+"c2D0B": ["c2D1B"],
+"c2D1B": ["c2D0B"],
+"c10BC": ["c10B2"],
+"c10B2": ["c10BC"],
+"c10B7": ["c10AF", "c10A1"],
+"c10AF": ["c10A1", "c10B7"],
+"c10A1": ["c10B7", "c10AF"],
+"c10A6": ["c10B1"],
+"c10B1": ["c10A6"],
+"c10A5": ["c10A8", "c10A4", "c10A2"],
+"c10A8": ["c10A4", "c10A2", "c10A5"],
+"c10A4": ["c10A2", "c10A5", "c10A8"],
+"c10A2": ["c10A5", "c10A8", "c10A4"],
+"c10B3": ["c10AD", "c10A3"],
+"c10AD": ["c10A3", "c10B3"],
+"c10A3": ["c10B3", "c10AD"],
+
+end: {}
+}
+
+
+
+
+var keyboarddef = [
+"§ „  “|1 !|2 ?|3 №|4 §|5 %|6 :|7 .|8 ;|9 ,|0 /|-|=",
+"q ღ|w ჯ|e უ|r კ|t ე ჱ|y ნ|u გ|i შ|o წ|p ზ|[ ხ ჴ|] ც",
+"a ფ ჶ|s ძ|d ვ ჳ|f თ|g ა|h პ|j რ|k ო|l ლ|; დ|' ჟ|\\",
+"`|z ჭ|x ჩ|c ყ|v ს|b მ|n ი ჲ|m ტ|, ქ|. ბ|/ ჰ ჵ"
+]
+
+
+/*  FOR KEYBOARDS WITH DIFFERENT CHARS ON SHIFT LEVEL
+var keyboardUC = [
+"§|1|2|3|4|5|6|7|8|9|0|-|=",
+"q|w|e|r|t උ|y|u|i|o|p|[|]",
+"a ඒ ඌ ඖ|s|d ො ඎ ෲ|f|g|h|j|k|l|;|'|\\",
+"`|z|x|c|v|b|n|m|,|.|/"
+]
+
+
+function event_toggleKbdShift () {
+    if (this.classList.contains('shiftKeyOn')) {
+        makeKeyboard(keyboarddef, keyboardLCGuide)
+        }
+    else makeKeyboard(keyboardUC, keyboardUCGuide)
+    }
+*/
+
+
+function toggleShift (node, LC, UC) {
+    var chars = document.querySelectorAll('.c')
+
+    if (node.className =='unshifted') {
+        for (let i=0;i<chars.length;i++) {
+            chars[i].textContent = chars[i].textContent.toLowerCase()
+            }
+		node.className = 'shifted'
+		} 
+	else {
+        for (let i=0;i<chars.length;i++) {
+            chars[i].textContent = chars[i].textContent.toUpperCase()
+            }
+		node.className = 'unshifted'
+		}
+	// reinitialise ids to codepoint values of character sequence (with no leading zeros)
+	node = document.querySelectorAll( '.c' ); 
+	for (var n = 0; n < node.length; n++ ) { 
+		content = node[n].textContent
+		id=''
+		for (i=0;i<content.length;i++) {
+			id += convertChar2CP(content[i])
+			}
+		node[n].id = id
+        node[n].dataset.c = 'c'+id
+        dec = parseInt(id, 16)
+        while (id.length<4) id = '0'+id
+        node[n].title = 'U+'+id+': '+charData[content]
+		}
+	}
+
