145c4818e2c3441a17ff81b07a0ee01a805d4a0e
r12a <ishida@w3.org>
2020-01-29 09:00:13 +0000

    + telugu

---
+++ telu/defaults.js
@@ -0,0 +1,56 @@
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'Noto Sans Telugu WF' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "40";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "2"; // number representing number of times 100px for height of text area
+	factoryDefaults.lineheight = "1.4"; // number representing line height of text area in pixels/100
+	factoryDefaults.language = 'te' // language to use for examples
+	factoryDefaults.uifont = 'Noto Sans Telugu WF'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "28";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "";  // default base for combining characters
+	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
+	factoryDefaults.contrast = "low" // contrast for UI text colours
+	factoryDefaults.hints = "none" // type of hint to show alongside characters in the grids
+
+
+var thisPicker = 'teluguPicker'
+
+if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
+else  defaults = factoryDefaults
+ 
+ 
+var template = {}
+	template.title = 'Telugu character app'
+	template.sample = "ప్రతిపత్తిస్వత్వముల విషయమున మానవులెల్లరును జన్మతః స్వతంత్రులును సమానులును నగుదురు."
+	template.blocklocation= '/scripts/telugu/block'  // blocklocation to use for examples
+	template.direction = "ltr" // indicates whether this is a picker for a RTL script
+	template.github = 'telugu'
+	template.scriptcode = 'Telu'
+
+
+
+var controls = [
+{"title":"Trans-<br/>literate", "alt":"Convert Telugu text to a latin transliteration.", "code":"doTranscription('transliterate')"},
+]
+
+
+
+var pulldown = [
+{"title":"Reverse<br/>transliterate", "alt":"Convert a Latin transliteration to Telugu text.", "code":"doTranscription('revTransliterate')"},
+]
+
+
+
+var inputAids = [
+{"title":"Shape-based lookup", "dataVar":"showShapeLookup", "dataLocn":"shapelist", "dataShortTitle":"S", "type":"shape", "desc":"Click on a panel of shapes to find similar characters."},
+
+{"title":"Hint at similar shapes", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint", "desc":"Show similar shapes as you mouse over a character."},
+
+{"title":"Type assist", "dataVar":"typeAssist", "dataLocn":"transcriptionPalette", "dataShortTitle":"T", "type":"palette", "initialCode":"makePalette(''); window.latinOnly=false;makeKbdEventList(translitCharacterMap);", "desc":"Use ASCII characters to type Telugu from the keyboard."},
+
+{"title":"Latin type-assist", "dataVar":"showLatinTrans", "dataLocn":"transcriptionPalette", "dataShortTitle":"L", "type":"palette", "initialCode":"window.latinOnly=true;makePalette(justLatinMap);makeKbdEventList(justLatinMap);", "desc":"Show characters needed for IPA or other transcriptions and transliterations."},
+
+//{"title":"ISO to Hindi", "dataVar":"showISOCharMap", "dataLocn":"transcriptionPalette", "dataShortTitle":"I", "type":"palette", "initialCode":"window.latinOnly=false;makePalette(isoCharacterMap);makeKbdEventList(isoCharacterMap);", "desc":"Create XXXX text from characters in the XXXX transcription."},
+
+{"title":"Reverse transliteration", "dataVar":"showTranslit", "dataLocn":"transcriptionPalette", "dataShortTitle":"R", "type":"palette", "initialCode":"window.latinOnly=false;makePalette(translitCharacterMap);makeKbdEventList(translitCharacterMap);", "desc":"Use ASCII characters to type Telugu from the keyboard via reverse transliteration."},
+]
