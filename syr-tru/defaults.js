37e11b8693e949cd79475bc8af189bff0f0f3631
r12a <ishida@w3.org>
2020-03-11 10:59:19 +0000

    + Surayt picker

---
+++ syr-tru/defaults.js
@@ -0,0 +1,57 @@
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'Serto Jerusalem WF' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "50";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "2"; // number representing number of times 100px for height of text area
+	factoryDefaults.lineheight = "1.2"; // number representing line height of text area in pixels/100
+	factoryDefaults.language = 'tru' // language to use for examples
+	factoryDefaults.uifont = 'Serto Jerusalem WF'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "40";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "";  // default base for combining characters
+	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
+	factoryDefaults.contrast = "low" // contrast for UI text colours
+	factoryDefaults.hints = "none" // type of hint to show alongside characters in the grids
+
+
+var thisPicker = 'turoyoPicker'
+
+if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
+else  defaults = factoryDefaults
+ 
+ 
+var template = {}
+	template.title = 'Surayt character picker'
+	template.sample = "ܐܘ ܢܳܫܐ ܟܷܬܠܶܗ ܚܰܡܫܐ ܪܷܓ݂ܫܶܐ: ܚܙܳܝܐ ܒܰܥ ܥܰܝܢܶܐ، ܫܡܳܥܐ ܒܰܢ ܐܰܕ݂ܢܶܐ، ܢܩܳܚܐ ܒܘ ܢܚܝܪܐ، ܓܝܳܫܐ ܒܰܢ ܐܝܕ݂ܶܐ ܘܛܥܳܡܐ ܒܘ ܠܝܫܳܢܐ."
+	template.blocklocation= '/scripts/syriac/block'  // blocklocation to use for examples
+	template.direction = "rtl" // indicates whether this is a picker for a RTL script
+	template.github = 'syr-tru'
+	template.scriptcode = 'Syrc'
+	template.cursive = true
+
+
+
+var controls = [
+{"title":"Trans-<br/>literate", "alt":"Convert Surayt text to a Latin transliteration.", "code":"doTranscription('transliterate')"},
+//{"title":"Trans-<br/>scribe", "alt":"Convert Assyrian text to a Latin transcription.", "code":"doTranscription('transcription')"},
+//{"title":"Transcribe<br/>to IPA", "alt":"Convert Assyrian text to an *approximate* phonetic transcription.", "code":"doTranscription('toIPA')"},
+]
+
+
+
+var pulldown = [
+]
+
+
+
+var inputAids = [
+//{"title":"Shape-based lookup", "dataVar":"showShapeLookup", "dataLocn":"shapelist", "dataShortTitle":"S", "type":"shape", "desc":"Click on a panel of shapes to find similar characters."},
+
+//{"title":"Hint at similar shapes", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint", "desc":"Show similar shapes as you mouse over a character."},
+
+{"title":"Type assist", "dataVar":"typeAssist", "dataLocn":"transcriptionPalette", "dataShortTitle":"T", "type":"palette", "initialCode":"setUpTypeAssist(false, '', typeAssistMap)", "desc":"Use ASCII characters to type Assyrian Neo-Aramaic from the keyboard."},
+
+{"title":"Latin type-assist", "dataVar":"showLatinTrans", "dataLocn":"transcriptionPalette", "dataShortTitle":"L", "type":"palette", "initialCode":"setUpTypeAssist(true, latinTypeAssistMap, latinTypeAssistMap)", "desc":"Show characters needed for IPA or other transcriptions and transliterations."},
+
+{"title":"Reverse transliteration", "dataVar":"showTranslit", "dataLocn":"transcriptionPalette", "dataShortTitle":"R", "type":"palette", "initialCode":"setUpTypeAssist(false, typeAssistMap, typeAssistMap)", "desc":"Use ASCII characters to type Surayt from the keyboard via reverse transliteration."},
+]
+
---
+++ syr-tru/localcode.js
@@ -0,0 +1,190 @@
+globals.showLOCTrans = ''
+globals.showCursive = ''
+globals.typeAssist = ' ✓'
+globals.showKeysTranslitToggle = false
+
+
+
+
+function localInitialise () {
+
+	}
+
+
+var _h = {
+"62A": ['646'],
+"646": ['62A'],
+"62F": ['631'],
+"631": ['62F'],
+"642": ['6CB'],
+"698": ['6CB'],
+"6CB": ['698','642'],
+"643": ['6AD'],
+"6AD": ['643'],
+"64A": ['6D0','64964A','6266D0'],
+"6D0": ['64A','64964A'],
+"64964A": ['64A','6D0'],
+'626649': ['6266D0'],
+'6266D0': ['626649'],
+'6266C7': ['626648'],
+'626648': ['6266C7'],
+"665": ['6D5'],
+"6D5": ['665'],
+end: {}
+}
+
+
+var keyboarddef = [
+"\u00A7 \u070F \u032E|1 !|2 \u030A \u0702|3 \u0325 \u0703|4 \u0749 \u0704|5 \u2670 \u0705|6 \u2671 \u0708|7 \u070A \u0709|8 » \u070B|9( \u070C|0 )|- « \u070D|= +",
+"q \u0729 \u0730 \u064E|w \u0718 \u0733 \u064B|e \u0716 \u0736 \u064F|r \u072A \u073A \u064C|t \u072C \u073D \u0653|y \u071D \u0738 \u0654|u \u071B \u0304 \u0748|i \u0725 \u0705 \u0703|o \u0727 \u0304 \u0745|p \u0726 \u0741 \u032D|[ \u0303|] \u074A",
+"a \u0710 \u0731 \u0650|s \u0723 \u0734 \u064D|d \u0715 \u0737|f \u0714 \u073B \u0621|g \u0713 \u073E \u0655|h \u0717 \u0711 \u0670|j \u071B \u0640 \u0748|k \u071F \u0740 \u0704|l \u0720 \u0320 \u0746|; \u071A \u0742 \u2026|' \u071E \u0330|\\ \u0706 :",
+"`|z \u0719 \u0732|x \u0728 \u0735|c \u0724 \u0738|v \u072B \u073C|b \u0712 \u073F|n \u0722 \u0739|m \u0721 .|, \u0700 \u060C|.  \u061B|/ \u0707 \u061F"]
+
+
+
+var rightOnly = new Set(['ܐ', 'ܕ', 'ܗ', 'ܞ', 'ܘ', 'ܙ', 'ܩ', 'ܪ', 'ܨ', 'ܬ'])
+
+var bothJoin = new Set(['ܒ', 'ܓ', 'ܚ', 'ܛ', 'ܝ', 'ܟ', 'ܠ', 'ܡ', 'ܢ', 'ܣ', 'ܤ', 'ܥ', 'ܦ', 'ܫ'])
+
+
+function event_mouseoverCharXX ()  {
+	// overrides the function in shared20/code.js to add shaping forms for all arabic styles
+	
+	// add cursive forms to table
+    if (this.id) {
+        var cursive = ''
+        var ncr = ''
+        if (this.id.length < 5) ncr = String.fromCodePoint('0x'+this.id)
+        if (window.rightOnly.has(ncr)) cursive = 'ܝ'+ncr+' '+ncr
+        else if (bothJoin.has(ncr)) cursive = ncr+'ܝ'+ncr+'ܝ'+ncr+' '+ncr
+        document.getElementById('cursive').innerHTML = cursive
+        }
+	
+	
+	// display character information
+	var span = document.createElement( 'span' );
+	span.setAttribute( 'id', 'charname' );
+	var charinfo = document.createTextNode( this.title );
+	span.appendChild(charinfo);
+	
+	var chardata = document.getElementById('chardata');	
+	chardata.replaceChild( span, chardata.firstChild );
+	
+	// highlight this character
+	this.style.backgroundColor = '#CF9'
+	this.style.backgroundColor = '#fc6'
+		this.style.backgroundColor = '#F4630B';
+		this.style.color = '#eee'
+	
+	// highlight similar characters
+	if (globals.showShapeHints && _h[this.id]) {
+		ptr = this.id
+		for (i=0;i<_h[ptr].length;i++) {
+			document.getElementById(_h[ptr][i]).style.backgroundColor = '#FFE6B2'
+			}
+		}
+	}
+
+
+
+function event_mouseoverCharXX ()  {
+	// overrides the function in shared20/code.js to add shaping forms for all arabic styles
+	
+	// add cursive forms to table
+    if (this.id) {
+        var cursive = ''
+        var ncr = ''
+        ncr = this.textContent
+        if (window.rightOnly.has(ncr)) cursive = 'ܝ'+ncr+' '+ncr
+        else if (bothJoin.has(ncr)) cursive = ncr+'ܝ'+ncr+'ܝ'+ncr+' '+ncr
+        document.getElementById('cursive').innerHTML = cursive
+        }
+	
+	
+	// display character information
+	var span = document.createElement( 'span' );
+	span.setAttribute( 'id', 'charname' );
+	var charinfo = document.createTextNode( this.title );
+	span.appendChild(charinfo);
+	
+	var chardata = document.getElementById('chardata');	
+	chardata.replaceChild( span, chardata.firstChild );
+	
+	// highlight this character
+	this.style.backgroundColor = '#CF9'
+	this.style.backgroundColor = '#fc6'
+		this.style.backgroundColor = '#F4630B';
+		this.style.color = '#eee'
+	
+	// highlight similar characters
+	if (globals.showShapeHints && _h[this.id]) {
+		ptr = this.id
+		for (i=0;i<_h[ptr].length;i++) {
+			document.getElementById(_h[ptr][i]).style.backgroundColor = '#FFE6B2'
+			}
+		}
+	}
+
+
+
+globals.showIPATrans = ''
+
+var ipaCharacterMap = `
+a a ܲ ɑ ܵ a ܰ a ܱ ɑ ܳ ɑ ܴ a \u0735\u0710\b a \u0732\u0710\b ɑ ܐ
+b b ܒ
+d d ܕ ð ܕ݂ ʤ ܓ̰
+e ɛ ܸ e ܹ ɛ ܶ ɛ ܷ e ܸ e ܶ e ܷ
+f f ܦ݂
+g g ܓ
+h h ܗ ħ ܚ
+i i ܼ i ܺ i ܻ ɪ \u0738\u0710\b i ܝܼ ɪ ܸ
+j jʰ ܞ
+k k ܟ
+l l ܠ
+m m ܡ
+n n ܢ
+o o ܿ o ّ o ܘܿ
+p p ܦ
+q q ܩ
+r r ܪ
+s s ܣ sˤ ܨ ʃ  ܫ ṡ ܤ
+t tˤ ܛ t ܬ ʧ ܟ̃ ʧ ܟ݂
+u u ܽ u ܾ u ܘܼ
+v v ܒ݂
+w w ܘ
+x x ܟ݂
+y j ܝ ɣ ܓ݂
+z z ܙ ʒ ܙ̃ ʒ ܙ̰ ʒ ܫ̰ ʒ ܫ̃
+{ ʕ ܥ
+} ʔ ܐ
+[ ̋ ̈ ʺ ݀ ¶ ܀
+, , ،
+. . . . ܁ . ܂ . ܊
+; ; ؛ ; ܈
+: : ܃ : ܄ : ܅ : ܆ : ܇
+? ? ؟ ? ܉
+`
+
+
+/*
+
+' ʾ ʔ ʿ ʕe
+b b b ḇ w
+c c ʔ c ʕ č ʧ
+d d d ḏ ð
+g g ɡ ḡ ɟ ḡ ʔ ḡ ɣ ḡ j ǧ ʤ
+h h h ḥ x
+k k cʰ ḵ x
+p p pʰ ꝑ f q q
+s s s ṣ s š ʃ
+t t tʰ t t ṯ θ ṭ t
+w w w w u
+y y i y e y j
+z z z ž ʒ
+a ă a a a aw o ay e
+i ĭ i î i 
+e e ɪ
+u û u
+o ô u
+`
+*/
