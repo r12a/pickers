b78bc28307d4e1811d3e709c900d864d449984af
r12a <ishida@w3.org>
2020-05-26 07:15:51 +0100

    khmer/localtranscription, Code tidy

--- khmer/localtranscription.js
+++ khmer/localtranscription.js
@@ -7,7 +7,7 @@ function localtranscribe (direction, str) {
 	}
 		
 
-
+var debug = false
 
 
 function transliterate (str) {
@@ -958,179 +958,194 @@ return str.trim()
 
 
 
+
+
+const bantok = '\u17cb'
+const robat = '\u17cc'
+const muusikoatan = '\u17c9'
+const triisap = '\u17ca'
+const nikahit = '\u17c6'
+const reahmuk = '\u17c7'
+const coeng = '\u17d2'
+const yuukaleapintu = '\u17c8'
+const virama = '\u17d1'
+const kakabat = '\u17ce'
+const toandakhiat = '\u17cd'
+const ahsda  = '\u17cf'
+
+
+
+
 function convertToIPA ( syllable ) {
+// syllable is an object
 
-// kill vowel after final if syllable is longer than 1 character and ends in consonant
-if (syllable.length > 1 && consonants.has(syllable[syllable.length-1])) syllable += 'u17D2'
+	// kill vowel after final if syllable is longer than 1 character and ends in consonant
+	if (syllable.length > 1 && consonants.has(syllable[syllable.length-1])) syllable += coeng
 
-	var outstr = '';
-	var series = 1;
-	var labials = { '\u1794':'', '\u1795':'', '\u1796':'', '\u1797':'', '\u1798':'' };
-	var velars = { '\u1780':'', '\u1781':'', '\u1782':'', '\u1783':'', '\u1784':'' };
-	var nasals = {  '\u1798':'', '\u1793':'', '\u1789':'', '\u1784':'', '\u178E':'' };
+	var outstr = ''
+	var series = 1
+	var labials = new Set(['\u1794', '\u1795', '\u1796', '\u1797', '\u1798'])
+	var velars = new Set(['\u1780', '\u1781', '\u1782', '\u1783', '\u1784'])
+	var nasals = new Set(['\u1798', '\u1793', '\u1789', '\u1784', '\u178E'])
 
-	if (syllable.robat) { outstr += 'rə'; }
+	if (syllable.robat) outstr += 'rə'
 	switch (syllable.initialcons.charAt(0) ) { // Initial consonant
 		case 'ក':  // ka
-			outstr += 'k'; 
-				if (syllable.initialcons.length > 1 && (syllable.initialcons[1] == '\u1793'  || syllable.initialcons[1]  == '\u1798' )) { outstr += 'ʰ'; }
-			series = 1;
-			break;
+			outstr += 'k'
+			if (syllable.initialcons.length > 1 && (syllable.initialcons[1] == 'ន'  || syllable.initialcons[1]  == 'ម' )) outstr += 'ʰ'
+			series = 1
+			break
 		case 'ខ':  // kha
-			outstr += 'k'; 
-			if (syllable.initialcons.length == 1 ) { outstr += 'ʰ'; }
-			series = 1;
-			break;
+			outstr += 'k'
+			if (syllable.initialcons.length == 1 ) outstr += 'ʰ'
+			series = 1
+			break
 		case 'គ':  // ko
-			outstr += 'k'; 
-			if (syllable.initialcons.length > 1 && (syllable.initialcons[1] == '\u1793'  || syllable.initialcons[1]  == '\u1798' )) { outstr += 'ʰ'; }
-			series = 2;
-			break;
+			outstr += 'k'
+			if (syllable.initialcons.length > 1 && (syllable.initialcons[1] == 'ន'  || syllable.initialcons[1]  == 'ម' )) outstr += 'ʰ'
+			series = 2
+			break
 		case 'ឃ':  // kho
-			outstr += 'k'; 
-			if (syllable.initialcons.length == 1 ) { outstr += 'ʰ'; }
-			series = 2;
-			break;
+			outstr += 'k' 
+			if (syllable.initialcons.length == 1 ) outstr += 'ʰ'
+			series = 2
+			break
 		case 'ង':  // ngo
-			outstr += 'ŋ'; 
-			if (!vowelHarmony) { series = 2; }
-			else { series = vowelHarmony; }
-			break;
+			outstr += 'ŋ'
+			if (!vowelHarmony) series = 2
+			else series = vowelHarmony
+			break
 		case '\u1785':  // ca
-			outstr += 'c'; 
-			series = 1;
-			break;
+			outstr += 'c'
+			series = 1
+			break
 		case '\u1786':  // cha
-			outstr += 'c'; 
-			if (syllable.initialcons.length == 1 ) { outstr += 'ʰ'; }
-			series = 1;
-			break;
+			outstr += 'c'
+			if (syllable.initialcons.length == 1 ) outstr += 'ʰ'
+			series = 1
+			break
 		case '\u1787':  // co
-			outstr += 'c'; 
-			series = 2;
-			break;
+			outstr += 'c'
+			series = 2
+			break
 		case '\u1788':  // cho
-			outstr += 'c'; 
-			if (syllable.initialcons.length == 1 ) { outstr += 'ʰ'; }
-			series = 2;
-			break;
+			outstr += 'c'
+			if (syllable.initialcons.length == 1 ) outstr += 'ʰ'
+			series = 2
+			break
 		case '\u1789':  // nyo
-			outstr += 'ɲ'; 
-			if (!vowelHarmony) { series = 2; }
-			else { series = vowelHarmony; }
-			break;
+			outstr += 'ɲ' 
+			if (!vowelHarmony) series = 2
+			else series = vowelHarmony
+			break
 		case '\u178a':  // da
-			outstr += 'ɗ'; 
-			series = 1;
-			break;
+			outstr += 'ɗ'
+			series = 1
+			break
 		case '\u178b':  // tha
-			outstr += 't'; 
-			if (syllable.initialcons.length == 1 ) { outstr += 'ʰ'; }
-			series = 1;
-			break;
+			outstr += 't'
+			if (syllable.initialcons.length == 1 ) outstr += 'ʰ'
+			series = 1
+			break
 		case '\u178c':  // do
-			outstr += 'ɗ'; 
-			series = 2;
-			break;
+			outstr += 'ɗ'
+			series = 2
+			break
 		case '\u178d':  // ttho
-			outstr += 'tʰ'; 
-			series = 2;
-			break;
+			outstr += 'tʰ'
+			series = 2
+			break
 		case '\u178e':  // nno
-			outstr += 'n'; 
-			if (!vowelHarmony) { series = 2; }
-			else { series = vowelHarmony; }
-			break;
+			outstr += 'n'
+			if (!vowelHarmony) series = 2
+			else series = vowelHarmony
+			break
 		case '\u178f':  // ta
-			if (syllable.presyllable && syllable.finalcons[0] in nasals) { outstr += 'ɗ'; }
-			else { outstr += 't';  }
-			series = 1;
-			break;
+			if (syllable.presyllable && nasals.has(syllable.finalcons[0])) outstr += 'ɗ'
+			else outstr += 't'
+			series = 1
+			break
 		case '\u1790':  // tha
-			outstr += 't'; 
-			if (syllable.initialcons.length == 1 ) { outstr += 'ʰ'; }
-			series = 1;
-			break;
+			outstr += 't'
+			if (syllable.initialcons.length == 1 ) outstr += 'ʰ'
+			series = 1
+			break
 		case '\u1791':  // to
-			outstr += 't'; 
-			series = 2;
-			break;
+			outstr += 't'
+			series = 2
+			break
 		case '\u1792':  // tho
-			outstr += 't'; 
-			if (syllable.initialcons.length == 1 ) { outstr += 'ʰ'; }
-			series = 2;
-			break;
+			outstr += 't'
+			if (syllable.initialcons.length == 1 ) outstr += 'ʰ'
+			series = 2
+			break
 		case '\u1793':  // no
-			outstr += 'n'; 
-			if (!vowelHarmony) { series = 2; }
-			else { series = vowelHarmony; }
-			break;
+			outstr += 'n'
+			if (!vowelHarmony) series = 2
+			else series = vowelHarmony
+			break
 		case '\u1794':  // ba
-			if (syllable.muusikoatan || syllable.initialcons.length > 1) {
-				outstr += 'p';
-				}
-			else outstr += 'ɓ'; 
-			series = 1;
-			break;
+			if (syllable.muusikoatan || syllable.initialcons.length > 1) outstr += 'p'
+			else outstr += 'ɓ'
+			series = 1
+			break
 		case '\u1795':  // pha
-			outstr += 'p'; 
-			if (syllable.initialcons.length == 1 ) { outstr += 'ʰ'; }
-			series = 1;
-			break;
+			outstr += 'p'
+			if (syllable.initialcons.length == 1 ) outstr += 'ʰ'
+			series = 1
+			break
 		case '\u1796':  // po
-			outstr += 'p'; 
-			series = 2;
-			break;
+			outstr += 'p'
+			series = 2
+			break
 		case '\u1797':  // pho
-			outstr += 'p'; 
-			if (syllable.initialcons.length == 1 ) { outstr += 'ʰ'; }
-			series = 2;
-			break;
+			outstr += 'p'
+			if (syllable.initialcons.length == 1 ) outstr += 'ʰ'
+			series = 2
+			break
 		case '\u1798':  // mo
-			outstr += 'm'; 
-			if (!vowelHarmony) { series = 2; }
-			else { series = vowelHarmony; }
-			break;
+			outstr += 'm'
+			if (!vowelHarmony) series = 2
+			else series = vowelHarmony
+			break
 		case '\u1799':  // yo
 			outstr += 'j'; 
 			if (!vowelHarmony) { series = 2; }
 			else { series = vowelHarmony; }
 			break;
 		case '\u179a':  // ro
-			outstr += 'r'; 
-			if (!vowelHarmony) { series = 2; }
-			else { series = vowelHarmony; }
-			break;
+			outstr += 'r'
+			if (!vowelHarmony) series = 2
+			else series = vowelHarmony
+			break
 		case '\u179b':  // lo
-			outstr += 'l'; 
-			if (!vowelHarmony) { series = 2; }
-			else { series = vowelHarmony; }
-			break;
+			outstr += 'l'
+			if (!vowelHarmony) series = 2
+			else series = vowelHarmony
+			break
 		case '\u179c':  // vo
-			outstr += 'ʋ'; 
-			if (!vowelHarmony) { series = 2; }
-			else { series = vowelHarmony; }
-			break;
+			outstr += 'ʋ'
+			if (!vowelHarmony) series = 2
+			else series = vowelHarmony
+			break
 		case '\u179f':  // sa
-			outstr += 's'; 
-			series = 1;
-			break;
+			outstr += 's'
+			series = 1
+			break
 		case '\u17a0':  // ha
-			if (syllable.initialcons.length == 1) {
-				outstr += 'h'; 
-				}
-			series = 1;
-			break;
+			if (syllable.initialcons.length == 1) outstr += 'h'
+			series = 1
+			break
 		case '\u17a1':  // la
-			outstr += 'l'; 
-			if (!vowelHarmony) { series = 1; }
-			else { series = vowelHarmony; }
-			break;
+			outstr += 'l'
+			if (!vowelHarmony) series = 1
+			else series = vowelHarmony
+			break
 		case '\u17a2':  // qa
-			outstr += 'ʔ'; 
-			series = 1;
-			break;
-		outstr += character;
+			outstr += 'ʔ'
+			series = 1
+			break
+		outstr += character
 		}
 
 	switch (syllable.initialcons.charAt(1) ) { // Initial subscript consonant
@@ -1270,14 +1285,14 @@ if (syllable.length > 1 && consonants.has(syllable[syllable.length-1])) syllable
 		// ==================================== Triple clusters  ===================================
 
 		if (syllable.initialcons[2]) {
-					switch (syllable.initialcons[2]) {  // this is either r in sanskrit or sahstraakaa, or l in ankleeh
-					case '\u179a':  // ro
-						outstr += 'r';
-						break;
-					case '\u179b': // la
-						outstr += 'l';
-						break;
-					}
+			switch (syllable.initialcons[2]) {  // this is either r in sanskrit or sahstraakaa, or l in ankleeh
+			case '\u179a':  // ro
+				outstr += 'r'
+				break
+			case '\u179b': // la
+				outstr += 'l'
+				break
+				}
 			}
 		
 		
@@ -1301,7 +1316,7 @@ if (syllable.length > 1 && consonants.has(syllable[syllable.length-1])) syllable
 				}
 			else if (syllable.bantok) {
 				if ( series == 1)  { vowelsound = 'ɑ'; }
-				else if (syllable.finalcons && syllable.finalcons[0] in labials) { vowelsound = 'u'; }
+				else if (syllable.finalcons && labials.has(syllable.finalcons[0])) vowelsound = 'u'
 				else { vowelsound = 'uə̆'; }
 				}
 			else if (syllable.yuukaleapintu) {
@@ -1341,7 +1356,7 @@ if (syllable.length > 1 && consonants.has(syllable[syllable.length-1])) syllable
 				}
 			else if (syllable.bantok) {  
 				if ( series == 1)  { vowelsound = 'a'; }
-				else if (syllable.finalcons && syllable.finalcons[0] in velars) { vowelsound = 'eə̆'; }
+				else if (syllable.finalcons && velars.has(syllable.finalcons[0])) vowelsound = 'eə̆'
 				else { vowelsound = 'oə̆'; }
 				}
 			else {
@@ -1607,96 +1622,101 @@ if (syllable.length > 1 && consonants.has(syllable[syllable.length-1])) syllable
 	}
 	
 	
-function transcribeToIPA (temp) {
-	temp = temp.replace(/ ¶/, ''); // remove leading and trailing spaces
-	temp = temp.replace(/^ */, ''); // remove leading and trailing spaces
-	temp = temp.replace(/ *$/, '');
-	temp = temp.replace(/\u200B/g, ' '); // convert zwsp to space
-	temp = temp.replace(/-/g, '- '); // convert hyphens to hyphen + space
-	var syllables = temp.split(' ');
-	var outstr = '';
-	var vowels = { "\u17B6":'', "\u17B7":'', "\u17B8":'', "\u17B9":'', "\u17BA":'', "\u17BB":'', "\u17BC":'', "\u17BD":'', "\u17C1":'', "\u17C2":'', "\u17C3":'', "\u17C4":'', "\u17C5":'', "\u17BE":'', "\u17BF":'', "\u17C0":'', "\u17d0":''  };
-	var independentvowels = { "\u17A5":'', "\u17A6":'', "\u17A7":'', "\u17A9":'', "\u17AA":'', "\u17AF":'', "\u17B0":'', "\u17B1":'', "\u17B2":'', "\u17B3":'', "\u17AB":'', "\u17AC":'', "\u17AD":'', "\u17AE":'' };
-	var consonants = { "\u1780":'', "\u1782":'', "\u1781":'', "\u1783":'', "\u1784":'', "\u1785":'', "\u1787":'', "\u1786":'', "\u1788":'', "\u1789":'', "\u178A":'', "\u178C":'', "\u178B":'', "\u178D":'', "\u178E":'', "\u178F":'', "\u1791":'', "\u1790":'', "\u1792":'', "\u1793":'', "\u1794":'', "\u1796":'', "\u1795":'', "\u1797":'', "\u1798":'', "\u1799":'', "\u17A1":'', "\u179B":'', "\u179A":'', "\u179F":'', "\u17A0":'', "\u179C":'', '\u17a2':''};
-	var irregularwords = { 'ព្រហ្មណ៍':'prum', 'ព្រាហ្មណ៍':'priəm'  };
-	var series = 1;
-	var bantok = '\u17cb';
-	var robat = '\u17cc';
-	var muusikoatan = '\u17c9';
-	var triisap = '\u17ca';
-	var nikahit = '\u17c6';
-	var reahmuk = '\u17c7';
-	var coeng = '\u17d2';
-	var yuukaleapintu = '\u17c8';
-	var virama = '\u17d1';
-	var kakabat = '\u17ce';
-	var toandakhiat = '\u17cd';
-	var ahsda  = '\u17cf';
-	vowelHarmony = '';
+
 		
-	for (a = 0; a<syllables.length; a++) {
-		glyphs = syllables[a];  //alert('>'+glyphs+'<');
-		if (glyphs == '') { outstr += ' '; continue; }
+
+
+
+
+function transcribeToIPA (inputStr) {
+
+	// inputStr should have spaces around words, and use hyphens to separate word-internal syllables
+	
+	inputStr = inputStr.replace(/\u200B/g, ' ') // convert zwsp to space
+	inputStr = inputStr.replace(/\s/, ' ').trim() // remove leading and trailing spaces
+	inputStr = inputStr.replace(/-/g, '- ') // convert hyphens to hyphen + space
+	
+	var syllables = inputStr.split(' ')
+	
+	var outstr = ''
+	
+	var vowels = new Set(["\u17B6", "\u17B7", "\u17B8", "\u17B9", "\u17BA", "\u17BB", "\u17BC", "\u17BD", "\u17C1", "\u17C2", "\u17C3", "\u17C4", "\u17C5", "\u17BE", "\u17BF", "\u17C0", "\u17d0"])
+	var independentvowels = new Set(["\u17A5", "\u17A6", "\u17A7", "\u17A9", "\u17AA", "\u17AF", "\u17B0", "\u17B1", "\u17B2", "\u17B3", "\u17AB", "\u17AC", "\u17AD", "\u17AE"])
+	var consonants = new Set(["\u1780", "\u1782", "\u1781", "\u1783", "\u1784", "\u1785", "\u1787", "\u1786", "\u1788", "\u1789", "\u178A", "\u178C", "\u178B", "\u178D", "\u178E", "\u178F", "\u1791", "\u1790", "\u1792", "\u1793", "\u1794", "\u1796", "\u1795", "\u1797", "\u1798", "\u1799", "\u17A1", "\u179B", "\u179A", "\u179F", "\u17A0", "\u179C", '\u17a2'])
+	var consonants = new Set(["\u1780", "\u1782", "\u1781", "\u1783", "\u1784", "\u1785", "\u1787", "\u1786", "\u1788", "\u1789", "\u178A", "\u178C", "\u178B", "\u178D", "\u178E", "\u178F", "\u1791", "\u1790", "\u1792", "\u1793", "\u1794", "\u1796", "\u1795", "\u1797", "\u1798", "\u1799", "\u17A1", "\u179B", "\u179A", "\u179F", "\u17A0", "\u179C", '\u17a2'])
+	var irregularwords = { 'ព្រហ្មណ៍':'prum', 'ព្រាហ្មណ៍':'priəm'  }
+	var series = 1
+	vowelHarmony = ''
+		
+	for (let cSyllable=0; cSyllable<syllables.length; cSyllable++) {
+		var glyphs = syllables[cSyllable]  
+		if (debug) console.log('>'+glyphs+'<')
+		if (glyphs === '') { 
+			outstr += ' '
+			continue
+			}
 	
 		// parse syllable constituents
-		var vowelposn = 0;
-		var infinal = false;
-		var syllable = { initialcons: '', vowel:'', vowelposn: 0, robat: false, muusikatoan:false, triisap:false, 
-								finalcons:'' , nikahit:false, reahmuk:false , finalcons: '', bantok:false, presyllable:false,
-								yuukaleapintu: false, toandakhiat: false  };
+		var vowelposn = 0
+		var inFinal = false
+		var syllable = {	initialcons: '', vowel:'', vowelposn: 0, robat: false, muusikatoan:false, triisap:false, 
+							finalcons:'' , nikahit:false, reahmuk:false , finalcons: '', bantok:false, presyllable:false,
+							yuukaleapintu: false, toandakhiat: false  }
 
-		for (var i=0; i<glyphs.length; i++) {
-			character = glyphs[i]; //alert(character.charCodeAt(0).toString(16));
-			if (character in consonants) {
-				//if ( i== 0  || (vowelposn == 0 && coengfound == true)) { syllable.initialcons += character; }
-				if ( i== 0  || (infinal == false && glyphs[i-1] == coeng)) { syllable.initialcons += character; }
-				else { syllable.finalcons += character; infinal = true; }
+		for (var cChar=0; cChar<glyphs.length; cChar++) {
+			character = glyphs[cChar] //alert(character.charCodeAt(0).toString(16));
+			if (consonants.has(character)) { // if consonant & first character, or not inFinal and preceded by coeng, capture initial consonant
+				if ( cChar === 0  || (inFinal === false && glyphs[cChar-1] === coeng)) syllable.initialcons += character
+				else { // otherwise capture a final consonant & set state to inFinal
+					syllable.finalcons += character
+					inFinal = true
+					}
 				}
-			else if (character in vowels) {
-				syllable.vowel = character;
-				vowelposn = i;
+			else if (vowels.has(character)) { // if vowel, capture it, and record the position
+				syllable.vowel = character
+				vowelposn = i
 				}
-			else if (character in independentvowels) {
-				syllable.vowel = character;
-				vowelposn = i;
+			else if (independentvowels.has(character)) {  // if independent vowel, also capture as vowel and record position
+				syllable.vowel = character
+				vowelposn = i
 				}
-			else if (character == robat) { 
-				if (infinal) { syllable.finalcons = ''; }
-				else { syllable.robat = true;  }
+			else if (character === robat) { // if robat & in final, delete final, otherwise set robat to true
+				if (inFinal) syllable.finalcons = ''
+				else syllable.robat = true
 				}
-			else if (character == muusikoatan) { syllable.muusikoatan = true;  }
-			else if (character == triisap) { syllable.triisap = true;  }
-			else if (character == nikahit) { syllable.nikahit = true;  }
-			else if (character == reahmuk) { syllable.reahmuk = true;  }
-			else if (character == bantok) { syllable.bantok = true;  }
-			else if (character == yuukaleapintu) { syllable.yuukaleapintu = true;  }
-			else if (character == toandakhiat) { syllable.toandakhiat = true;  }
-			else if (character == '-') { syllable.presyllable = true;  }
-			else if (character == coeng) { coengfound = true;   }// alert('coeng found');
+			else if (character == muusikoatan) syllable.muusikoatan = true
+			else if (character == triisap) syllable.triisap = true
+			else if (character == nikahit) syllable.nikahit = true
+			else if (character == reahmuk) syllable.reahmuk = true
+			else if (character == bantok) syllable.bantok = true
+			else if (character == yuukaleapintu) syllable.yuukaleapintu = true
+			else if (character == toandakhiat) syllable.toandakhiat = true
+			else if (character == '-') syllable.presyllable = true
+			else if (character == coeng) coengfound = true  // alert('coeng found');
 			else if (character == virama || character == kakabat || character == toandakhiat || character == ahsda) {  } // do nothing
-			else { alert(i+' Unknown character'+character+' '+character.charCodeAt(0).toString(16)); }
+			else console.log(i+' Unknown character'+character+' '+character.charCodeAt(0).toString(16))
 			}
-		//document.getElementById('latin').value =syllable.initialcons+":"+syllable.finalcons+":"+syllable.vowel;
-		//alert(1);
-			
+		
+		// check that this syllable has no toandakhiat (syllable sound killer) before converting
 		if (!syllable.toandakhiat) { 
-			result = convertToIPA(syllable); 
-			outstr += result[0]; 
+			result = convertToIPA(syllable)
+			outstr += result[0]
 			}
-		//alert( 'series'+result[1]);
 		
 		// record the series for the next syllable if this is the first part of a disyllable
 		// this will be used for second syllables starting with liquids to determine the series of that syllable
 		// a null string signifies that this is not a presyllable
-		if (syllable.presyllable) { vowelHarmony = result[1]; } 
-		else { vowelHarmony = ''; }
-		// if (! syllable.presyllable) { outstr += ' '; }
+		if (syllable.presyllable) vowelHarmony = result[1]
+		else {
+			vowelHarmony = ''
+			outstr += ' '
+			}
 		}
 
 	return outstr 
 	}
 		
-	
+
 
 
 
