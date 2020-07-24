9b8fa789a40fbb4ac8201478730a231bf69d61f6
r12a <ishida@w3.org>
2020-07-24 10:23:42 +0100

    Make Gentium Plus the default font, rather than Times New Roman.

--- greek/defaults.js
+++ greek/defaults.js
@@ -1,11 +1,11 @@
 var defaults = {}
 var factoryDefaults = {}
-	factoryDefaults.font = 'Times New Roman' // text area font name (a single font, no quotes)
+	factoryDefaults.font = 'Gentium Plus WF' // text area font name (a single font, no quotes)
 	factoryDefaults.size = "32";  // text area font size, number of pixels (just the number)
 	factoryDefaults.rows = "2"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "1.5"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'el' // language to use for examples
-	factoryDefaults.uifont = 'Times New Roman'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uifont = 'Gentium Plus WF'; // font name  for selection area (a single font, no quotes)
 	factoryDefaults.uisize = "28";  // selection area font size, number of pixels (just the number)
 	factoryDefaults.ccbase = "\u25CC";  // default base for combining characters
 	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
@@ -18,7 +18,7 @@ var thisPicker = 'greekPicker'
 if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
 else  defaults = factoryDefaults
  
-var webFonts = [ "Doulos SIL WF" ]
+var webFonts = [ "Gentium Plus WF" ]
 
 
 var template = {}
