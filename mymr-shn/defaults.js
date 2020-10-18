d3092ef81534e477ae0231e10e1fb137035708ab
r12a <ishida@w3.org>
2020-10-18 08:08:06 +0100

    Move shan to mymr-shn

---
+++ mymr-shn/defaults.js
@@ -0,0 +1,62 @@
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'Noto Serif Myanmar WF' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "32";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "2"; // number representing number of times 100px for height of text area
+	factoryDefaults.lineheight = "2"; // number representing line height of text area in pixels/100
+	factoryDefaults.language = 'shn' // language to use for examples
+	factoryDefaults.uifont = 'Noto Serif Myanmar WF'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "26";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "";  // default base for combining characters
+	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
+	factoryDefaults.contrast = "low" // contrast for UI text colours
+	factoryDefaults.hints = "none" // type of hint to show alongside characters in the grids
+
+
+var thisPicker = 'shanPicker'
+
+if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
+else  defaults = factoryDefaults
+ 
+var webFonts = [ "Noto Serif Myanmar WF", "Noto Sans Myanmar WF", "Tharlon WF" ]
+
+
+var template = {}
+	template.title = 'Shan character app'
+	template.sample = "တွၼ်ႈ 1 ၵူၼ်းၵူႊၵေႃႉၼႆႉ ပဵၼ်ဢၼ်ၵိူတ်ႇမႃးလူၺ်ႈၵုင်ႇမုၼ်ဢၼ်လွတ်ႈလႅဝ်းၽဵင်ႇပဵင်းၵၼ် လႄႈ သုၼ်ႇလႆႈဢၼ် လွတ်ႈလႅဝ်းၽဵင်ႇ ပဵင်းၵၼ်။"
+	template.blocklocation= '/scripts/myanmar/block'  // blocklocation to use for examples
+	template.direction = "ltr" // indicates whether this is a picker for a RTL script
+	template.github = 'shan'
+	template.scriptcode = 'Mymr'
+	template.fontLocale = 'mymr-shn'
+
+
+
+
+var controls = [
+{"title":"Trans-<br/>literate", "alt":"Convert Shan text to a Latin transliteration.", "code":"doTranscription('transliterate')"},
+]
+
+
+
+
+var pulldown = [
+]
+
+
+
+var inputAids = [
+{"title":"Shape-based lookup", "dataVar":"showShapeLookup", "dataLocn":"shapelist", "dataShortTitle":"S", "type":"shape", "desc":"Click on a panel of shapes to find similar characters."},
+
+//{"title":"Hint at similar shapes", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint", "desc":"Show similar shapes as you mouse over a character."},
+
+{"title":"Type assist", "dataVar":"typeAssist", "dataLocn":"transcriptionPalette", "dataShortTitle":"T", "type":"palette", "initialCode":"setUpTypeAssist(false, '', typeAssistMap)", "desc":"Use ASCII characters to type Shan from the keyboard."},
+
+{"title":"Latin type-assist", "dataVar":"showLatinTrans", "dataLocn":"transcriptionPalette", "dataShortTitle":"L", "type":"palette", "initialCode":"setUpTypeAssist(true, latinTypeAssistMap, latinTypeAssistMap)", "desc":"Show characters needed for IPA or other transcriptions and transliterations."},
+
+//{"title":"ISO to Hindi", "dataVar":"showISOCharMap", "dataLocn":"transcriptionPalette", "dataShortTitle":"I", "type":"palette", "initialCode":"window.latinOnly=false;makePalette(isoCharacterMap);makeKbdEventList(isoCharacterMap);", "desc":"Create XXXX text from characters in the XXXX transcription."},
+
+{"title":"Reverse transliteration", "dataVar":"showTranslit", "dataLocn":"transcriptionPalette", "dataShortTitle":"R", "type":"palette", "initialCode":"setUpTypeAssist(false, typeAssistMap, typeAssistMap)", "desc":"Use ASCII characters to type Shan from the keyboard via reverse transliteration."},
+
+{"title":"Keyboard", "dataVar":"showKeyboard", "dataLocn":"keyboard", "dataShortTitle":"K", "type":"keyboard", "desc":"Select characters from a keyboard layout."}
+]
---
+++ mymr-shn/localcode.js
@@ -0,0 +1,43 @@
+globals.showTranslit =  ''
+globals.showMesherTrans =  ''
+globals.showISOTrans = ''
+globals.typeAssist = ' ✓'
+globals.showKeysTranslitToggle = false
+
+
+function localInitialise () {
+	}
+	
+
+var _h = {
+
+end: {}
+}
+
+
+
+
+
+var keyboarddef = [
+"§|1 ႑|2 ႒|3 ႓|4 ႔|5 ႕|6 ႖|7 ႗|8 ႘|9 ႙|0 ႐|-|=",
+"q ၸ ဢ|w တ ၻ|e ၼ|r မ ႞|t ဢ ြ|y ပ ၿ|u ၵ ၷ|i င ရ|o ဝ သ|p ႁ ႀ|[|]",
+"a ေ ဵ|s ႄ ႅ|d ိ ီ|f ် ႂ်|g ွ ႂ|h ႉ ံ|j ႇ ႆ|k ု|l ူ ႊ|; ႈ း|'|\\",
+"`|z ၽ|x ထ ၻ|c ၶ|v လ|b ယ ျ|n ၺ ႟|m ၢ ႃ|, ၊|. ။|/"
+]
+
+
+
+function toggleShift (node, LC, UC) {
+	if (node.className ==='unshifted') {
+		document.getElementById(LC).style.display = 'none'
+		document.getElementById(UC).style.display = 'block'
+		node.className = 'shifted'
+		} 
+	else {
+		document.getElementById(LC).style.display = 'block'
+		document.getElementById(UC).style.display = 'none'
+		node.className = 'unshifted'
+		}
+	}
+
+
