da517c239c11b3d819607990e6dc000bc61d1bcc
r12a <ishida@w3.org>
2020-11-06 12:04:00 +0000

    +syrc, replaces syriac

---
+++ syrc/defaults.js
@@ -0,0 +1,66 @@
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'Estrangelo Edessa WF' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "36";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "150"; // number representing number of times 100px for height of text area
+	factoryDefaults.lineheight = "1.2"; // number representing line height of text area in pixels/100
+	factoryDefaults.language = 'syr' // language to use for examples
+	factoryDefaults.uifont = 'Noto Sans Syriac Estrangela'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "28";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "";  // default base for combining characters
+	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
+	factoryDefaults.contrast = "low" // contrast for UI text colours
+	factoryDefaults.hints = "none" // type of hint to show alongside characters in the grids
+
+
+var thisPicker = 'syriacPicker'
+
+if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
+else  defaults = factoryDefaults
+ 
+var webFonts = [ "Noto Sans Syriac Eastern WF", "Noto Sans Syriac Estrangela WF", "Noto Sans Syriac Western WF", "East Syriac Adiabene WF", "Estrangelo Edessa WF", "Serto Jerusalem WF" ]
+
+
+var template = {}
+	template.title = 'Syriac character app'
+    template.sample = "ܘܐ̄ܢܵܫ̈ܝܼܢ ܐܵܡܪܝܼܢ ܕܲܐܟܙܢܵܐ ܩܲܕ݂ܡܵܝܲܬ݂ ܪܟ݂ܲܒ݂ ܐܵܬ݂ܘ̈ܵܬ݂ܲܐ ܥܸܒ݂ܪ̈ܵܝܵܬ݂ܲܐ ܘܲܒ݂ܗܹܝܢ ܣܡ ܢܡܘܣܐ ܆ ܗܟܢܐ ܘܫܝܠܡܘܢ ܪܟܒ ܐܬܘ̈ܬܐ ܕܣܦܪ̈ܐ ܐܚܪ̈ܢܐ ܘܝܗ̣ܒ ܠܥܡ̈ܡܐ ܕܐܬܝܩܪ ܡܢܗܘܢ ܂"
+    template.blocklocation= '/scripts/syriac/block'  // blocklocation to use for examples
+	template.direction = "rtl" // indicates whether this is a picker for a RTL script
+	template.github = 'syriac'
+	template.scriptcode = 'Syrc'
+	template.cursive = true
+	template.fontLocale = 'syrc'
+	template.hints = true
+
+
+var controls = [
+{"title":"Trans-<br/>literate", "alt":"Convert Syriac text to a Latin transliteration.", "code":"doTranscription('transliterate')"},
+]
+
+
+
+var pulldown = [
+{"title":"Reverse<br/>transliterate", "alt":"Convert a Latin transliteration to Syriac text.", "code":"doTranscription('revTransliterate')"},
+]
+
+
+var inputAids = [
+{"title":"Shape-based lookup", "dataVar":"showShapeLookup", "dataLocn":"shapelist", "dataShortTitle":"S", "type":"shape", "desc":"Click on a panel of shapes to find similar characters."},
+
+{"title":"Hint at similar shapes", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint", "desc":"Show similar shapes as you mouse over a character."},
+
+{"title":"Type assist", "dataVar":"typeAssist", "dataLocn":"transcriptionPalette", "dataShortTitle":"T", "type":"palette", "initialCode":"setUpTypeAssist(false, '', typeAssistMap)", "desc":"Use ASCII characters to type Syriac from the keyboard."},
+
+{"title":"Latin type-assist", "dataVar":"showLatinTrans", "dataLocn":"transcriptionPalette", "dataShortTitle":"L", "type":"palette", "initialCode":"setUpTypeAssist(true, latinTypeAssistMap, latinTypeAssistMap)", "desc":"Show characters needed for IPA or other transcriptions and transliterations."},
+
+{"title":"Reverse transliteration", "dataVar":"showTranslit", "dataLocn":"transcriptionPalette", "dataShortTitle":"R", "type":"palette", "initialCode":"setUpTypeAssist(false, typeAssistMap, typeAssistMap)", "desc":"Use ASCII characters to type Syriac from the keyboard via reverse transliteration."},
+
+{"title":"Keyboard", "dataVar":"showKeyboard", "dataLocn":"keyboard", "dataShortTitle":"K", "type":"keyboard", "desc":"Select characters from a keyboard layout."}
+
+
+
+
+//{"title":"Show cursive forms", "dataVar":"showCursive", "dataLocn":"cursive", "dataShortTitle":"S", "type":"hint"},
+
+]
+
---
+++ syrc/localtranscription.js
@@ -0,0 +1,169 @@
+﻿function localtranscribe (direction, str) {
+	
+	if (direction == 'transliterate') { return transliterate(str) }
+	if (direction == 'toLatin') { return toLatin(str) }
+	if (direction == 'toUN') { return transcribetoUN(str) }
+	}
+
+
+
+
+function transliterate (str) {
+
+str = ' '+str
+
+var strArray = [...str]
+var exclusions = new Set(['(',')','[',']','.',' '])
+
+for (i=0;i<strArray.length;i++) {
+	if (exclusions.has(strArray[i])) continue
+	if (! autoTranslitArray[strArray[i]]) continue
+	re = new RegExp(strArray[i],'g')
+	str = str.replace(re, autoTranslitArray[strArray[i]])
+	}
+
+
+return str.trim()
+}
+
+
+
+
+
+function toLatin (str) {
+	// transcribes syriac text into a Latin transcription
+	
+    
+    var punctuation = new Set([' ', '،', '؛', '.', '؟', '܀', '܁', '܂', '܃', '܄', '܅', '܆', '܇', '܈', '܉'])
+    
+	
+	// add initial space so that beginning of word rules work
+	str = ' '+str+' '
+
+    // a-vowel or i-vowel + alaph at end of word
+    str = str.replace(/\u0735\u0710([ ،؛.؟܀܁܂܃܄܅܆܇܈܉]+)/g, "â$1")
+    str = str.replace(/\u0732\u0710([ ،؛.؟܀܁܂܃܄܅܆܇܈܉]+)/g, "ȃ$1")
+    str = str.replace(/\u0738\u0710([ ،؛.؟܀܁܂܃܄܅܆܇܈܉]+)/g, "ȋ$1")
+
+
+    // rukkakha
+    str = str.replace(/ܒ݂/g, "ḇ")
+    str = str.replace(/ܓ݂/g, "ḡ")
+    str = str.replace(/ܕ݂/g, "ḏ")
+    str = str.replace(/ܟ݂/g, "ḵ")
+    str = str.replace(/ܦ݂/g, "p̄")
+    str = str.replace(/ܬ݂/g, "ṯ")
+    
+    // maǧliyana
+    str = str.replace(/ܓ̰/g, "ǧ")
+    str = str.replace(/ܙ̃/g, "ž")
+    str = str.replace(/ܙ̰/g, "ž")
+    str = str.replace(/ܫ̰/g, "ẑ")
+  
+
+    // hbasa 
+    str = str.replace(/ܝܼ/g, "î")
+    str = str.replace(/ܘܼ/g, "û")
+  
+    // rwiha 
+    str = str.replace(/ܘܿ/g, "ô")
+  
+    
+    str = str.replace(/\u0710/g, "\u02BE")
+    str = str.replace(/\u0712/g, "b")
+    str = str.replace(/\u0713/g, "g")
+    str = str.replace(/\u0715/g, "d")
+    str = str.replace(/\u0717/g, "h")
+    str = str.replace(/\u0718/g, "w")
+    str = str.replace(/\u0719/g, "z")
+    str = str.replace(/\u071A/g, "\u1E25")
+    str = str.replace(/\u071B/g, "\u1E6D")
+    str = str.replace(/\u071D/g, "y")
+    str = str.replace(/\u071F/g, "k")
+    str = str.replace(/\u0720/g, "l")
+    str = str.replace(/\u0721/g, "m")
+    str = str.replace(/\u0722/g, "n")
+    str = str.replace(/\u0723/g, "s")
+    str = str.replace(/\u0725/g, "\u02BF")
+    str = str.replace(/\u0726/g, "p")
+    str = str.replace(/\u0728/g, "\u1E63")
+    str = str.replace(/\u0729/g, "q")
+    str = str.replace(/\u072A/g, "r")
+    str = str.replace(/\u072B/g, "\u0161")
+    str = str.replace(/\u072C/g, "t")
+
+    
+    // dotted vowels
+    str = str.replace(/\u0732/g, "a")
+    str = str.replace(/\u0735/g, "\u0101")
+    str = str.replace(/\u0738/g, "e")
+    str = str.replace(/\u0739/g, "\u0113")
+    str = str.replace(/\u073C/g, "i")
+    str = str.replace(/\u073F/g, "o")
+ 
+ 
+    // greek vowels
+    str = str.replace(/\u0730/g, "\u0227")
+    str = str.replace(/\u0731/g, "\u1EA1")
+    str = str.replace(/\u0733/g, "\u00E5")
+    str = str.replace(/\u0734/g, "\u1E01")
+    str = str.replace(/\u0736/g, "\u0117")
+    str = str.replace(/\u0737/g, "\u1EB9")
+    str = str.replace(/\u073A/g, "\u00EF")
+    str = str.replace(/\u073B/g, "\u1ECB")
+    str = str.replace(/\u073D/g, "\u016F")
+    str = str.replace(/\u073E/g, "\u1EE5")
+    str = str.replace(/\u0651/g, "\u022F")
+ 
+ 
+    // diacritics
+    str = str.replace(/\u0308/g, "\u030B")
+    str = str.replace(/\u0740/g, "ʺ")
+ 
+ 
+    // punctuation
+    str = str.replace(/\u060C/g, ",")
+    str = str.replace(/\u061B/g, ";")
+    str = str.replace(/\u061F/g, "?")
+    str = str.replace(/\u0701/g, ".")
+    str = str.replace(/\u0702/g, ".")
+    str = str.replace(/\u0703/g, ":")
+    str = str.replace(/\u0704/g, ":")
+    str = str.replace(/\u0705/g, ":")
+    str = str.replace(/\u0706/g, ":")
+    str = str.replace(/\u0707/g, ":")
+    str = str.replace(/\u0708/g, ";")
+    str = str.replace(/\u0709/g, "?")
+    str = str.replace(/\u070A/g, ".")
+
+
+
+	
+	// add markup for ambiguous cases
+	str = str.replace(/\[/g,'<span class=alts><span class=altfirst>')
+	str = str.replace(/\|/g,'</span><span class=alt>')
+	str = str.replace(/\{/g,'</span><span class=altlast>')
+	str = str.replace(/\]/g,'</span></span>')
+	//str = str.replace(/¶/g,'')
+
+
+	return str.trim()
+	}
+
+
+
+
+
+function transcribetoISO (chstring) {
+					
+
+	}
+
+
+
+
+
+
+
+
+
