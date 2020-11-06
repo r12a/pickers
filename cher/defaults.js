87698a60424a6b80b26bf968aaddb20bd2fdf95f
r12a <ishida@w3.org>
2020-11-06 11:53:36 +0000

    +cher, replaces cherokee

---
+++ cher/defaults.js
@@ -0,0 +1,62 @@
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'Noto Sans Cherokee WF' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "36";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "150"; // px height of text area
+	factoryDefaults.lineheight = "1.5"; // number representing line height of text area in pixels/100
+	factoryDefaults.language = 'chr' // language to use for examples
+	factoryDefaults.uifont = 'Noto Sans Cherokee WF'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "24";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "\u25CC";  // default base for combining characters
+	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
+	factoryDefaults.contrast = "low" // contrast for UI text colours
+	factoryDefaults.hints = "" // type of hint to show alongside characters in the grids
+
+
+var thisPicker = 'cherokeePicker'
+
+if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
+else  defaults = factoryDefaults
+ 
+var webFonts = [ "Noto Sans Cherokee WF" ]
+
+
+var template = {}
+	template.title = 'Cherokee character app'
+	template.sample = "2. ᏂᎦᏛ ᎠᏂᏴᏫ ᎤᏂᎲ ᎾᏍᎩ ᏚᏳᎪᏛ ᎤᏂᎩᏍᏗ ᏂᎦᎥᏉ ᎠᏰᎵ ᏂᎬᎾᏛᎢ, ᎤᏠᏯᏍᏗ ᎤᏩᏌ ᎤᏤᎵ ᎡᎥᎢ, ᎠᎴ ᎢᎦᎷᎯᏍᏗ ᎤᎾᎢ ᎤᏩᏌ ᎤᏤᎵ ᎠᏰᎵ ᏂᎬᎾᏛᎢ."
+	template.blocklocation= 'cherokee'  // blocklocation to use for examples
+	template.direction = "ltr" // indicates whether this is a picker for a RTL script
+	template.github = '/scripts/cherokee/block'
+	template.scriptcode = 'Cher'
+	template.fontLocale = 'cher'
+	template.hints = false
+
+
+
+var controls = [
+{"title":"Trans-<br/>literate", "alt":"Convert Cherokee text to a Latin transliteration.", "code":"doTranscription('transliterate')"},
+]
+
+
+
+var pulldown = [
+{"title":"Reverse<br/>translit.", "alt":"Convert Cherokee transliteration back to Cherokee.", "code":"doTranscription('reverse')"},
+]
+
+
+
+var inputAids = [
+{"title":"Shape-based lookup", "dataVar":"showShapeLookup", "dataLocn":"shapelist", "dataShortTitle":"S", "type":"shape", "initialCode":"kbd=document.getElementById('alphabet'); if (kbd.style.display==='none') {kbd.style.display='block';} else {kbd.style.display='none';}", "desc":"Click on a panel of shapes to find similar characters."},
+
+//{"title":"Hint at similar shapes", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint", "desc":"Show similar shapes as you mouse over a character."},
+
+//{"title":"Type assist", "dataVar":"typeAssist", "dataLocn":"transcriptionPalette", "dataShortTitle":"T", "type":"palette", "initialCode":"makePalette(''); window.latinOnly=false;makeKbdEventList(translitCharacterMap);", "desc":"Use ASCII characters to type XXXX from the keyboard."},
+
+{"title":"Latin type-assist", "dataVar":"showLatinTrans", "dataLocn":"transcriptionPalette", "dataShortTitle":"L", "type":"palette", "initialCode":"setUpTypeAssist(true, latinTypeAssistMap, latinTypeAssistMap)", "desc":"Show characters needed for IPA or other transcriptions and transliterations."},
+
+//{"title":"Reverse transliteration", "dataVar":"showTranslit", "dataLocn":"transcriptionPalette", "dataShortTitle":"R", "type":"palette", "initialCode":"window.latinOnly=false;makePalette(translitCharacterMap);makeKbdEventList(translitCharacterMap);", "desc":"Use ASCII characters to type XXXX from the keyboard via reverse transliteration."},
+
+{"title":"Keyboard", "dataVar":"showKeyboard", "dataLocn":"keyboard", "dataShortTitle":"K", "type":"keyboard", "desc":"Select characters from a keyboard layout."}
+
+]
+
---
+++ cher/localcode.js
@@ -0,0 +1,135 @@
+globals.typeAssist = ''
+globals.showKeysTranslitToggle = false
+
+
+
+function localInitialise () {
+	}
+
+
+
+var _h = {
+'531': ['54D'],
+"544": ['54D'],
+"561": ['57A'],
+'56F': ['587','576'],
+"574": ['57D','574576','574565','57456B','57E576','57456D','57E'],
+"574576": ['574',"57E576"],
+"574565": ['574'],
+"57456B": ['574',"57456D"],
+"57456D": ['574',"57456B"],
+"57E576": ['574','574576'],
+'576': ['587','56F','57D'],
+"57A": ['561'],
+"57D": ['574','576','587','56F','57E'],
+"57E": ['574','57D'],
+'57F': ['583'],
+'583': ['57F'],
+'587': ['56F','576','57D'],
+'57C': ['572', '580', '564', '56B', '578'],
+'578': ['572', '580', '564', '56B', '57C'],
+'572': ['564', '578'],
+'580': ['56B', '57C', '578'],
+'56B': ['580', '570', '578'],
+'564': ['572', '578'],
+'570': ['56B', '578'],
+'548': ['54C'],
+'54C': ['548'],
+'535': ['567', '537'],
+'565': ['567', '537'],
+'567': ['535', '565'],
+'537': ['565', '535'],
+'536': ['541', '549'],
+'549': ['541', '536'],
+'541': ['536', '549'],
+'53A': ['56E'],
+'56A': ['56E'],
+'56E': ['53A', '56A'],
+'53F': ['54E'],
+'54E': ['53F'],
+'53E': ['543'],
+'543': ['53E'],
+'573': ['56E', '571'],
+'545': ['551'],
+'551': ['545'],
+'540': ['577'],
+'577': ['540'],
+'54F': ['556'],
+'556': ['54F'],
+
+
+end: {}
+}
+
+
+
+
+var keyboarddef = [
+"§|1 ᏣᎳᎩ Ꮁ|2 ᎣᏏᏲ Ꮗ|3 ᏩᏙ Ꮷ|4 Ꮩ Ꮀ|5 Ꮶ Ꮉ|6 Ꮬ Ꮭ|7 Ꮛ Ꮱ|8 Ꮦ Ꮊ|9 Ꮢ (|0 Ꮔ )|- Ꮏ Ꮌ|= Ᏻ Ꮍ",
+"q Ꭺ Ꮖ|w Ꮃ Ꮻ|e Ꭱ Ꮳ|r Ꮫ Ꮟ|t Ꮤ Ꮨ|y Ꮿ Ᏺ|u Ꭴ Ꭽ|i Ꭲ Ᏹ|o Ꭳ Ꮼ|p Ꮑ Ꮺ|[ Ꮥ Ꮡ|] Ꮆ Ꮴ",
+"a Ꭰ Ꮜ|s Ꮝ Ꮞ|d Ꮧ Ꮠ|f Ꭹ Ꮘ|g Ꭶ Ꮵ|h Ꭿ Ꮂ|j Ꮪ Ꭻ|k Ꮈ Ꭷ|l Ꮅ Ꭾ|; Ꮸ Ꮰ|' |\\ Ꮹ Ꮾ",
+"` Ꮚ|z Ꭼ Ꮓ|x Ᏼ Ꮽ|c Ꮣ Ꮯ|v Ꭵ Ꮮ|b Ꭸ Ᏸ|n Ꮎ Ꮋ|m Ꮕ Ꮇ|, Ꮲ|. Ꮄ|/ Ꮒ Ꮙ"
+]
+
+
+var keyboardUC = [
+"§|1 ꮳꮃꭹ ꮁ|2 ꭳꮟᏺ ꮗ|3 ꮹꮩ ꮷ|4 ꮩ ꮀ|5 ꮶ ꮉ|6 ꮬ ꮭ|7 ꮛ ꮱ|8 ꮦ ꮊ|9 ꮢ (|0 ꮔ )|- ꮏ ꮌ|= ᏻ ꮍ", 
+"q ꭺ ꮖ|w ꮃ ꮻ|e ꭱ ꮳ|r ꮫ ꮟ|t ꮤ ꮨ|y ꮿ ᏺ|u ꭴ ꭽ|i ꭲ ᏹ|o ꭳ ꮼ|p ꮑ ꮺ|[ ꮥ ꮡ|] ꮆ ꮴ", 
+"a ꭰ ꮜ|s ꮝ ꮞ|d ꮧ ꮠ|f ꭹ ꮘ|g ꭶ ꮵ|h ꭿ ꮂ|j ꮪ ꭻ|k ꮈ ꭷ|l ꮅ ꭾ|; ꮸ ꮰ|' |\\ ꮹ ꮾ", 
+"` ꮚ|z ꭼ ꮓ|x ᏼ ꮽ|c ꮣ ꮯ|v ꭵ ꮮ|b ꭸ ᏸ|n ꮎ ꮋ|m ꮕ ꮇ|, ꮲ|. ꮄ|/ ꮒ ꮙ" 
+]
+
+
+
+function event_toggleKbdShift () {
+    if (this.classList.contains('shiftKeyOn')) {
+        makeKeyboard(keyboarddef, keyboardLCGuide)
+        }
+    else makeKeyboard(keyboardUC, keyboardUCGuide)
+    }
+
+
+
+
+
+
+
+
+
+
+function toggleShift (node) {
+	if (node.className =='unshifted') {
+		document.getElementById('lowercase').style.display = 'none'
+		document.getElementById('uppercase').style.display = 'block'
+		node.className = 'shifted'
+		} 
+	else {
+		document.getElementById('lowercase').style.display = 'block'
+		document.getElementById('uppercase').style.display = 'none'
+		node.className = 'unshifted'
+		}
+	}
+
+function toggleShift (node, LC, UC) {
+	if (node.classList.contains('shifted')) {
+		document.getElementById(LC).style.display = 'none'
+		document.getElementById(UC).style.display = 'block'
+		} 
+	else {
+		document.getElementById(LC).style.display = 'block'
+		document.getElementById(UC).style.display = 'none'
+		}
+	}
+
+function toggleShiftDefault (node) {
+	var cells = document.getElementById('defaultpanel').querySelectorAll('span')
+	if (node.className =='unshifted') {
+		for (var i=0;i<cells.length;i++) cells[i].textContent = cells[i].textContent.toUpperCase()
+		node.className = 'shifted'
+		} 
+	else {
+		for (var i=0;i<cells.length;i++) cells[i].textContent = cells[i].textContent.toLowerCase()
+		node.className = 'unshifted'
+		}
+	}
+
