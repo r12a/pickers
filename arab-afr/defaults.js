7c2ef6d98eab6689937a16427917a5c38cc58670
r12a <ishida@w3.org>
2020-11-07 08:13:45 +0000

    Replace ajami in title with arabic. Set Scheherazade New as default font for text area & ui. Incorporate 'unencoded' characters in main body and remove details section.

--- arab-afr/defaults.js
+++ arab-afr/defaults.js
@@ -1,11 +1,11 @@
 var defaults = {}
 var factoryDefaults = {}
-	factoryDefaults.font = 'Scheherazade WF' // text area font name (a single font, no quotes)
+	factoryDefaults.font = 'Scheherazade New WF' // text area font name (a single font, no quotes)
 	factoryDefaults.size = "50";  // text area font size, number of pixels (just the number)
 	factoryDefaults.rows = "150"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "1.5"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'ar' // language to use for examples
-	factoryDefaults.uifont = 'Noto Arabic Sans Light WF'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uifont = 'Scheherazade New WF'; // font name  for selection area (a single font, no quotes)
 	factoryDefaults.uisize = "24";  // selection area font size, number of pixels (just the number)
 	factoryDefaults.ccbase = "ـ";  // default base for combining characters
 	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
@@ -18,18 +18,18 @@ var thisPicker = 'arabafrPicker'
 if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
 else  defaults = factoryDefaults
  
-var webFonts = [ "Scheherazade WF", "Noto Arabic Sans Light WF", "Alkalami WF", "Alkalami Light WF" ]
+var webFonts = [ "Scheherazade New WF", "Noto Arabic Sans Light WF", "Alkalami WF", "Alkalami Light WF" ]
 
 
 var template = {}
-	template.title = 'Pan-African Ajami character app'
+	template.title = 'Pan-African Arabic'
 	template.sample = "رَایُوَا بَبَّنْ رَبُو نَا ، غُنْ مَسَاٻِى دُونْ شِدَیْنَا ، تَرْسَشِنْ أَیْكِى نَ ٻَرْنَا ، فَیْ دَ ٻُویٜ سِڟَیْدَ سُنَّا ، شِبِ أَللَّهْ بَادَكَنْغَرَا بَا"
     template.blocklocation= '/scripts/arabic/block'  // blocklocation to use for examples
 	template.direction = "rtl" // indicates whether this is a picker for a RTL script
 	template.github = 'arab-afr'
 	template.scriptcode = 'Arab'
+	template.fontLocale = 'arab'
 	template.cursive = true
-	template.fontLocale = 'arab-ajami'
 	template.hints = false
 
 
