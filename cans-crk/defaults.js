d28d70ae192ee962dd7c8eb80b31605299a971f5
r12a <ishida@w3.org>
2020-11-07 18:37:46 +0000

    v26

--- cans-crk/defaults.js
+++ cans-crk/defaults.js
@@ -1,8 +1,8 @@
 var defaults = {}
 var factoryDefaults = {}
 	factoryDefaults.font = 'Noto Sans Canadian Aboriginal WF' // text area font name (a single font, no quotes)
-	factoryDefaults.size = "35";  // text area font size, number of pixels (just the number)
-	factoryDefaults.rows = "2.2"; // number representing number of times 100px for height of text area
+	factoryDefaults.size = "36";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "150"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "1.5"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'crk' // language to use for examples
 	factoryDefaults.uifont = 'Noto Sans Canadian Aboriginal WF'; // font name  for selection area (a single font, no quotes)
@@ -30,6 +30,7 @@ var template = {}
 	template.github = 'cans-crk'
 	template.scriptcode = 'Cans'
 	template.fontLocale = 'cans'
+	template.hints = true
 
 
 
