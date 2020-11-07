a434ed185f5b8950272fdb1781157c13b1b55a88
r12a <ishida@w3.org>
2020-11-07 16:50:56 +0000

    Arranged selection panel phonetically v26 final touches

--- cyrl-ru/localcode.js
+++ cyrl-ru/localcode.js
@@ -184,6 +184,38 @@ function toggleShift (node, LC, UC) {
 	}
 
 
+function toggleShift (node) {
+    var chars = document.querySelectorAll('.c')
+
+	if (! node.classList.contains('shifted')) {
+        for (let i=0;i<chars.length;i++) {
+            if (chars[i].dataset.uc) chars[i].textContent = chars[i].dataset.uc
+            else chars[i].textContent = chars[i].textContent.toUpperCase()
+            }
+		} 
+	else {
+        for (let i=0;i<chars.length;i++) {
+            if (chars[i].dataset.lc) chars[i].textContent = chars[i].dataset.lc
+            else chars[i].textContent = chars[i].textContent.toLowerCase()
+            }
+		}
+	// reinitialise ids to codepoint values of character sequence (with no leading zeros)
+	node = document.querySelectorAll( '.c' ); 
+	for (var n = 0; n < node.length; n++ ) { 
+		content = node[n].textContent
+		id=''
+		for (i=0;i<content.length;i++) {
+			id += convertChar2CP(content[i])
+			}
+		node[n].id = id
+        node[n].dataset.c = 'c'+id
+        dec = parseInt(id, 16)
+        while (id.length<4) id = '0'+id
+        node[n].title = 'U+'+id+': '+charData[content]
+		}
+	}
+
+
 function toTitleCase () {
     // convert text area so each word begins with capital letter
     var str = document.getElementById('output').value
