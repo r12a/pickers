268be321244e876827238d6300f380fe45f89a5e
r12a <ishida@w3.org>
2020-11-08 22:06:12 +0000

    v26

--- phnx/defaults.js
+++ phnx/defaults.js
@@ -1,8 +1,8 @@
 var defaults = {}
 var factoryDefaults = {}
 	factoryDefaults.font = 'Noto Sans Phoenician WF' // text area font name (a single font, no quotes)
-	factoryDefaults.size = "40";  // text area font size, number of pixels (just the number)
-	factoryDefaults.rows = "2"; // number representing number of times 100px for height of text area
+	factoryDefaults.size = "32";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "150"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "1.4"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'phn' // language to use for examples
 	factoryDefaults.uifont = 'Noto Sans Phoenician WF'; // font name  for selection area (a single font, no quotes)
@@ -29,6 +29,7 @@ var template = {}
 	template.github = 'phnx'
 	template.scriptcode = 'Phnx'
 	template.fontLocale = "phnx"
+	template.hints = true
 
 
 
