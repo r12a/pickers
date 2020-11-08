c87a5a32e423e7b7dce4ea21ec9ddad7a25c68c6
r12a <ishida@w3.org>
2020-11-08 21:58:41 +0000

    v26

--- palm/defaults.js
+++ palm/defaults.js
@@ -2,7 +2,7 @@ var defaults = {}
 var factoryDefaults = {}
 	factoryDefaults.font = 'Noto Sans Palmyrene WF' // text area font name (a single font, no quotes)
 	factoryDefaults.size = "32";  // text area font size, number of pixels (just the number)
-	factoryDefaults.rows = "2"; // number representing number of times 100px for height of text area
+	factoryDefaults.rows = "150"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "1.4"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'arc' // language to use for examples
 	factoryDefaults.uifont = 'Noto Sans Palmyrene WF'; // font name  for selection area (a single font, no quotes)
@@ -29,6 +29,7 @@ var template = {}
 	template.github = 'palm'
 	template.scriptcode = 'Palm'
 	template.fontLocale = "palm"
+	template.hints = true
 
 
 
