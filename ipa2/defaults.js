f06bfaf679978a1e273ca930393fea11269c1bb8
r12a <ishida@w3.org>
2020-11-08 10:44:53 +0000

    v26

--- ipa2/defaults.js
+++ ipa2/defaults.js
@@ -1,9 +1,9 @@
 var defaults = {}
 var factoryDefaults = {}
 	factoryDefaults.font = 'Doulos SIL WF' // text area font name (a single font, no quotes)
-	factoryDefaults.size = "36";  // text area font size, number of pixels (just the number)
-	factoryDefaults.rows = "1.5"; // number representing number of times 100px for height of text area
-	factoryDefaults.lineheight = "1.5"; // number representing line height of text area in pixels/100
+	factoryDefaults.size = "32";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "100"; // px height of text area
+	factoryDefaults.lineheight = "1.4"; // line height of text area
 	factoryDefaults.language = 'en-fonipa' // language to use for examples
 	factoryDefaults.uifont = 'Doulos SIL WF'; // font name  for selection area (a single font, no quotes)
 	factoryDefaults.uisize = "24";  // selection area font size, number of pixels (just the number)
@@ -21,13 +21,14 @@ var webFonts = [ "Doulos SIL WF" ]
 
 
 var template = {}
-	template.title = 'IPA character app'
+	template.title = 'IPA <span style="font-size:50%">by place</span>'
 	template.sample = "/ɔːl ˈhɟʉːmən ˈbiːɪŋz ɑː bɔːn friː ænd ˈiːkwəl ɪn ˈdɪɡnɪtɪ ənd raɪts ‖ ðeɪ  ɑːr ɪnˈdæʊd wɪð ˈriːzᵊn ənd ˈkɒnʃəns ænd ʃʊd ækt təˈɰɔːdz wʌn əˈnʌðə ɪn ə ˈspɪrɪt ɒv ˈbrʌðəˌħʊd/"
 	template.blocklocation= '/scripts/latin/block'  // blocklocation to use for examples
 	template.direction = "ltr" // indicates whether this is a picker for a RTL script
 	template.github = 'ipa'
 	template.scriptcode = 'Latn'
 	template.fontLocale = 'ipa'
+	template.hints = false
 
 
 
