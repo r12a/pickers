51454ae66936fed6021bfc4f01bfb6200485db55
r12a <ishida@w3.org>
2020-11-08 19:02:50 +0000

    v26

--- nko/defaults.js
+++ nko/defaults.js
@@ -1,12 +1,12 @@
 var defaults = {}
 var factoryDefaults = {}
 	factoryDefaults.font = 'Noto Sans NKo WF' // text area font name (a single font, no quotes)
-	factoryDefaults.size = "35";  // text area font size, number of pixels (just the number)
-	factoryDefaults.rows = "2.5"; // number representing number of times 100px for height of text area
-	factoryDefaults.lineheight = "1.2"; // number representing line height of text area in pixels/100
+	factoryDefaults.size = "32";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "150"; // number representing number of times 100px for height of text area
+	factoryDefaults.lineheight = "1.4"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'nqo' // language to use for examples
 	factoryDefaults.uifont = 'Noto Sans NKo WF'; // font name  for selection area (a single font, no quotes)
-	factoryDefaults.uisize = "32";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.uisize = "28";  // selection area font size, number of pixels (just the number)
 	factoryDefaults.ccbase = "\u00A0";  // default base for combining characters
 	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
 	factoryDefaults.contrast = "low" // contrast for UI text colours
@@ -29,6 +29,7 @@ var template = {}
 	template.github = 'nko'
 	template.scriptcode = 'Nkoo'
 	template.fontLocale = 'nkoo'
+	template.hints = true
 
 
 
