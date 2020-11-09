a0900ceb7ba1025de0cd0f9800f7914930288a20
r12a <ishida@w3.org>
2020-11-09 08:45:42 +0000

    v26

--- arab-ug/defaults.js
+++ arab-ug/defaults.js
@@ -1,12 +1,12 @@
 var defaults = {}
 var factoryDefaults = {}
 	factoryDefaults.font = 'Scheherazade' // text area font name (a single font, no quotes)
-	factoryDefaults.size = "50";  // text area font size, number of pixels (just the number)
-	factoryDefaults.rows = "2.5"; // number representing number of times 100px for height of text area
+	factoryDefaults.size = "40";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "150"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "1.8"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'ug' // language to use for examples
 	factoryDefaults.uifont = 'Scheherazade'; // font name  for selection area (a single font, no quotes)
-	factoryDefaults.uisize = "40";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.uisize = "36";  // selection area font size, number of pixels (just the number)
 	factoryDefaults.ccbase = "\u25CC";  // default base for combining characters
 	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
 	factoryDefaults.contrast = "low" // contrast for UI text colours
@@ -30,6 +30,7 @@ var template = {}
 	template.scriptcode = 'Arab'
 	template.cursive = true
 	template.fontLocale = 'arab-ug'
+	template.hints = true
 
 
 
