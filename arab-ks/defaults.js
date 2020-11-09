2e69ca428565c440c58ab5ea59f465ab2a1fa1cb
r12a <ishida@w3.org>
2020-11-09 10:58:21 +0000

    v26

--- arab-ks/defaults.js
+++ arab-ks/defaults.js
@@ -1,8 +1,8 @@
 var defaults = {}
 var factoryDefaults = {}
 	factoryDefaults.font = 'Noto Nastaliq Urdu WF' // text area font name (a single font, no quotes)
-	factoryDefaults.size = "48";  // text area font size, number of pixels (just the number)
-	factoryDefaults.rows = "2"; // number representing number of times 100px for height of text area
+	factoryDefaults.size = "32";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "150"; // number representing number of px for height of text area
 	factoryDefaults.lineheight = "2"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'ks' // language to use for examples
 	factoryDefaults.uifont = 'Noto Nastaliq Urdu WF'; // font name  for selection area (a single font, no quotes)
@@ -10,7 +10,7 @@ var factoryDefaults = {}
 	factoryDefaults.ccbase = "";  // default base for combining characters
 	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
 	factoryDefaults.contrast = "low" // contrast for UI text colours
-	factoryDefaults.hints = "none" // type of hint to show alongside characters in the grids
+	factoryDefaults.hints = true // type of hint to show alongside characters in the grids
 
 
 var thisPicker = 'kashmiriaPicker'
