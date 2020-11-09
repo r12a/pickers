6cfabcf8d9f0865724370c51c0773972d2c00ab1
r12a <ishida@w3.org>
2020-11-09 10:58:41 +0000

    template update

--- _template_ltr/localcode.js
+++ _template_ltr/localcode.js
@@ -37,6 +37,38 @@ function toggleSubjoined (node) {
 	}
 
 
+function toggleShift (node) {
+    var chars = document.querySelectorAll('.c')
+
+	if (node.classList.contains('shifted')) {
+        for (let i=0;i<chars.length;i++) {
+            if (chars[i].dataset.lc) chars[i].textContent = chars[i].dataset.lc
+            else chars[i].textContent = chars[i].textContent.toLowerCase()
+            }
+		} 
+	else {
+        for (let i=0;i<chars.length;i++) {
+            if (chars[i].dataset.uc) chars[i].textContent = chars[i].dataset.uc
+            else chars[i].textContent = chars[i].textContent.toUpperCase()
+            }
+		}
+	// reinitialise ids to codepoint values of character sequence (with no leading zeros)
+	node = document.querySelectorAll( '.c' ); 
+	for (var n = 0; n < node.length; n++ ) { 
+		content = node[n].textContent
+		id=''
+		for (i=0;i<content.length;i++) {
+			id += convertChar2CP(content[i])
+			}
+		node[n].id = id
+        node[n].dataset.c = 'c'+id
+        dec = parseInt(id, 16)
+        while (id.length<4) id = '0'+id
+        node[n].title = 'U+'+id+': '+charData[content]
+		}
+	}
+
+
 var _h = {
 '974': ['973', '93B', '93A', '93E', '93B'],
 '973': ['93B', '93A', '93E', '93B', '974'],
