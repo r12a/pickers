41a8d86612b4b321eaf55e5a3d86e2f863d95fb6
r12a <ishida@w3.org>
2020-11-16 12:13:43 +0000

    bali, Add new Noto Sans font, and make Noto Serif the UI default

--- bali/defaults.js
+++ bali/defaults.js
@@ -5,7 +5,7 @@ var factoryDefaults = {}
 	factoryDefaults.rows = "150"; // px height of text area
 	factoryDefaults.lineheight = "2"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'ban' // language to use for examples
-	factoryDefaults.uifont = 'Aksara Bali WF'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uifont = 'Noto Serif Balinese WF'; // font name  for selection area (a single font, no quotes)
 	factoryDefaults.uisize = "24";  // selection area font size, number of pixels (just the number)
 	factoryDefaults.ccbase = "\u25CC";  // default base for combining characters
 	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
@@ -18,7 +18,7 @@ var thisPicker = 'balinesePicker'
 if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
 else  defaults = factoryDefaults
  
-var webFonts = [ "Aksara Bali WF", "Noto Serif Balinese WF" ]
+var webFonts = [ "Aksara Bali WF", "Noto Serif Balinese WF", "Noto Sans Balinese WF" ]
 
 
 var template = {}
