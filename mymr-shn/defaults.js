53fe0c80d29abd65ef0e5a2ab2670757007e2951
r12a <ishida@w3.org>
2020-12-17 16:55:57 +0000

    mymr-shn, + Find vocab

--- mymr-shn/defaults.js
+++ mymr-shn/defaults.js
@@ -45,6 +45,8 @@ var pulldown = [
 {"title":"Reverse<br/>transliterate", "alt":"Convert a Latin transliteration to Shan text.", "code":"doTranscription('revTransliterate')", "warning":"The Latin text must follow the transliteration scheme developed for this app."},
 
 {"title":"Vocab to<br>Markup", "alt":"Convert a vocab entry to example markup.", "code":"vocab2Markup(getHighlightedText(document.getElementById('output')))"},
+
+{"title":"Find vocab", "alt":"Search for examples containing the highlighted text", "code":"openVocabWindow('/scripts/myanmar/shn_vocab')"},
 ]
 
 
