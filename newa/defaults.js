be5801c51f5ebd42848aa82fcd2fb8ba62af97c8
r12a <ishida@w3.org>
2021-03-09 11:46:19 +0000

    newa: Lots of small changes

--- newa/defaults.js
+++ newa/defaults.js
@@ -4,10 +4,10 @@ var factoryDefaults = {}
 	factoryDefaults.size = "32";  // text area font size, number of pixels (just the number)
 	factoryDefaults.rows = "150"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "1.8"; // number representing line height of text area in pixels/100
-	factoryDefaults.language = 'ne' // language to use for examples
+	factoryDefaults.language = 'new' // language to use for examples
 	factoryDefaults.uifont = 'Noto Sans Newa WF'; // font name  for selection area (a single font, no quotes)
 	factoryDefaults.uisize = "28";  // selection area font size, number of pixels (just the number)
-	factoryDefaults.ccbase = "\u25CC";  // default base for combining characters
+	factoryDefaults.ccbase = "";  // default base for combining characters
 	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
 	factoryDefaults.contrast = "low" // contrast for UI text colours
 	factoryDefaults.hints = "none" // type of hint to show alongside characters in the grids
@@ -22,8 +22,7 @@ var webFonts = [ "Noto Sans Newa WF" ]
 
 var template = {}
 	template.title = 'Newa character app'
-	template.sample = "𑐩𑐸𑐮𑐸𑐎𑐫𑑂 𑐰𑑅𑐐𑐸 𑐬𑐵𑐖𑐣𑑁𑐟𑐶𑐎 𑐫𑐬𑐶𑐰𑐬𑑂𑐟𑐣 𑐤𑐹𑐐𑐸 𑐳𑐰𑐶𑐢𑐵𑐣𑑄 𑐣𑐾𑐫𑐵𑑅𑐫𑐵𑐟 𑐳𑑄𑐑𑐷𑐫 𑐮𑑀𑐎𑐟𑐵𑐣𑑂𑐟𑑂𑐬𑐎 𑐐𑐞𑐟𑐣𑑂𑐟𑑂𑐬 𑐣𑐶𑐬𑑂𑐩𑐵𑐞 𑐫𑐵𑐂𑐐𑐸 𑐖𑐹𑐐𑐸𑐮𑐶𑑄 𑐬𑐾𑐥𑐵𑑅𑐫𑐵 𑐁𑐡𑐶𑐰𑐵𑐳𑐷 𑐣𑐾𑐰𑐵𑑅𑐟𑐣𑐫𑐸𑐳𑑄 𑐣𑐾𑐰𑐵𑑅 𑐳𑐵𑐫𑐟𑑂𑐟 𑐬𑐵𑐖𑑂𑐬𑐫𑐵 𑐩𑐵𑐐 𑐫𑐵𑑅𑐐𑐸 𑐚𑑂𑐬𑐸 𑑋"
-	// source: https://scriptsource.org/cms/scripts/render_graphic.php?&type=graphic_in_html&title=Popup&filename=../sites/s/media/database/ssproto/entries/f8/2s/f82szejag6_Lipi%20Pau%20Feb%202009.jpg
+	template.sample = "𑐳𑐎𑐮𑐾𑑄 𑐩𑐣𑐹𑐟 𑐳𑑂𑐰𑐟𑐣𑑂𑐟𑑂𑐬 𑐰 𑐖𑑂𑐰𑐮𑐶𑐖𑑂𑐰𑑅 𑐁𑐟𑑂𑐩𑐳𑐩𑑂𑐩𑐵𑐣 𑐰 𑐰𑐵𑑄 𑐡𑐂𑐎𑐠𑑄 𑐧𑐸𑐂 𑑋 𑐂𑐥𑐶𑑄 𑐳𑑂𑐰𑐰𑐶𑐰𑐾𑐎 𑐰 𑐳𑐡𑑂𑐧𑐸𑐡𑑂𑐔𑐶 𑐡𑐫𑐵𑑅 𑐰𑐶𑐰𑐾𑐎𑐱𑐷𑐮 𑐖𑐸𑐂 𑐀𑐮𑐾 𑐠𑐰𑑄𑐠𑐰𑐫𑑂 𑐡𑐵𑐖𑐵𑐎𑐠𑑄 𑐴𑐣𑐵𑐧𑐣𑐵 𑐫𑐵𑐂 𑑋"
 	template.blocklocation= 'newa'  // blocklocation to use for examples
 	template.direction = "ltr" // indicates whether this is a picker for a RTL script
 	template.github = 'newa'
@@ -46,14 +45,16 @@ var pulldown = [
 {"title":"Vocab to<br>Markup", "alt":"Convert a vocab entry to example markup.", "code":"vocab2Markup(getHighlightedText(document.getElementById('output')))"},
 
 {"title":"Translit+", "alt":"Convert to a Latin transliteration but then apply additional phonetic transformations.", "code":"doTranscription('transcription')", "warning":"Adds inherent vowels based on a very generalised rule. The result should be checked."},
+
+{"title":"Find vocab", "alt":"Search for examples containing the highlighted text", "code":"openVocabWindow('/scripts/newa/new_vocab')"},
 ]
 
 
 
 var inputAids = [
-//{"title":"Shape-based lookup", "dataVar":"showShapeLookup", "dataLocn":"shapelist", "dataShortTitle":"S", "type":"shape", "desc":"Click on a panel of shapes to find similar characters."},
+{"title":"Shape-based lookup", "dataVar":"showShapeLookup", "dataLocn":"shapelist", "dataShortTitle":"S", "type":"shape", "desc":"Click on a panel of shapes to find similar characters."},
 
-//{"title":"Hint at similar shapes", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint", "desc":"Show similar shapes as you mouse over a character."},
+{"title":"Hint at similar shapes", "dataVar":"showShapeHints", "dataLocn":"", "dataShortTitle":"H", "type":"hint", "desc":"Show similar shapes as you mouse over a character."},
 
 {"title":"Type assist", "dataVar":"typeAssist", "dataLocn":"transcriptionPalette", "dataShortTitle":"T", "type":"palette", "initialCode":"setUpTypeAssist(false, '', typeAssistMap)", "desc":"Use ASCII characters to type Newa from the keyboard."},
 
