1706129645170ef2dd2e8ea9000435aec64113b8
r12a <ishida@w3.org>
2021-03-30 10:52:43 +0100

    kali

---
+++ kali/defaults.js
@@ -0,0 +1,142 @@
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'Noto Sans Kayah Li WF' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "32";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "150"; // px height of text area
+	factoryDefaults.lineheight = "1.4"; // line height of text area
+	factoryDefaults.language = 'kyu' // language to use for examples
+	factoryDefaults.uifont = 'Noto Sans Kayah Li WF'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "24";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "";  // default base for combining characters
+	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
+	factoryDefaults.contrast = "low" // contrast for UI text colours
+	factoryDefaults.hints = "none" // type of hint to show alongside characters in the grids
+
+
+var thisPicker = 'kayahliPicker'
+
+if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
+else  defaults = factoryDefaults
+ 
+var webFonts = [ "Noto Sans Kayah Li WF" ]
+
+
+var template = {}
+	template.title = 'Kayah Li'
+	template.sample = "ꤙꤤ ꤋꤢꤨ꤭ꤡꤛꤥꤔꤢ ꤡꤛꤣ ꤟꤢꤩꤏꤥ꤬ꤘꤣ꤬ꤑꤢ꤭ ꤕꤢꤩ ꤔꤤꤔꤢ꤬ꤠꤢꤩ꤬ ꤠꤟꤤ꤭ꤢꤨ ꤒꤢ꤬ꤟꤢꤩ꤬ ꤔꤢ ꤡꤢꤪꤗꤢꤚꤛꤢ꤬ ꤢ꤬ꤒꤟꤢꤧ꤬ꤟꤢꤩꤙꤢꤧ꤬ ꤢ꤬ꤋꤢꤧ꤭ ꤔꤌꤣ꤬ ꤢꤧ꤬ꤞꤤ꤭ ꤕꤟꤛꤢꤐꤟꤢꤧꤜꤟꤛꤢ꤬ꤊꤟꤢ꤬ ꤢꤧ꤬ꤞꤤ꤭ꤞꤢꤧ ꤚꤢꤪ ꤢ꤬ ꤔꤟꤤꤘꤢ꤭ ꤜꤟꤢꤪ꤭ꤋꤛꤢꤩ꤭ꤞꤛꤢ ꤡꤢꤪꤗꤢ꤬ꤚꤛꤢ꤬ꤘꤢꤨ꤬ ꤍꤟꤥ꤯"
+	template.blocklocation= 'kali'  // blocklocation to use for examples
+	template.direction = "ltr" // indicates whether this is a picker for a RTL script
+	template.github = 'kali'
+	template.scriptcode = 'Kali'
+	template.fontLocale = "kali"
+	template.hints = true
+
+
+
+var controls = [
+{"title":"Trans-<br/>literate", "alt":"Convert Kayah Li text to a one-to-one Latin transliteration.", "code":"doTranscription('transliterate')"},
+
+
+{"title":"Make<br/>vocab", "alt":"Create an entry for a vocab file.", 
+
+"code":`_output=document.getElementById('output'); 
+input=replaceSlash(getHighlightedText(_output),'|').split('|'); 
+if (! hasHighlight(_output)) _output.value=''; 
+
+term = input[0];
+meaning = input[1];
+ipa = input[2]? input[2] : transliterate(input[0]);
+alt = input[3]? input[3] : '';
+notes = input[4]? input[4] : '';
+
+add(getVocabWithAlt(term, meaning, ipa, notes, alt));
+vocab2Example(getHighlightedText(document.getElementById('output')));
+_output.focus();`},
+]
+
+
+
+
+
+var pulldown = [
+{"title":"Reverse<br/>transliterate", "alt":"Convert a Latin transliteration to Kayah Li text.", "code":"doTranscription('revTransliterate')", "warning":"The Latin text must follow the transliteration scheme developed for this app."},
+
+{"title":"Vocab to<br>Markup", "alt":"Convert a vocab entry to example markup.", "code":"vocab2Markup(getHighlightedText(document.getElementById('output')))"},
+
+//{"title":"Translit+", "alt":"Convert to a Latin transliteration but then apply additional phonetic transformations.", "code":"doTranscription('translitPlus')", "warning":"Adds inherent vowels and palatalisation. The result should be checked."},
+
+//{"title":"XXXX to Latin", "alt":"Convert XXXX text to a Latin transcription.", "code":"doTranscription('transcription')"},
+
+//{"title":"XXXX to Latin", "alt":"Convert XXXX script text to the Latin orthography.", "code":"doTranscription('transcription')", "warning":"The result of this conversion needs to be checked for accuracy."},
+
+//{"title":"XXXX to Radice", "alt":"Convert XXXX text to the Latin transcription used by Radice.", "code":"doTranscription('transcription')"},
+
+//{"title":"XXXX<br/>to ISO", "alt":"Convert XXXX text to an ISO 15919 Latin transcription.", "code":"doTranscription('toISO15919')"},
+
+//{"title":"XXXX<br/>to IPA", "alt":"Convert XXX text to an <em>approximate</em> IPA transcription.", "code":"doTranscription('toIPA')", "warning":"This only produces an <em>approximation</em> to an IPA transcription. Use it as a base and refine it by hand." },
+
+//{"title":"XXXX<br/>to IPA", "alt":"Convert XXX text to an <em>approximate</em> IPA transcription.", "code":"doTranscription('toISO15919')", "warning":"Separate syllables with spaces before use! Full transcription requires fully vowelled text! This only produces an APPROXIMATION to an IPA transcription. Use it as a base and refine it by hand." },
+
+//{"title":"ISO to<br/>XXXX", "alt":"Convert an ISO 15919 transcription to XXXX text.", "code":"doTranscription('toDeva')"},
+
+{"title":"Find vocab", "alt":"Search for examples containing the highlighted text", "code":"openVocabWindow('/scripts/kali/kyu_vocab')"},
+]
+
+
+//, "warning":"The results should be checked for accuracy."
+
+
+
+
+
+
+var inputAids = [
+{"title":"Shape-based lookup", "id":"showShapeLookup", "dataShortTitle":"S", "type":"shape", "desc":"Click on a panel of shapes to find similar characters."},
+
+{"title":"Hint at similar shapes", "id":"showShapeHints", "dataShortTitle":"H", "type":"hint", "desc":"Show similar shapes as you mouse over a character."},
+
+{"id":"showRevTransSwitch", "title":"Default type-assist: Maps keyboard mapped to useful characters.", 
+"desc":"Use ASCII characters to type Kayah Li from the keyboard using a customised key mapping.",
+"dataShortTitle":"T", "type":"palette", "initialCode":"mapstring=makeTypeAssistMap(cols.key); setUpTypeAssist(false, mapstring, mapstring)", 
+},
+
+{"title":"Type assist: IPA to Kayah Li.", 
+"desc":"Use an IPA keyboard mapping to type Kayah Li from the keyboard.",
+"dataShortTitle":"æ", "type":"palette",
+"initialCode":"mapstring=makeComplexTypeAssistMap(cols.ipaLoc);setUpTypeAssist(false, mapstring, mapstring)"
+},
+
+{"title":"Type assist: Latin transcription to Kayah Li.", 
+"desc":"Use a  mapping from a Latin trancription to type Kayah Li from the keyboard.",
+"dataShortTitle":"a", "type":"palette", "initialCode":"mapstring=makeComplexTypeAssistMap(cols.transcription);setUpTypeAssist(false, mapstring, mapstring)"
+},
+
+{"title":"Type assist: Kyebogyi keyboard mappings.", 
+"desc":"Use a Kyebogyi keyboard mapping to type from the keyboard.",
+"dataShortTitle":"k", "type":"palette", 
+"initialCode":"setUpTypeAssist(false, makeTypeAssistMap(cols.kbd), makeTypeAssistMap(cols.kbd)); document.getElementById('keyboard').style.display='block';"
+},
+
+
+{"id":"showLatinTransSwitch", "title":"Latin type-assist", 
+"desc":"Show characters needed for IPA or other transcriptions and transliterations.",
+"dataShortTitle":"L", "type":"palette", 
+"initialCode":"setUpTypeAssist(true, latinTypeAssistMap, latinTypeAssistMap)"
+},
+
+{"id":"togglePalette", "title":"Type-assist palette toggle", 
+"desc":"Show or hide the palette used for type-assist input.",
+"dataShortTitle":"P", "type":"toggle", "initialCode":"palette=document.getElementById('transcriptionPalette'); if (palette.style.display==='none') {palette.style.display='block';} else {palette.style.display='none';}"
+},
+]
+
+
+
+
+// this indicates which items are to be described in the help
+// options include: intro,shape,hinting,typeAssist,latin,reverse & keyboard
+var inputAidsHelp = 'showIntro,'
+for (let i=0;i<inputAids.length;i++) {
+	if (inputAids[i].dataVar) inputAidsHelp += ','+inputAids[i].dataVar
+	}
+
---
+++ kali/localcode.js
@@ -0,0 +1,102 @@
+globals.showTranslit =  ''
+globals.typeAssist = ''
+globals.showKeysTranslitToggle = false
+
+
+function localInitialise () {
+
+	}
+
+function toggleExtended (node) {
+    var chars = document.querySelectorAll('.x')
+
+	if (node.classList.contains('shifted')) {
+        for (let i=0;i<chars.length;i++) {
+            chars[i].style.display = 'none'
+            }
+		document.getElementById('translitvowels').style.display = 'none'
+		}
+    else {
+        for (let i=0;i<chars.length;i++) {
+            chars[i].style.display = 'inline-block'
+            }
+		document.getElementById('translitvowels').style.display = 'block'
+		} 
+	}
+
+
+
+function toggleShift (node) {
+    var chars = document.querySelectorAll('.c')
+
+	if (node.classList.contains('shifted')) {
+        for (let i=0;i<chars.length;i++) {
+            if (chars[i].dataset.lc) chars[i].textContent = chars[i].dataset.lc
+            else chars[i].textContent = chars[i].textContent.toLowerCase()
+            }
+		} 
+	else {
+        for (let i=0;i<chars.length;i++) {
+            if (chars[i].dataset.uc) chars[i].textContent = chars[i].dataset.uc
+            else chars[i].textContent = chars[i].textContent.toUpperCase()
+            }
+		}
+	// reinitialise ids to codepoint values of character sequence (with no leading zeros)
+	node = document.querySelectorAll( '.c' ); 
+	for (var n = 0; n < node.length; n++ ) { 
+		content = node[n].textContent
+		id=''
+		for (i=0;i<content.length;i++) {
+			id += convertChar2CP(content[i])
+			}
+		node[n].id = id
+        node[n].dataset.c = 'c'+id
+        dec = parseInt(id, 16)
+        while (id.length<4) id = '0'+id
+        node[n].title = 'U+'+id+': '+charData[content]
+		}
+	}
+
+
+var _h = {
+"A90B": ["A915", "A924", "A90B"],
+"A915": ["A924", "A90B", "A90B"],
+"A924": ["A90B", "A90B", "A915"],
+"A919": ["A91A", "A906", "A912", "A90A", "A919"],
+"A91A": ["A906", "A912", "A90A", "A919", "A919"],
+"A906": ["A912", "A90A", "A919", "A919", "A91A"],
+"A912": ["A90A", "A919", "A919", "A91A", "A906"],
+"A90A": ["A919", "A919", "A91A", "A906", "A912"],
+"A918": ["A903", "A918"],
+"A903": ["A918", "A918"],
+"A913": ["A91D", "A907", "A909", "A917", "A913"],
+"A91D": ["A907", "A909", "A917", "A913", "A913"],
+"A907": ["A909", "A917", "A913", "A913", "A91D"],
+"A909": ["A917", "A913", "A913", "A91D", "A907"],
+"A917": ["A913", "A913", "A91D", "A907", "A909"],
+"A921": ["A90F", "A921"],
+"A90F": ["A921", "A921"],
+"A91E": ["A911", "A91E"],
+"A911": ["A91E", "A91E"],
+"A91B": ["A91C", "A91B"],
+"A91C": ["A91B", "A91B"],
+end: {}
+}
+
+
+var keyboarddef = [
+"§|1 ꤁ !|2 ꤂ ꤮|3 ꤃ #|4 ꤄ :|5 ꤅ %|6 ꤆ “|7 ꤇ ”|8 ꤈ *|9 ꤉ (|0 ꤀ )|- - _|== +",
+"q ꤠ|w ꤎ|e ꤙ|r ꤚ|t ꤣ|y ꤤ|u ꤥ|i ꤞ|o ꤓ|p ꤑ|[ ꤘ|]",
+"a ꤊ|s ꤔ|d ꤛ|f ꤢ ꤢꤦ|g ꤬ ꤢꤧ|h ꤭ ꤢꤨ|j ꤒ ꤢꤩ|k ꤟ ꤢꤪ|l ꤕ|; ꤜ|' ꤍ|\\ \\",
+"`|z ꤡ|x ꤌ|c ꤏ|v ꤋ|b ꤗ|n ꤖ|m ꤝ|, ꤐ|. ,|/ ꤯"
+]
+
+// source: http://www.karennirefugees.com/uploads/3/0/1/9/3019325/karenni_uni_keyboard.pdf
+
+
+
+
+
+
+
+
