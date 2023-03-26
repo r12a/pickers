9111d1cf39a5d0b9bbc61c5482bb50ca4b2e4ed5
r12a <ishida@w3.org>
2023-03-26 08:24:06 +0100

    + Fulfulde

---
+++ arab-ff/defaults.js
@@ -0,0 +1,77 @@
+﻿var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'Scheherazade WF' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "32";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "150"; // number representing number of times 100px for height of text area
+	factoryDefaults.lineheight = "2"; // number representing line height of text area in pixels/100
+	factoryDefaults.language = 'ff' // language to use for examples
+	factoryDefaults.uifont = 'Scheherazade WF'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "26";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "";  // default base for combining characters
+	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
+	factoryDefaults.contrast = "low" // contrast for UI text colours
+	factoryDefaults.hints = "none" // type of hint to show alongside characters in the grids
+
+
+var thisPicker = 'arabffPicker'
+
+if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
+else  defaults = factoryDefaults
+ 
+var webFonts = [ "Scheherazade WF" ]
+
+
+var template = {}
+	template.title = 'Fulfulde (Ajamiya)'
+	template.sample = "عِنَّمَ عَادٜىٰجِ ڢٛڢ ݠٛتِ، ندِمْطِدِ عٜ جِبِنَنْندٜ تٛ بَنْنغٜ حَکّٜىٰجِ ⸫  عٜࢡٜ نغٛودِ مِيجٛ عٜ حَکِّلَنْتَاغَلْ عٜتٜ عٜࢡٜ ݠٛتِ حُوڢٛ ندِرْدٜ عٜ ندٜرْ ࢡ عِينغُيُمَّاغُ ⸫"
+	template.sampleSource = 'Transliterated by myself from https://en.wikipedia.org/wiki/Fula_language#Sample_Text'
+	template.blocklocation= 'arab'  // block directory
+	template.noteslocation = 'arab/ff' // location of script notes
+	template.vocablocation = 'arab/ff_vocab' // location of term database
+	template.direction = "rtl" // indicates whether this is a picker for a RTL script
+	template.github = 'arab-ff'
+	template.scriptcode = 'Arab'
+	template.fontLocale = 'arab-ar'
+	template.cursive = true
+	template.hints = true
+	
+	template.defaultSpace = ' '
+	template.spaces = [['SP', ' '], ['ZWSP', '\u200B'], ['NBSP','\u00A0'], 
+	['NNBSP','\u202F'], ['EMSP','\u2003'] ]
+	template.defaultInvisible = 'إ'
+	template.invisibles = [['ZWJ','\u200D'], ['ZWNJ','\u200C'], ['WJ','\u2060'], 
+	['SHY','\u200C'], ['NBHY','\u2011'], 
+	['RLM','\u200F'], ['LRM','\u200E'], ['ALM','\u061C']]
+	template.moreKeys = [['RLI','\u2067'], ['LRI','\u2066'], ['FSI','\u2068'], 
+	['PDI','\u2069'], ['RLO','\u202E'], ['LRO','\u202D'], ['PDF','\u202C'], 
+	['CGJ','\u034F']]
+
+
+
+
+var controls = [
+{"title":"Trans-<br/>literate", "alt":"Convert Ajamiya text to a Latin transliteration.", "code":"doTranscription('transliterate')"},
+]
+
+
+
+var pulldown = [
+]
+
+
+
+
+
+var show = {}  // indicates which inputAids to use
+    show.shape = false
+    show.hints = false
+    show.default = true
+    show.latin = true
+    show.ipa = true
+    show.transc = 'Latin'
+    show.kbd = ""
+    show.language = "Fulfulde"
+
+
+var inputAids = []
+
