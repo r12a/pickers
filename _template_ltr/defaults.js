4258e6779928706dda9ea1326993059ec47cb797
r12a <ishida@w3.org>
2024-02-10 07:34:06 +0000

    template updates

--- _template_ltr/defaults.js
+++ _template_ltr/defaults.js
@@ -59,23 +59,6 @@ var template = {}
 
 var controls = [
 {"title":"Trans-<br/>literate", "alt":"Convert XXXX text to a one-to-one Latin transliteration.", "code":"doTranscription('transliterate')"},
-
-
-{"title":"Make<br/>vocab", "alt":"Create an entry for a vocab file.", 
-
-"code":`_output=document.getElementById('output'); 
-input=replaceSlash(getHighlightedText(_output),'|').split('|'); 
-if (! hasHighlight(_output)) _output.value=''; 
-
-term = input[0];
-meaning = input[1];
-ipa = input[2]? input[2] : '';
-alt = input[3]? input[3] : '';
-notes = input[4]? input[4] : '';
-
-add(getVocabWithAlt(term, meaning, ipa, notes, alt));
-vocab2Example(getHighlightedText(document.getElementById('output')));
-_output.focus();`},
 ]
 
 
@@ -83,71 +66,28 @@ _output.focus();`},
 
 
 var pulldown = [
-{"title":"Reverse<br/>transliterate", "alt":"Convert a Latin transliteration to XXXX text.", "code":"doTranscription('revTransliterate')", "warning":"The Latin text must follow the transliteration scheme developed for this app."},
+{"title":"Inuktitut to IPA", "alt":"Convert Inuktitut text to an <em>approximate</em> IPA transcription.", "code":"doTranscription('toIPA')", "warning":"This only produces an <em>approximation</em> to an IPA transcription. Use it as a base and refine it by hand." },
 
-{"title":"Vocab to<br>Markup", "alt":"Convert a vocab entry to example markup.", "code":"vocab2Markup(getHighlightedText(document.getElementById('output')))"},
-
-{"title":"Translit+", "alt":"Convert to a Latin transliteration but then apply additional phonetic transformations.", "code":"doTranscription('translitPlus')", "warning":"Adds inherent vowels and palatalisation. The result should be checked."},
-
-{"title":"XXXX to Latin", "alt":"Convert XXXX text to a Latin transcription.", "code":"doTranscription('transcription')"},
-
-{"title":"XXXX to Latin", "alt":"Convert XXXX script text to the Latin orthography.", "code":"doTranscription('transcription')", "warning":"The result of this conversion needs to be checked for accuracy."},
-
-{"title":"XXXX to Radice", "alt":"Convert XXXX text to the Latin transcription used by Radice.", "code":"doTranscription('transcription')"},
-
-{"title":"XXXX<br/>to ISO", "alt":"Convert XXXX text to an ISO 15919 Latin transcription.", "code":"doTranscription('toISO15919')"},
-
-{"title":"XXXX<br/>to IPA", "alt":"Convert XXX text to an <em>approximate</em> IPA transcription.", "code":"doTranscription('toIPA')", "warning":"This only produces an <em>approximation</em> to an IPA transcription. Use it as a base and refine it by hand." },
-
-{"title":"XXXX<br/>to IPA", "alt":"Convert XXX text to an <em>approximate</em> IPA transcription.", "code":"doTranscription('toISO15919')", "warning":"Separate syllables with spaces before use! Full transcription requires fully vowelled text! This only produces an APPROXIMATION to an IPA transcription. Use it as a base and refine it by hand." },
-
-{"title":"ISO to<br/>XXXX", "alt":"Convert an ISO 15919 transcription to XXXX text.", "code":"doTranscription('toDeva')"},
+{"title":"Inuktitut to Latin", "alt":"Convert Inuktitut script text to the Latin orthography.", "code":"doTranscription('transcription')", "warning":"The result of this conversion needs to be checked for accuracy."},
 ]
 
 
-, "warning":"The results should be checked for accuracy."
 
 
-var inputAids = [
-{"title":"Shape-based lookup", "id":"showShapeLookup", "dataShortTitle":"S", "type":"shape", "desc":"Click on a panel of shapes to find similar characters."},
-
-{"title":"Hint at similar shapes", "id":"showShapeHints", "dataShortTitle":"H", "type":"hint", "desc":"Show similar shapes as you mouse over a character."},
-
-{"id":"showRevTransSwitch", 
-"title":"Default type-assist: Map keyboard to characters for easy input. Press ` to switch.", 
-"desc":"Use ASCII characters to type XXXXXX from the keyboard using a customised key mapping.",
-"dataShortTitle":"T", "type":"palette", "initialCode":"mapstring=makeTypeAssistMap(cols.key); setUpTypeAssist(false, mapstring, mapstring)", 
-},
-
-{"title":"Type assist: IPA to XXXXXX.", 
-"desc":"Use an IPA keyboard mapping to type XXXXXX from the keyboard.",
-"dataShortTitle":"[...]", "type":"palette",
-"initialCode":"mapstring=makeComplexTypeAssistMap(cols.ipaLoc);setUpTypeAssist(false, mapstring, mapstring)"
-},
-
-{"title":"Type assist: Latin transcription to XXXXXX.", 
-"desc":"Use a  mapping from Latin to type XXXXXX from the keyboard.",
-"dataShortTitle":"t", "type":"palette", "initialCode":"mapstring=makeComplexTypeAssistMap(cols.transcription);setUpTypeAssist(false, mapstring, mapstring)"
-},
-
-{"title":"Type assist: Map keys to a XXXXX keyboard.", 
-"desc":"Use a XXXXXX XXXXXX keyboard mapping to type from the keyboard.",
-"dataShortTitle":"⌨", "type":"palette", 
-"initialCode":"setUpTypeAssist(false, makeTypeAssistMap(cols.kbd), makeTypeAssistMap(cols.kbd)); document.getElementById('keyboard').style.display='block';"
-},
-
-{"id":"showLatinTransSwitch", "title":"Type-assist: Latin characters needed for transcriptions", 
-"desc":"Show characters needed for IPA or other transcriptions and transliterations.",
-"dataShortTitle":"L", "type":"palette", 
-"initialCode":"setUpTypeAssist(true, latinTypeAssistMap, latinTypeAssistMap)"
-},
 
 
-{"id":"togglePalette", "title":"Show/hide the type-assist palette. ~ also works.", 
-"desc":"Show or hide the palette used for type-assist input.",
-"dataShortTitle":"P", "type":"toggle", "initialCode":"palette=document.getElementById('transcriptionPalette'); if (palette.style.display==='none') {palette.style.display='block';} else {palette.style.display='none';}"
-},
+var show = {}  // indicates which inputAids to use
+    show.shape = true
+    show.hints = false
+    show.default = true
+    show.latin = true
+    show.ipa = true
+    show.transc = 'Latin'
+    show.kbd = ""
+    show.language = "Inuktitut"
 
-]
+
+var inputAids = []
+
 
 
--- _template_ltr/localcode.js
+++ _template_ltr/localcode.js
@@ -3,9 +3,49 @@ globals.typeAssist = ''
 globals.showKeysTranslitToggle = false
 
 
-function localInitialise () {
 
+
+
+window.charCheckerList = [
+//{ wrong:"", right:"" },
+]
+
+
+
+
+
+// list of fonts to add to the standard selection
+window.fontsuggestions =  
+`Namdhinggo SIL`
+
+
+function localInitialise () {
 	}
+	
+
+
+
+
+var _h = {
+end: {}
+}
+
+
+
+
+
+var keyboarddef = [
+"§|1 ꤁ !|2 ꤂ ꤮|3 ꤃ #|4 ꤄ :|5 ꤅ %|6 ꤆ “|7 ꤇ ”|8 ꤈ *|9 ꤉ (|0 ꤀ )|- - _|== +",
+"q ꤠ|w ꤎ|e ꤙ|r ꤚ|t ꤣ|y ꤤ|u ꤥ|i ꤞ|o ꤓ|p ꤑ|[ ꤘ|]",
+"a ꤊ|s ꤔ|d ꤛ|f ꤢ ꤢꤦ|g ꤩ ꤢꤧ|h ꤪ ꤢꤨ|j ꤒ ꤢꤩ|k ꤟ ꤢꤪ|l ꤕ|; ꤜ|' ꤍ|\\ \\",
+"`|z ꤡ|x ꤌ|c ꤏ|v ꤋ|b ꤗ|n ꤖ|m ꤝ|, ꤐ|. ,|/ ꤯"
+]
+// source: 
+
+
+
+
+
 
 function toggleExtended (node) {
     var chars = document.querySelectorAll('.x')
@@ -68,101 +108,6 @@ function toggleShift (node) {
 		}
 	}
 
-
-var _h = {
-'974': ['973', '93B', '93A', '93E', '93B'],
-'973': ['93B', '93A', '93E', '93B', '974'],
-'93B': ['93A', '93E', '93B', '974', '973'],
-'93A': ['93E', '93B', '974', '973', '93B'],
-'93E': ['93B', '974', '973', '93B', '93A'],
-'93B': ['974', '973', '93B', '93A', '93E'],
-'908': ['919', '939', '907'],
-'919': ['939', '907', '908'],
-'939': ['907', '908', '919'],
-'907': ['908', '919', '939'],
-'946': ['947', '94A', '94B', '90E'],
-'947': ['94A', '94B', '90E', '946'],
-'94A': ['94B', '90E', '946', '947'],
-'94B': ['90E', '946', '947', '94A'],
-'90E': ['946', '947', '94A', '94B'],
-'92B': ['915'],
-'915': ['92B'],
-'92A': ['925', '92F'],
-'925': ['92F', '92A'],
-'92F': ['92A', '925'],
-'938': ['92E'],
-'92E': ['938'],
-'92C': ['937', '935'],
-'937': ['935', '92C'],
-'935': ['92C', '937'],
-
-end: {}
-}
-
-
-var keyboarddef = [
-"§|1 ꤁ !|2 ꤂ ꤮|3 ꤃ #|4 ꤄ :|5 ꤅ %|6 ꤆ “|7 ꤇ ”|8 ꤈ *|9 ꤉ (|0 ꤀ )|- - _|== +",
-"q ꤠ|w ꤎ|e ꤙ|r ꤚ|t ꤣ|y ꤤ|u ꤥ|i ꤞ|o ꤓ|p ꤑ|[ ꤘ|]",
-"a ꤊ|s ꤔ|d ꤛ|f ꤢ ꤢꤦ|g ꤩ ꤢꤧ|h ꤪ ꤢꤨ|j ꤒ ꤢꤩ|k ꤟ ꤢꤪ|l ꤕ|; ꤜ|' ꤍ|\\ \\",
-"`|z ꤡ|x ꤌ|c ꤏ|v ꤋ|b ꤗ|n ꤖ|m ꤝ|, ꤐ|. ,|/ ꤯"
-]
-
-
-
-globals.showISOCharMap = ''
-
-var isoCharacterMap = `
-a ā ा ai ै au ौ
-A a अ ā आ ai ऐ au औ
-b b ब
-B bh भ
-c c च
-C ch छ
-d d द ḍ ड
-D dh ध ḍh ढ
-e ē े
-E ē ए
-f f \u095E
-g g ग ġ \u095A
-G gh घ
-h h ह ḥ ः
-i i ि ī ी
-I i इ ī ई
-j j ज
-J jh झ
-k k क
-K kh ख k͟h \u0959
-l l ल ḷ ळ
-m m म ṁ ं
-n n न ñ ञ ṇ ण
-N ṅ ङ ̃ ँ
-o ō ो
-O ō ओ
-p p प
-P ph फ
-q q \u0958
-r r र ṛ \u095C ṛh \u095D
-R r̥ ृ r̥̄ ॄ r̥ ऋ r̥̄ ॠ
-s s स ś श ṣ ष
-t t त ṭ ट
-T th थ ṭh ठ
-u u ु ū ू
-U u उ ū ऊ
-v v व
-y y य
-z z \u095B
-0 0 ०
-1 1 १
-2 2 २
-3 3 ३
-4 4 ४
-5 5 ५
-6 6 ६
-7 7 ७
-8 8 ८
-9 9 ९
-\\ ' ऽ
-`
 
 
 
