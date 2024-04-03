40f0ed574fb43516d03b377a3db7871df38f56c5
r12a <ishida@w3.org>
2024-04-03 12:35:21 +0100

    + Korean picker

---
+++ kore-ko/defaults.js
@@ -0,0 +1,78 @@
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = '' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "36";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "150"; // px height of text area
+	factoryDefaults.lineheight = "1.4"; // line height of text area
+	factoryDefaults.language = 'ko' // language to use for examples
+	factoryDefaults.uifont = ''; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "28";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "";  // default base for combining characters
+	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
+	factoryDefaults.contrast = "low" // contrast for UI text colours
+	factoryDefaults.hints = "none" // type of hint to show alongside characters in the grids
+
+
+var thisPicker = 'korekoPicker'
+
+if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
+else  defaults = factoryDefaults
+ 
+var webFonts = [ "" ]
+
+
+var template = {}
+	template.title = 'Korean'
+	template.sample = "모든 인간은 태어날 때부터 자유로우며 그 존엄과 권리에 있어 동등하다. 인간은 천부적으로 이성과 양심을 부여받았으며 서로 형제애의 정신으로 행동하여야 한다."
+	template.sampleSource = 'https://unicode.org/udhr/d/udhr_kor.html'
+	template.blocklocation= 'kore'  // block directory
+	template.noteslocation = 'kore/ko' // location of script notes
+	template.vocablocation = 'kore/ko_vocab' // location of term database
+	template.direction = "ltr" // indicates whether this is a picker for a RTL script
+	template.github = 'kore'
+	template.scriptcode = 'Kore'
+	template.fontLocale = "kore"
+	template.hints = true
+	template.bicameral = false
+
+	
+	template.defaultSpace = ' '
+	template.spaces = [['SP', ' '], ['ZWSP', '\u200B'], ['NBSP','\u00A0'], 
+	['NNBSP','\u202F'], ['EMSP','\u2003']]
+	template.defaultInvisible = ''
+	template.invisibles = [['ZWJ','\u200D'], ['ZWNJ','\u200C'], ['WJ','\u2060'], 
+	['SHY','\u00AD'], ['NBHY','\u2011'], ['CGJ','\u034F']]
+
+
+
+var controls = [
+{"title":"Trans-<br/>literate", "alt":"Convert Korean text to a one-to-one Latin transliteration.", "code":"doTranscription('transliterate')"},
+]
+
+
+
+
+
+var pulldown = [
+]
+
+
+
+
+
+
+var show = {}  // indicates which inputAids to use
+    show.shape = true
+    show.hints = false
+    show.default = true
+    show.latin = true
+    show.ipa = true
+    show.transc = 'Latin'
+    show.kbd = ""
+    show.language = "Korean"
+
+
+var inputAids = []
+
+
+
