1675af5bd874a53e46e517891c581580181d72ce
r12a <ishida@w3.org>
2024-07-24 06:49:42 +0100

    + arab-ps

---
+++ arab-ps/defaults.js
@@ -0,0 +1,89 @@
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'Scheherazade WF' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "36";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "180"; // px height of text area
+	factoryDefaults.lineheight = "1.4"; // line height of text area
+	factoryDefaults.language = 'ps' // language to use for examples
+	factoryDefaults.uifont = 'Scheherazade New'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "28";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "";  // default base for combining characters
+	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
+	factoryDefaults.contrast = "low" // contrast for UI text colours
+	factoryDefaults.hints = "none" // type of hint to show alongside characters in the grids
+
+
+var thisPicker = 'psPicker'
+
+if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
+else  defaults = factoryDefaults
+ 
+var webFonts = [ "Scheherazade WF", "Noto Naskh Arabic WF" ]
+
+// list of fonts to add to the standard selection
+window.fontsuggestions =  
+`RibengUni`
+
+
+var template = {}
+	template.title = 'Pashto'
+	template.sample = "د بشر ټول افراد ازاد نړۍ ته راځي او د حيثيت او د حقوقو له پلوه سره برابر دي۔ ټول د عقل او وجدان خاوندان دي او بايد يو له بل سره د ورورۍ په روحيه سره چلنند کړي۔"
+	template.sampleSource = 'http://efele.net/udhr/d/udhr_pbu.html'
+	template.blocklocation= 'arab'  // block directory
+	template.noteslocation = 'arab/ps' // location of script notes
+	template.vocablocation = 'arab/ps_vocab' // location of term database
+	template.direction = "rtl" // indicates whether this is a picker for a RTL script
+	template.github = 'arab-ps'
+	template.scriptcode = 'Arab'
+	template.fontLocale = "arab"
+	template.hints = true
+	template.bicameral = false
+
+	
+	template.defaultSpace = ' '
+	template.spaces = [['SP', ' '], ['ZWSP', '\u200B'], ['NBSP','\u00A0'], 
+	['NNBSP','\u202F'], ['EMSP','\u2003'], ['IDSP','\u3000']]
+	template.defaultInvisible = ''
+	template.invisibles = [['ZWJ','\u200D'], ['ZWNJ','\u200C'], ['WJ','\u2060'], 
+	['SHY','\u00AD'], ['NBHY','\u2011'], ['CGJ','\u034F'],  
+	['RLM','\u200F'], ['LRM','\u200E'], ['ALM','\u061C']]
+	template.moreKeys = [['RLI','\u2067'], ['LRI','\u2066'], ['FSI','\u2068'], 
+	['PDI','\u2069'], ['RLO','\u202E'], ['LRO','\u202D'], ['PDF','\u202C'], 
+	['CGJ','\u034F']]
+
+
+
+var controls = [
+{"title":"Trans-<br/>literate", "alt":"Convert Pashto text to a one-to-one Latin transliteration.", "code":"doTranscription('transliterate')"},
+]
+
+
+
+
+
+var pulldown = [
+//{"title":"Inuktitut to IPA", "alt":"Convert Inuktitut text to an <em>approximate</em> IPA transcription.", "code":"doTranscription('toIPA')", "warning":"This only produces an <em>approximation</em> to an IPA transcription. Use it as a base and refine it by hand." },
+
+//{"title":"Inuktitut to Latin", "alt":"Convert Inuktitut script text to the Latin orthography.", "code":"doTranscription('transcription')", "warning":"The result of this conversion needs to be checked for accuracy."},
+]
+
+
+
+
+
+
+var show = {}  // indicates which inputAids to use
+    show.shape = true
+    show.hints = false
+    show.default = true
+    show.latin = true
+    show.ipa = true
+    show.transc = 'Latin'
+    show.kbd = ""
+    show.language = "Pashto"
+
+
+var inputAids = []
+
+
+
