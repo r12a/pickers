dedfc7ddee6a777fe95c4ba807352bbfcaea3e69
r12a <ishida@w3.org>
2025-04-23 09:35:32 +0100

    guru   Add noto serif webfont & make it the default

--- guru/defaults.js
+++ guru/defaults.js
@@ -1,11 +1,11 @@
 var defaults = {}
 var factoryDefaults = {}
-	factoryDefaults.font = 'Noto Sans Gurmukhi WF' // text area font name (a single font, no quotes)
+	factoryDefaults.font = 'Noto Serif Gurmukhi WF' // text area font name (a single font, no quotes)
 	factoryDefaults.size = "36";  // text area font size, number of pixels (just the number)
 	factoryDefaults.rows = "150"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "1.8"; // number representing line height of text area in pixels/100
 	factoryDefaults.language = 'pa' // language to use for examples
-	factoryDefaults.uifont = 'Noto Sans Gurmukhi WF'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uifont = 'Noto Serif Gurmukhi WF'; // font name  for selection area (a single font, no quotes)
 	factoryDefaults.uisize = "28";  // selection area font size, number of pixels (just the number)
 	factoryDefaults.ccbase = "\u25CC";  // default base for combining characters
 	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
@@ -18,7 +18,7 @@ var thisPicker = 'gurmukhiPicker'
 if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
 else  defaults = factoryDefaults
  
-var webFonts = [ "Noto Sans Gurmukhi WF" ]
+var webFonts = [ "Noto Serif Gurmukhi WF", "Noto Sans Gurmukhi WF" ]
 
 
 var template = {}
