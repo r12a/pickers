b55e2fef6257e6697aa442e1272081fcfb0e7c71
r12a <ishida@w3.org>
2025-05-07 21:47:43 +0100

    New styling (batch 6)

--- arab-ug/defaults.js
+++ arab-ug/defaults.js
@@ -1,6 +1,6 @@
 var defaults = {}
 var factoryDefaults = {}
-	factoryDefaults.font = 'Scheherazade' // text area font name (a single font, no quotes)
+	factoryDefaults.font = 'Scheherazade WF' // text area font name (a single font, no quotes)
 	factoryDefaults.size = "40";  // text area font size, number of pixels (just the number)
 	factoryDefaults.rows = "150"; // number representing number of times 100px for height of text area
 	factoryDefaults.lineheight = "1.8"; // number representing line height of text area in pixels/100
@@ -18,7 +18,7 @@ var thisPicker = 'uighurPicker'
 if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
 else  defaults = factoryDefaults
  
-var webFonts = [ "Scheherazade WF", "Noto Nastaliq Urdu WF" ]
+var webFonts = [ "Scheherazade WF" ]
 
 
 var template = {}
