e112227d4aaa2913b11027ab25c780eedbf1f29c
r12a <ishida@w3.org>
2025-10-08 05:48:18 +0100

    + Tangsa

---
+++ tnsa/defaults.js
@@ -0,0 +1,87 @@
+var defaults = {}
+var factoryDefaults = {}
+	factoryDefaults.font = 'Noto Sans Tangsa WF' // text area font name (a single font, no quotes)
+	factoryDefaults.size = "40";  // text area font size, number of pixels (just the number)
+	factoryDefaults.rows = "30"; // vh height of text area
+	factoryDefaults.lineheight = "1.4"; // line height of text area
+	factoryDefaults.language = 'nst' // language to use for examples
+	factoryDefaults.uifont = 'Noto Sans Tangsa WF'; // font name  for selection area (a single font, no quotes)
+	factoryDefaults.uisize = "28";  // selection area font size, number of pixels (just the number)
+	factoryDefaults.ccbase = "";  // default base for combining characters
+	factoryDefaults.uidir = "ltr" // indicates the base direction for the selection area
+	factoryDefaults.contrast = "low" // contrast for UI text colours
+	factoryDefaults.hints = "none" // type of hint to show alongside characters in the grids
+
+
+var thisPicker = 'tnsaPicker'
+
+if (localStorage.pickersStore && localStorage[thisPicker]) defaults = JSON.parse(localStorage[thisPicker]) 
+else  defaults = factoryDefaults
+ 
+var webFonts = [ "Noto Sans Tangsa WF" ]
+
+// list of fonts to add to the standard selection
+window.fontsuggestions =  
+``
+
+
+var template = {}
+	template.title = 'Tangsa'
+	template.sample = "𖩸𖪰𖪒𖪤𖪌𖪧 𖪬𖪗, 𖪸𖪙 𖪵𖪃 𖪠𖪗 𖪭𖩽𖪦𖩴 𖪬𖩴𖪢𖩵 𖪩𖩻𖪬 𖪲𖪒𖪰𖪖 𖪣𖩰 𖪦𖪗. 𖪭𖩽𖪦𖩴 𖪬𖩴𖪢𖩵 𖪫𖪗 𖪦𖪌𖪂 𖪠𖪄𖪀 𖪵𖪌𖪐 𖪥𖩻𖪫 𖪩𖪌𖪬 𖪥𖩶-𖪩𖪇 𖪫𖪌𖪬 𖩸𖪷𖪕 𖪴𖩿. 𖪲𖪒𖪠𖩲 𖪰𖪖 𖪣𖩻𖪫 𖪡𖪏 𖪩𖪇 𖪬𖪗 𖪦𖪌𖪂 𖪠𖪆 𖪠𖩲 𖪰𖪖 𖪮𖩸𖪫 𖪮𖩶 𖩸𖪰𖪖 𖩸𖪴𖩰𖩸𖪀 𖪩𖪘 𖪲𖪒𖪷𖩴𖪀 𖪰𖪖 𖪡𖪍 𖪬𖪗."
+	template.sampleSource = 'https://fonts.google.com/noto/specimen/Noto+Sans+Tangsa'
+	template.blocklocation= 'tnsa'  // block directory
+	template.noteslocation = 'tnsa/nst' // location of script notes
+	template.vocablocation = 'tnsa/nst_vocab' // location of term database
+	template.direction = "ltr" // indicates whether this is a picker for a RTL script
+	template.github = 'tnsa'
+	template.scriptcode = 'tnsa'
+	template.fontLocale = "tnsa"
+	template.hints = true
+	template.bicameral = true
+
+	
+	template.defaultSpace = ' '
+	template.spaces = [['SP', ' '], ['ZWSP', '\u200B'], ['NBSP','\u00A0'], 
+	['NNBSP','\u202F'], ['EMSP','\u2003']]
+	template.defaultInvisible = ''
+	template.invisibles = [['ZWJ','\u200D'], ['ZWNJ','\u200C'], ['WJ','\u2060'], 
+	['SHY','\u00AD'], ['NBHY','\u2011'], ['CGJ','\u034F']]
+
+
+
+var controls = [
+{"title":"Trans-<br/>literate", "alt":"Convert XXXX text to a one-to-one Latin transliteration.", "code":"doTranscription('transliterate')"},
+]
+
+
+
+
+
+var pulldown = [
+/*
+{"title":"Inuktitut to IPA", "alt":"Convert Inuktitut text to an <em>approximate</em> IPA transcription.", "code":"doTranscription('toIPA')", "warning":"This only produces an <em>approximation</em> to an IPA transcription. Use it as a base and refine it by hand." },
+
+{"title":"Inuktitut to Latin", "alt":"Convert Inuktitut script text to the Latin orthography.", "code":"doTranscription('transcription')", "warning":"The result of this conversion needs to be checked for accuracy."},
+*/
+]
+
+
+
+
+
+
+var show = {}  // indicates which inputAids to use
+    show.shape = true
+    show.hints = false
+    show.default = true
+    show.latin = true
+    show.ipa = true
+    show.transc = 'Latin'
+    show.kbd = ""
+    show.language = "Inuktitut"
+
+
+var inputAids = []
+
+
+
